1d5bdb19cba0944f69c6cff4ac7186f1e774987f
Yuan Wang <me@yuanwang.ca>
2019-12-11 21:23:42 -0600

    pin lorri to latest commit

--- overlays/lorri.nix
+++ overlays/lorri.nix
@@ -4,10 +4,10 @@ self: super: {
     let src = super.fetchFromGitHub {
           owner  = "target";
           repo   = "lorri";
-          # rev    = "refs/heads/rolling-release";
-          rev    = "38eae3d487526ece9d1b8c9bb0d27fb45cf60816";
-          sha256 = "11k9lxg9cv6dlxj4haydvw4dhcfyszwvx7jx9p24jadqsy9jmbj4";
-          # date = 2019-08-20T10:44:02-04:00;
+          # rev    = "refs/master";
+          rev    = "152a3ccdc3025dd12f0bdb69a260550f12114722";
+          sha256 = "1915f027766kjfy650pxw9zzsk7ifxgn48dkr38wrlqrvlpxyv4s";
+          # date = 2019-12-11T12:35:56+01:00;
           fetchSubmodules = true;
         }; in
       import src { inherit src; pkgs = self; };
