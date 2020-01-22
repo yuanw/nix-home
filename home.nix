2fabb0aa72ca61eee6c8b6ee07f53d14cace34a4
Yuan Wang <yuan.wang@workiva.com>
2020-01-22 12:22:53 -0600

    add git signing

--- home.nix
+++ home.nix
@@ -5,6 +5,12 @@
   programs.git = {
     enable = true;
     userEmail = "yuan.wang@workiva.com";
+
+    signing = {
+      key = "19AD3F6B1A5BF3BF";
+      signByDefault = true;
+    };
+
     extraConfig = {
       github.user           = "yuanwang-wf";
     };
