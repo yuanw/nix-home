bcd0f7d87aa9135b6c7dde174410b57adbe749da
Yuan Wang <me@yuanwang.ca>
2020-09-30 23:48:52 -0600

    wire spacebar

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,6 +1,8 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
 
-{
+in with pkgs.stdenv;
+with lib; {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   nixpkgs.overlays = let path = ./overlays;
@@ -35,6 +37,28 @@
     enable = true;
     skhdConfig = "cmd - e : open ~/.nix-profile/Applications/Emacs.app";
   };
+
+  services.spacebar.enable = true;
+  services.spacebar.package = pkgs.spacebar;
+  services.spacebar.config = {
+    debug_output = "on";
+    clock_format = "%R";
+    space_icon_strip = "   ";
+    text_font = ''"Menlo:Bold:12.0"'';
+    icon_font = ''"FontAwesome:Regular:12.0"'';
+    background_color = "0xff202020";
+    foreground_color = "0xffa8a8a8";
+    space_icon_color = "0xff14b1ab";
+    dnd_icon_color = "0xfffcf7bb";
+    clock_icon_color = "0xff99d8d0";
+    power_icon_color = "0xfff69e7b";
+    battery_icon_color = "0xffffbcbc";
+    power_icon_strip = " ";
+    space_icon = "";
+    clock_icon = "";
+    dnd_icon = "";
+  };
+
   services.yabai = {
     enable = true;
     package = pkgs.yabai;
@@ -82,6 +106,29 @@
   programs.zsh.enable = true; # default shell on catalina
   programs.bash.enable = false;
   time.timeZone = "America/Regina";
+
+  system.defaults = {
+    dock = {
+      autohide = true;
+      mru-spaces = false;
+      minimize-to-application = true;
+    };
+
+    screencapture.location = "/tmp";
+
+    finder = {
+      AppleShowAllExtensions = true;
+      _FXShowPosixPathInTitle = true;
+      FXEnableExtensionChangeWarning = false;
+    };
+
+    trackpad = {
+      Clicking = true;
+      TrackpadThreeFingerDrag = true;
+    };
+
+    NSGlobalDomain._HIHideMenuBar = true;
+  };
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
