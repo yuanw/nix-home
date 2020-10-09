98cf70034e4b545a0140936dd77ea35ad7064464
Yuan Wang <yuan.wang@workiva.com>
2020-10-08 20:49:54 -0600

    minor cleanup

--- machines/wf17084/configuration.nix
+++ machines/wf17084/configuration.nix
@@ -1,13 +1,12 @@
-let
-  sources = import ../nix/sources.nix;
-  pkgs = import sources.nixpkgs { };
-in { config, lib, ... }: {
+{ config, lib, ... }: {
   imports = lib.attrValues (import ../../modules);
   networking.hostName = "wf17084";
-  #services.yabai.enableScriptingAddition = false;
-  #
-  programs.workShell.enable = true;
-  programs.dart.enable = true;
+
+
+  programs = {
+    dart.enable = true;
+    workShell.enable = true;
+  };
 
   home-manager.users.yuanwang.programs.git = {
     userEmail = "yuan.wang@workiva.com";
