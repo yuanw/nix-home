1dfd2a7098114ee267f20683d592e26378915c81
Yuan Wang <me@yuanwang.ca>
2020-10-12 11:48:09 -0600

    updates

--- overlays/pragmata-pro-font.nix
+++ overlays/pragmata-pro-font.nix
@@ -3,17 +3,17 @@ self: super: {
     stdenv.mkDerivation rec {
       nativeBuildInputs = [ unzip ];
       pname = "pragmata-pro-font";
-      version = "1.2";
+      version = "0.828";
       buildInputs = [ unzip ];
 
-      src = ../private/EssentialPragmataPro-Regular1.2.zip;
+      src = ../private/PragmataPro0.828-2.zip;
       sha256 =
         "64f4d9afa1afd5e4ae1b1d01f795b03018eb6583b1455c393a5a1397008de2c4";
       phases = [ "installPhase" ];
       installPhase = ''
-        mkdir -p $out/share/fonts/Essential-PragmataPro
+        mkdir -p $out/share/fonts/PragmataPro
         unzip $src
-        cp EssentialPragmataPro-Regular1.2/*.ttf $out/share/fonts/Essential-PragmataPro
+        cp PragmataPro0.828/*.ttf $out/share/fonts/PragmataPro
       '';
     };
 }
