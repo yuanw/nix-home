fa4968bff015c3d160c42c3ad3bb7834a0508348
Yuan Wang <me@yuanwang.ca>
2020-11-05 02:43:35 -0600

    updates

--- conf.d/doom/config.el
+++ conf.d/doom/config.el
@@ -29,6 +29,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org-notes"
+      org-roam-dailies-directory (concat org-directory "dailies")
       org-agenda-files (directory-files-recursively org-directory "\.org$"))
 
 
