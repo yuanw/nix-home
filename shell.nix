4d768935ef04ea6981c4fdd77340dc154a84d6fb
Yuan Wang <yuan.wang@workiva.com>
2020-11-05 21:24:32 -0600

    exclude nix/sources.nix for lint or format

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 { sources ? import ./nix/sources.nix }:
 let
   pkgs = import sources.nixpkgs { };
-  format = pkgs.writeShellScriptBin "format" "${pkgs.fd}/bin/fd -e nix -x ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
-  lint = pkgs.writeShellScriptBin "lint" "${pkgs.fd}/bin/fd -e nix -x ${pkgs.nix-linter}/bin/nix-linter && echo No lint errors!";
+  format = pkgs.writeShellScriptBin "format" "${pkgs.fd}/bin/fd --exclude '/nix/sources.nix' -e nix -x ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
+  lint = pkgs.writeShellScriptBin "lint" "${pkgs.fd}/bin/fd --exclude '/nix/sources.nix' -e nix -x ${pkgs.nix-linter}/bin/nix-linter && echo No lint errors!";
   rebuild = pkgs.writeShellScriptBin "rebuild" ''
     ${format}/bin/format
+    ${lint}/bin/lint
     darwin-rebuild switch --show-trace \
       -I nixpkgs=${sources.nixpkgs}
   '';
@@ -12,7 +13,5 @@ in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     niv
-    lint
     rebuild
-  ];
-}
+  ];}
