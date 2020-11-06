2dfe1ed02ae3b32a81b71dad4117ba331e747400
Yuan Wang <yuan.wang@workiva.com>
2020-11-05 20:53:53 -0600

    use pinned channels

--- modules/module-list/workShell.nix
+++ modules/module-list/workShell.nix
@@ -1,4 +1,4 @@
-{ config, lib,  ... }:
+{ config, lib, ... }:
 
 with lib;
 let
--- shell.nix
+++ shell.nix
@@ -3,15 +3,16 @@ let
   pkgs = import sources.nixpkgs { };
   format = pkgs.writeShellScriptBin "format" "${pkgs.fd}/bin/fd -e nix -x ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
   lint = pkgs.writeShellScriptBin "lint" "${pkgs.fd}/bin/fd -e nix -x ${pkgs.nix-linter}/bin/nix-linter && echo No lint errors!";
-  cleanup = pkgs.writeShellScriptBin "cleanup" ''
-    set -e
+  rebuild = pkgs.writeShellScriptBin "rebuild" ''
     ${format}/bin/format
+    darwin-rebuild switch --show-trace \
+      -I nixpkgs=${sources.nixpkgs}
   '';
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
     niv
     lint
-    cleanup
+    rebuild
   ];
 }
