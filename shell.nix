f94af30a7ba139cee5134c0bf50a4eee3c1133c5
Yuan Wang <me@yuanwang.ca>
2020-11-15 22:41:38 -0600

    format shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,10 @@
 { sources ? import ./nix/sources.nix }:
 let
   pkgs = import sources.nixpkgs { };
-  format = pkgs.writeShellScriptBin "format" "${pkgs.fd}/bin/fd --exclude '/nix/sources.nix' -e nix -x ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
-  lint = pkgs.writeShellScriptBin "lint" "${pkgs.fd}/bin/fd --exclude '/nix/sources.nix' -e nix -x ${pkgs.nix-linter}/bin/nix-linter && echo No lint errors!";
+  format = pkgs.writeShellScriptBin "format"
+    "${pkgs.fd}/bin/fd --exclude '/nix/sources.nix' -e nix -x ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt";
+  lint = pkgs.writeShellScriptBin "lint"
+    "${pkgs.fd}/bin/fd --exclude '/nix/sources.nix' -e nix -x ${pkgs.nix-linter}/bin/nix-linter && echo No lint errors!";
   rebuild = pkgs.writeShellScriptBin "rebuild" ''
     ${format}/bin/format
     ${lint}/bin/lint
@@ -10,11 +12,7 @@ let
       -I darwin=${sources.nix-darwin} \
       -I nixpkgs=${sources.nixpkgs}
   '';
-in
-pkgs.mkShell {
+in pkgs.mkShell {
   name = "nix-darwin-shell";
-  buildInputs = with pkgs; [
-    niv
-    rebuild
-  ];
+  buildInputs = with pkgs; [ niv rebuild ];
 }
