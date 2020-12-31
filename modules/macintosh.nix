a2c641b228a0f856369e0e54f117dd55ee60d0a8
Yuan Wang <yuan.wang@workiva.com>
2020-12-31 11:00:09 -0600

    add dracula theme for tmux

--- modules/macintosh.nix
+++ modules/macintosh.nix
@@ -300,6 +300,23 @@ with lib; {
       tmux = {
         enable = true;
         terminal = "screen-256color";
+        clock24 = true;
+        plugins = with pkgs.tmuxPlugins; [
+          sensible
+          yank
+          {
+            plugin = dracula;
+            extraConfig = ''
+              set -g @dracula-show-battery false
+              set -g @dracula-show-powerline true
+              set -g @dracula-refresh-rate 10
+            '';
+          }
+        ];
+
+        extraConfig = ''
+          set -g mouse on
+        '';
       };
 
       zoxide = {
