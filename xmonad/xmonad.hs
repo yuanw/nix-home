786dcf6dd077ede60a805e5220a4dcaae996f043
Yuan Wang <me@yuanwang.ca>
2021-01-07 20:35:14 -0600

    drop xmobar for now

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -241,9 +241,7 @@ myStartupHook = return ()
 
 -- Run xmonad with the settings you specify. No need to modify this.
 --
-main = do
-  xmproc <- spawnPipe "xmobar"
-  xmonad $ docks $ ewmh defaults
+main = xmonad $ docks $ ewmh defaults
 
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
