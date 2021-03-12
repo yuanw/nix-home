2500cc4ed39e334ef69ea73f593fe70e58e90c53
yuanwang <me@yuanwang.ca>
2021-03-11 20:40:20 -0600

    update readme

--- overlays/default.nix
+++ overlays/default.nix
@@ -25,7 +25,7 @@ final: prev:
     };
 
   dart = prev.callPackage ./dart.nix { };
-  pragmata-pro = prev.callPack ./pragmata-pro-font.nix { };
+  pragmata-pro = prev.callPackage ./pragmata-pro-font.nix { };
 
   Stretchly = final.installApplication rec {
     name = "Stretchly";
