84edc03cc71c74ae0e93e8a10f06e6b8be2ae1d8
yuanw <me@yuanwang.ca>
2021-04-02 20:07:57 +0000

    update

--- flake.nix
+++ flake.nix
@@ -43,7 +43,6 @@
                 extraConfig = { github.user = "yuanw"; };
               };
               programs = {
-                stevenBlackHosts.enable = true;
               };
             })
           ];
