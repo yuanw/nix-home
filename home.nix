7827ad8e099b36e9a2cc53d2b00a3149223f80a9
yuanwang <yuan.wang@workiva.com>
2021-04-10 13:34:47 -0600

    update

--- home.nix
+++ home.nix
@@ -62,7 +62,7 @@
           x = 12;
           y = 0;
         };
-        window.decorations = "none";
+        #window.decorations = "none";
         window.dynamic_padding = false;
         # background_opacity = 0.8;
         # base16-material-palenight-256
