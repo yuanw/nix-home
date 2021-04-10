1a746ccadedf13bca6b023495683d26b0bf16910
yuanwang <yuan.wang@workiva.com>
2021-04-10 13:49:18 -0600

    use 002 for now

--- overlays/alerter.nix
+++ overlays/alerter.nix
@@ -2,16 +2,15 @@
 stdenv.mkDerivation rec {
 
   pname = "alerter";
-  version = "004";
+  version = "002";
 
   buildInputs = [ unzip ];
 
   src = fetchurl {
     url =
-      "https://github.com/vjeantet/alerter/releases/download/004/alerter_v004_darwin_amd64.zip";
-    sha256 = "0wk8nzdkn30djdxz44za4jyl9z7m7c22a3v9ycgxn35yrrw3qmnz";
+      "https://github.com/vjeantet/alerter/releases/download/002/alerter_v002_darwin_amd64.zip";
+    sha256 = "0jgcp89rvzzfcybqhwah50ripfp41q1v3zv8d69qp466kk99lxka";
   };
-
   # Work around the "unpacker appears to have produced no directories"
   # case that happens when the archive doesn't have a subdirectory.
   setSourceRoot = "sourceRoot=`pwd`";
