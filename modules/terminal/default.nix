a5e28e32ab584896633236bdddd0ab2211885bb7
yuanwang <yuan.wang@workiva.com>
2021-04-21 22:25:39 -0600

    does this work

--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -49,7 +49,6 @@ in {
             setw -g window-status-activity-style "none"
             setw -g window-status-separator ""
             setw -g window-status-style "none,fg=colour60,bg=colour60"
-            set -g @prefix_highlight_fg 'white'
             set -g @prefix_highlight_show_copy_mode 'on'
             set -g @prefix_highlight_copy_mode_attr 'fg=black,bg=yellow,bold' # default is 'fg=default,bg=yellow'
             set -g @prefix_highlight_show_sync_mode 'on'
@@ -57,8 +56,8 @@ in {
             set -g @prefix_highlight_prefix_prompt 'Wait'
             set -g @prefix_highlight_copy_prompt 'Copy'
             set -g @prefix_highlight_sync_prompt 'Sync'
-            set -g status-left "#[fg=colour232,bg=colour117] #S #[fg=colour117,bg=colour60,nobold,nounderscore,noitalics]"
-            set -g status-right "#{prefix_highlight} |  #[fg=colour60,bg=colour60,nobold,nounderscore,noitalics]#[fg=colour146,bg=colour60] %Y-%m-%d  %H:%M #[fg=colour117,bg=colour60,nobold,nounderscore,noitalics]#[fg=colour232,bg=colour117] #h "
+            set -g status-left "#[fg=colour232,bg=colour117]#{?client_prefix,#[bg=colour167],} #S #[fg=colour117,bg=colour60,nobold,nounderscore,noitalics]"
+            set -g status-right "#[fg=colour60,bg=colour60,nobold,nounderscore,noitalics]#[fg=colour146,bg=colour60] %Y-%m-%d  %H:%M #[fg=colour117,bg=colour60,nobold,nounderscore,noitalics]#[fg=colour232,bg=colour117] #h "
             setw -g window-status-format "#[fg=colour60,bg=colour60] #I #[fg=colour60,bg=colour60] #W "
             setw -g window-status-current-format "#[fg=colour60,bg=colour60,nobold,nounderscore,noitalics]#[fg=colour146,bg=colour60] #I #[fg=colour146,bg=colour60] #W #[fg=colour60,bg=colour60,nobold,nounderscore,noitalics]"
           '';
