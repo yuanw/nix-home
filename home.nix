a2a8498dbd32bc3bf4cf3575f29e11a14df55546
yuanwang <yuan.wang@workiva.com>
2021-04-28 09:18:56 -0600

    update

--- home.nix
+++ home.nix
@@ -22,8 +22,9 @@
       enable = true;
       config.theme = "palenight";
       themes = {
-        palenight = builtins.readFile (pkgs.fetchgit {
-          url = "https://github.com/equinusocio/material-theme";
+        palenight = builtins.readFile (pkgs.fetchFromGitHub {
+          owner = "equinusocio";
+          repo = "material-theme";
           rev = "614b7e8bc7369c32e852297d42253643ebf90d55";
           sha256 = "1gjfisksvqa2d08na0yln7yxny4i16wrmvlfnwllbqrgwh26v94g";
         } + "/schemes/Material-Theme-Palenight.tmTheme");
