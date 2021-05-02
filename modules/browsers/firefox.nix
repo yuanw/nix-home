0ec63c149da86f3be4ee4524763beb845e248385
yuanwang <me@yuanwang.ca>
2021-05-02 13:35:48 -0600

    add userChrome css

--- modules/browsers/firefox.nix
+++ modules/browsers/firefox.nix
@@ -16,9 +16,51 @@ in {
         https-everywhere
         privacy-badger
       ];
-      programs.firefox.profiles.default.settings."browser.search.isUS" = true;
-      programs.firefox.profiles.default.isDefault = true;
-      programs.firefox.profiles.default.name = "Default";
+      programs.firefox.profiles = {
+        home = {
+          id = 0;
+          userChrome = (builtins.readFile (pkgs.substituteAll {
+            name = "homeUserChrome";
+            src = ./userChrome.css;
+            tabLineColour = "#5e81ac";
+          }));
+          settings = {
+            "app.update.auto" = false;
+            "browser.startup.homepage" = "https://lobste.rs";
+            "browser.search.region" = "GB";
+            "browser.search.countryCode" = "GB";
+            "browser.search.isUS" = false;
+            "browser.shell.checkDefaultBrowser" = false;
+            "browser.ctrlTab.recentlyUsedOrder" = false;
+            "browser.newtabpage.enabled" = false;
+            "browser.newtabpage.enhanced" = false;
+            "browser.newtab.preload" = false;
+            "browser.newtabpage.directory.ping" = "";
+            "browser.newtabpage.directory.source" = "data:text/plain,{}";
+            "browser.bookmarks.showMobileBookmarks" = true;
+            "browser.uidensity" = 1;
+            "browser.urlbar.placeholderName" = "DuckDuckGo";
+            "browser.urlbar.update1" = true;
+            "distribution.searchplugins.defaultLocale" = "en-GB";
+            "general.useragent.locale" = "en-GB";
+            "identity.fxaccounts.account.device.name" =
+              config.networking.hostName;
+            "privacy.trackingprotection.enabled" = true;
+            "privacy.trackingprotection.socialtracking.enabled" = true;
+            "privacy.trackingprotection.socialtracking.annotate.enabled" = true;
+            "reader.color_scheme" = "sepia";
+            "services.sync.declinedEngines" = "addons,passwords,prefs";
+            "services.sync.engine.addons" = false;
+            "services.sync.engineStatusChanged.addons" = true;
+            "services.sync.engine.passwords" = false;
+            "services.sync.engine.prefs" = false;
+            "services.sync.engineStatusChanged.prefs" = true;
+            "signon.rememberSignons" = false;
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          };
+
+        };
+      };
     };
   };
 }
