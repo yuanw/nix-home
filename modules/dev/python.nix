deb09c0fc5921fac099d5b34766f92703378a51a
yuanwang <yuan.wang@workiva.com>
2021-05-27 15:45:07 -0600

    using python38

--- modules/dev/python.nix
+++ modules/dev/python.nix
@@ -7,7 +7,7 @@ in {
 
   config = mkIf cfg.enable {
     home-manager.users.${localConfig.username}.home.packages = [
-      (pkgs.python37.withPackages (ps:
+      (pkgs.python38.withPackages (ps:
         with ps; [
           pip
           ipython
