c59782936d730e9d34768059cc033ab7f6901ed6
yuanw <me@yuanwang.ca>
2021-06-06 15:32:34 -0600

    does this work ?

--- modules/dev/haskell.nix
+++ modules/dev/haskell.nix
@@ -1,27 +1,26 @@
 { config, lib, pkgs, localConfig, ... }:
 
 with lib;
-let cfg = config.programs.haskell;
+let
+  cfg = config.programs.haskell;
+  haskell-env = pkgs.unstable.haskellPackages.ghcWithHoogle (hp:
+    with hp; [
+      xmonad
+      xmonad-contrib
+      xmonad-extras
+      apply-refact
+      haskell-language-server
+      brittany
+      cabal-install
+      hlint
+      xmobar
+    ]);
 in {
   options.programs.haskell = { enable = mkEnableOption "haskell"; };
 
   config = mkIf cfg.enable {
     home-manager.users.${localConfig.username} = {
-      home.packages = [
-        pkgs.haskellPackages.ghcWithHoogle
-        (hp:
-          with hp; [
-            xmonad
-            xmonad-contrib
-            xmonad-extras
-            apply-refact
-            haskell-language-server
-            brittany
-            cabal-install
-            hlint
-            xmobar
-          ])
-      ];
+      home.packages = [ haskell-env ];
     };
   };
 }
