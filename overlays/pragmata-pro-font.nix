076666d9ef880b9380b63cfcbbc2df0c423c93f2
yuanwang <yuan.wang@workiva.com>
2021-06-07 08:11:38 -0600

    update font

--- overlays/pragmata-pro-font.nix
+++ overlays/pragmata-pro-font.nix
@@ -3,10 +3,10 @@
 stdenv.mkDerivation rec {
   nativeBuildInputs = [ unzip ];
   pname = "pragmata-pro-font";
-  version = "0.828-2";
+  version = "0.829";
   buildInputs = [ unzip ];
 
-  src = ../private/PragmataPro0.828-2.zip;
+  src = ../private/PragmataPro0.829.zip;
   sha256 = "64f4d9afa1afd5e4ae1b1d01f795b03018eb6583b1455c393a5a1397008de2c4";
   phases = [ "installPhase" ];
   installPhase = ''
