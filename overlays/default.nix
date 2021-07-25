14ca5eed0bdf78ff07356067865ec10e7897db3a
yuanwang <yuan.wang@workiva.com>
2021-07-25 07:58:41 -0600

    update julia mac version to 1.6.2

--- overlays/default.nix
+++ overlays/default.nix
@@ -32,11 +32,6 @@ final: prev:
   # gap marked as broken for darwin, it seems build on my mac
   gap = prev.gap.overrideAttrs (oldAttrs: rec { meta.broken = false; });
 
-  emacsOsxNativeTile = prev.emacsPgtkGcc.overrideAttrs (oldAttrs: rec {
-    patches =
-      [ ./patches/no-titlebar.patch ./patches/fix-window-role-yabai.patch ];
-  });
-
   emacsCatalina = prev.emacs.overrideAttrs (o: rec { CFLAGS = ""; });
 
   Docker = final.installApplication rec {
@@ -69,12 +64,12 @@ final: prev:
 
   juliaMac = final.installApplication rec {
     name = "Julia";
-    version = "1.6.1";
+    version = "1.6.2";
     sourceRoot = "Julia-1.6.app";
     src = prev.fetchurl {
       url =
-        "https://julialang-s3.julialang.org/bin/mac/x64/1.6/julia-1.6.1-mac64.dmg";
-      sha256 = "1qgygxlrb6y1h29m8f42fnharvpyncnnv0xf7l8ifnfafsy9ydgd";
+        "https://julialang-s3.julialang.org/bin/mac/x64/1.6/julia-1.6.2-mac64.dmg";
+      sha256 = "1j9pqi7lvh8v1j81bpy7gjaa7kdr4s92qkc27rdp2z6wl48f3dbg";
     };
     description = "High Performance";
     homepage = "https://julialang.org/";
