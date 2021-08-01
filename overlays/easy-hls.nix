6b53bd19da0553d19a7ed1599503c75325f2209c
yuanwang <yuan.wang@workiva.com>
2021-08-01 13:48:56 -0600

    update hls

--- overlays/easy-hls.nix
+++ overlays/easy-hls.nix
@@ -4,31 +4,32 @@
 , ghcVersions ? [
   "8.6.4"
   "8.6.5"
-  "8.8.2"
   "8.8.3"
   "8.8.4"
   "8.10.2"
   "8.10.3"
   "8.10.4"
+  "8.10.5"
+  "9.0.1"
 ] }:
 let
   inherit (stdenv) mkDerivation isDarwin;
   hlsBins = [ "wrapper" ] ++ ghcVersions;
 in mkDerivation rec {
   pname = "haskell-language-server";
-  version = "1.0.0";
+  version = "1.3.0";
   src = if stdenv.isDarwin then
     fetchzip {
       url =
         "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-macOS-${version}.tar.gz";
-      sha256 = "PXv8k7GebeHHsqOlgju2NIrubApg8JK8OpRNDevTqqU=";
+      sha256 = "0mga2m7qx898szfq4w04a3ji7gwh4jm6zasq4xky7c7hwkmr9y5s";
       stripRoot = false;
     }
   else
     fetchzip {
       url =
         "https://github.com/haskell/haskell-language-server/releases/download/${version}/haskell-language-server-Linux-${version}.tar.gz";
-      sha256 = "oncBl94KOFjsg22mgxucAxa4T5Hq1SjmsGQ3yXXidjI=";
+      sha256 = "0wgj2jvqfpifcqvyqs4snfln460f4rlgbn66hbmialkg67a2q8qg";
       stripRoot = false;
     };
 
