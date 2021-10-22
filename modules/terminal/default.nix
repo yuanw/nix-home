688afec3a8e9992fcfd7723fe180acd1c603fb10
Yuan Wang <me@yuanwang.ca>
2021-10-22 07:23:45 -0600

    add some comments

--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -113,6 +113,7 @@ in {
                  xargs tmux join-pane -v -s"
             set-option -g renumber-windows on
 
+            # keep this at the bottom
             bind-key Tab display-menu -T "#[align=centre]Sessions" "Switch" . 'choose-session -Zw' Last l "switch-client -l" ${tmuxMenuSeperator} \
               "Open Main Workspace" m "display-popup -E \" td ${cfg.mainWorkspaceDir} \"" "Open Sec Workspace" s "display-popup -E \" td ${cfg.secondaryWorkspaceDir} \""   ${tmuxMenuSeperator} \
               "Kill Current Session" k "run-shell 'tmux switch-client -n \; tmux kill-session -t #{session_name}'"  "Kill Other Sessions" o "display-popup -E \"tkill \"" ${tmuxMenuSeperator} \
