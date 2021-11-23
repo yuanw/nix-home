c88b88af9245a18c395a5a5d6975aa6df3e9884b
yuanwang <yuan.wang@workiva.com>
2021-11-23 07:04:54 -0600

    yabai 3.3.7 -> 3.3.10

--- overlays/default.nix
+++ overlays/default.nix
@@ -32,7 +32,15 @@ final: prev:
     '';
 
   });
-
+  yabai = prev.yabai.overrideAttrs (
+    o: rec {
+      version = "3.3.10";
+      src = builtins.fetchTarball {
+        url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
+        sha256 = "1z95njalhvyfs2xx6d91p9b013pc4ad846drhw0k5gipvl03pp92";
+      };
+    }
+  );
   alerter = prev.callPackage ./alerter.nix { };
   dart = prev.callPackage ./dart.nix { };
   hls = prev.callPackage ./easy-hls.nix { };
