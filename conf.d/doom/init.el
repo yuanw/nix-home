adb1a1c0904185b3401347a49a009a3a980346ba
yuanw <me@yuanwang.ca>
2022-01-30 12:54:57 -0600

    modified doom config

--- conf.d/doom/init.el
+++ conf.d/doom/init.el
@@ -153,7 +153,7 @@
        ;;plantuml          ; diagrams for confusing people more
        purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
-        +pyenv
+        ;;+pyenv
         +lsp)
         ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
@@ -175,7 +175,7 @@
        ;;(wanderlust +gmail)
 
        :app
-       everywhere
+       ;;everywhere
        ;;calendar
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
