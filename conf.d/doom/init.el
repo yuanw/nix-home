a2e77a37d323400792ec997a2e597224d15dd3cf
yuanw <me@yuanwang.ca>
2022-06-18 15:14:58 -0600

    update doom config

--- conf.d/doom/init.el
+++ conf.d/doom/init.el
@@ -103,6 +103,7 @@
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
+       tree-sitter
 
        :lang
        agda              ; types of types of types of types...
