b310a951fb74be52f5644270713dd609daba842d
yuanwang <yuan.wang@workiva.com>
2022-08-09 09:45:10 -0600

    workaround some doom error

--- modules/wm/yabai.nix
+++ modules/wm/yabai.nix
@@ -15,7 +15,7 @@ let
   moveConfig = builtins.readFile ./skhdrc;
   # it is nice to reference pkgs full path
   laucherConfig = ''
-    shift + ctrl + alt - d: ${pkgs.emacs}/bin/emacsclient -c
+    shift + ctrl + alt - d: ${pkgs.emacs}/bin/emacs
     shift + ctrl + alt - e: emacsclient --eval "(emacs-everywhere)"
     shift + ctrl + alt - return : open -n -a ~/.nix-profile/Applications/Alacritty.app
     shift + ctrl + alt - v: osascript -e 'tell application "Viscosity" to connect "work"'
