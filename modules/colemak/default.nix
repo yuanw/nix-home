c75132ba68d7d990df4566ae9be2b5238ae51277
yuanwang <yuan.wang@workiva.com>
2022-08-25 13:46:19 -0600

    tweak colemak settings

--- modules/colemak/default.nix
+++ modules/colemak/default.nix
@@ -5,11 +5,11 @@ let
   cfg = config.modules.colemak;
   colemak = pkgs.writeShellScriptBin "colemak" ''
     case "$1" in
-      "l1" | "level1" | "1" ) gotta-go-fast -prw 60 $XDG_CONFIG_HOME/colemak/level1.txt;;
-      "l2" | "level2" | "2" ) gotta-go-fast -prw 60 $XDG_CONFIG_HOME/colemak/level2.txt;;
-      "l3" | "level3" | "3" ) gotta-go-fast -prw 60 $XDG_CONFIG_HOME/colemak/level3.txt;;
-      "l4" | "level4" | "4" ) gotta-go-fast -prw 60 $XDG_CONFIG_HOME/colemak/level4.txt;;
-      "l5" | "level5" | "5" ) gotta-go-fast -prw 60 $XDG_CONFIG_HOME/colemak/level5.txt;;
+      "l1" | "level1" | "1" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level1.txt;;
+      "l2" | "level2" | "2" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level2.txt;;
+      "l3" | "level3" | "3" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level3.txt;;
+      "l4" | "level4" | "4" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level4.txt;;
+      "l5" | "level5" | "5" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level5.txt;;
       *) gotta-go-fast -w 60;;
     esac
   '';
