909e349f6a045d64db4bfd8b733b46bb5d4091f2
yuanwang <yuan.wang@workiva.com>
2022-08-30 18:10:13 -0600

    format

--- macintosh.nix
+++ macintosh.nix
@@ -6,27 +6,27 @@ with lib; {
   nix = {
     package = pkgs.nixFlakes;
 
-  configureBuildUsers = true;
+    configureBuildUsers = true;
     settings = {
-    substituters= [
-      "https://utdemir.cachix.org"
-      "https://hs-nix-template.cachix.org"
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://cachix.org/api/v1/cache/yuanwang-wf"
-      "https://cachix.org/api/v1/cache/emacs"
-    ];
-    trusted-public-keys = [
-      "utdemir.cachix.org-1:mDgucWXufo3UuSymLuQumqOq1bNeclnnIEkD4fFMhsw="
-      "hs-nix-template.cachix.org-1:/YbjZCrYAw7d9ayLayk7ZhBdTEkR10ZFmFuOq6ZJo4c="
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "yuanwang-wf.cachix.org-1:P/RZ5Iuuuv2MYCNCnAsLfPGmgKMKeTwPaJclkrcwx80="
-      "emacs.cachix.org-1:b1SMJNLY/mZF6GxQE+eDBeps7WnkT0Po55TAyzwOxTY="
-    ];
+      substituters = [
+        "https://utdemir.cachix.org"
+        "https://hs-nix-template.cachix.org"
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://cachix.org/api/v1/cache/yuanwang-wf"
+        "https://cachix.org/api/v1/cache/emacs"
+      ];
+      trusted-public-keys = [
+        "utdemir.cachix.org-1:mDgucWXufo3UuSymLuQumqOq1bNeclnnIEkD4fFMhsw="
+        "hs-nix-template.cachix.org-1:/YbjZCrYAw7d9ayLayk7ZhBdTEkR10ZFmFuOq6ZJo4c="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "yuanwang-wf.cachix.org-1:P/RZ5Iuuuv2MYCNCnAsLfPGmgKMKeTwPaJclkrcwx80="
+        "emacs.cachix.org-1:b1SMJNLY/mZF6GxQE+eDBeps7WnkT0Po55TAyzwOxTY="
+      ];
 
-  max-jobs = 8;
-    trusted-users = [ "root" config.my.username ];
+      max-jobs = 8;
+      trusted-users = [ "root" config.my.username ];
     };
     # Avoid unwanted garbage collection when using nix-direnv
     extraOptions = ''
