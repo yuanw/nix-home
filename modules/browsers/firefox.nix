a56d119be5d7fd8eade6c7b15e7f58d3c5722c3c
yuanwang <yuan.wang@workiva.com>
2023-01-23 10:24:13 -0600

    turn nixos wiki search

--- modules/browsers/firefox.nix
+++ modules/browsers/firefox.nix
@@ -50,15 +50,15 @@ in {
                   "''${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
                 definedAliases = [ "@np" ];
               };
-              "NixOS Wiki" = {
-                urls = [{
-                  template =
-                    "https://nixos.wiki/index.php?search={searchTerms}";
-                }];
-                iconUpdateURL = "https://nixos.wiki/favicon.png";
-                updateInterval = 24 * 60 * 60 * 1000; # every day
-                definedAliases = [ "@nw" ];
-              };
+              # "NixOS Wiki" = {
+              #   urls = [{
+              #     template =
+              #       "https://nixos.wiki/index.php?search={searchTerms}";
+              #   }];
+              #   iconUpdateURL = "https://nixos.wiki/favicon.png";
+              #   updateInterval = 24 * 60 * 60 * 1000; # every day
+              #   definedAliases = [ "@nw" ];
+              # };
             };
           };
             # userChrome = ''
