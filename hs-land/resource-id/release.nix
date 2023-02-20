c577f1a97a99c7541bc75ba75dfe6b6612f1eaf9
yuanwang <yuan.wang@workiva.com>
2023-02-20 12:03:06 -0600

    format

--- hs-land/resource-id/release.nix
+++ hs-land/resource-id/release.nix
@@ -1,6 +1,5 @@
-{ mkDerivation, base, base64-bytestring, bytestring, lib
-, optparse-applicative, pretty-terminal, text
-}:
+{ mkDerivation, base, base64-bytestring, bytestring, lib, optparse-applicative
+, pretty-terminal, text }:
 mkDerivation {
   pname = "resource-id";
   version = "0.1.0.0";
@@ -8,16 +7,23 @@ mkDerivation {
   isLibrary = false;
   isExecutable = true;
   libraryHaskellDepends = [
-    base base64-bytestring bytestring optparse-applicative
-    pretty-terminal text
+    base
+    base64-bytestring
+    bytestring
+    optparse-applicative
+    pretty-terminal
+    text
   ];
   executableHaskellDepends = [
-    base base64-bytestring bytestring optparse-applicative
-    pretty-terminal text
+    base
+    base64-bytestring
+    bytestring
+    optparse-applicative
+    pretty-terminal
+    text
   ];
-  testHaskellDepends = [
-    base base64-bytestring bytestring pretty-terminal text
-  ];
+  testHaskellDepends =
+    [ base base64-bytestring bytestring pretty-terminal text ];
   homepage = "https://github.com/yuanwang-wf/resource-id#readme";
   license = lib.licenses.bsd3;
   mainProgram = "rid";
