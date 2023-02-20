7ccda2f1361f6b21e74a384f3fde684118fd1289
yuanwang <yuan.wang@workiva.com>
2023-02-20 14:54:22 -0600

    provide overlay

--- hs-land/overlay.nix
+++ hs-land/overlay.nix
@@ -4,15 +4,12 @@ final: prev:
   haskellPackages = prev.haskellPackages.override {
     overrides = haskellPackagesNew: haskellPackagesOld: rec {
       ws-access-token =
-        haskellPackagesNew.callPackage ./ws-access-token/release.nix
-        { };
+        haskellPackagesNew.callPackage ./ws-access-token/release.nix { };
 
       resource-id =
         haskellPackagesNew.callPackage ./resource-id/release.nix { };
 
-      hi-chew =
-        haskellPackagesNew.callPackage ./hi-chew/release.nix { };
-
+      hi-chew = haskellPackagesNew.callPackage ./hi-chew/release.nix { };
 
     };
   };
