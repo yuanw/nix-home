0f58bafff4240d378f460cfebbf187a2cf68a1fb
yuanwang <yuan.wang@workiva.com>
2023-02-20 11:47:02 -0600

    wip

--- hs-land/flake.nix
+++ hs-land/flake.nix
@@ -3,24 +3,53 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     haskell-flake.url = "github:srid/haskell-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+        flake-root.url = "github:srid/flake-root";
+    mission-control.url = "github:Platonic-Systems/mission-control";
+
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      imports = [ inputs.haskell-flake.flakeModule ];
-      perSystem = { self', pkgs, ... }: {
-        haskellProjects.default = {
-          packages.example.root =
-            ./.; # This value is detected based on .cabal files
+      imports = [ inputs.haskell-flake.flakeModule
+                     inputs.treefmt-nix.flakeModule
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+                ];
+      perSystem = { self', system, lib, config, pkgs, ... }: {
+        haskellProjects.resource-id = {
+          packages.resource-id.root = ./resource-id; # This value is detected based on .cabal files
           # overrides = self: super: { };
-          # devShell = {
-          #  enable = true;  # Enabled by default
-          #  tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
-          #  hlsCheck.enable = true;
-          # };
+          devShell = {
+           enable = true;  # Enabled by default
+           tools = hp: {
+              treefmt = config.treefmt.build.wrapper;
+            } // config.treefmt.build.programs;
+           hlsCheck.enable = true;
+          };
+        };
+         # Auto formatters. This also adds a flake check to ensure that the
+        # source tree was auto formatted.
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
+
+          programs.ormolu.enable = true;
+          programs.nixpkgs-fmt.enable = true;
+          programs.cabal-fmt.enable = true;
+          programs.hlint.enable = true;
+
+          # We use fourmolu
+          programs.ormolu.package = pkgs.haskellPackages.fourmolu;
+          settings.formatter.ormolu = {
+            options = [
+              "--ghc-opt"
+              "-XImportQualifiedPost"
+            ];
+          };
         };
         # haskell-flake doesn't set the default package, but you can do it here.
-        packages.default = self'.packages.example;
+        packages.resource-id = self'.packages.resource-id;
       };
     };
 }
