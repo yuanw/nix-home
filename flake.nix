09d9fb19ac6dd5a26a18a0639199e67600123b5b
yuanw <me@yuanwang.ca>
2023-02-25 07:42:19 -0600

    add specialArgs

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
     };
     nur.url = "github:nix-community/NUR";
     emacs.url = "github:nix-community/emacs-overlay";
+    qmk_firmware = { url = "git+https://github.com/Bastardkb/bastardkb-qmk?submodules=1&shallow=1"; flake = false; };
   };
 
   outputs = inputs@{ self, nixpkgs-stable, nixpkgs, darwin, home-manager, nur
@@ -60,6 +61,7 @@
       mkNixSystem = { modules }:
         nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
+          specialArgs = inputs;
           modules = modules ++ [
             ./nixos_system.nix
             hosts.nixosModule
