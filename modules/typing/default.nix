ced96d5fb713fca85f983b9d01ace42061a5947f
yuanwang <yuan.wang@workiva.com>
2023-02-25 15:05:31 -0600

    try out custom ttyper

--- modules/typing/default.nix
+++ modules/typing/default.nix
@@ -3,11 +3,11 @@
 with lib;
 let
   cfg = config.modules.typing;
-  colemak = pkgs.writeShellScriptBin "colemak" ''
+  adept = pkgs.writeShellScriptBin "adept" ''
     case "$1" in
-      "l1" | "level1" | "1" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level1.txt;;
-      "l2" | "level2" | "2" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level2.txt;;
-      "l3" | "level3" | "3" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level3.txt;;
+      "l1" | "level1" | "1" ) ttyper --language-file level1;;
+      "l2" | "level2" | "2" ) ttyper --language-file level2;;;
+      "l3" | "level3" | "3" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level2.txt;;
       "l4" | "level4" | "4" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level4.txt;;
       "l5" | "level5" | "5" ) gotta-go-fast -h 10 -prw 60 $XDG_CONFIG_HOME/colemak/level5.txt;;
       *) gotta-go-fast -w 60;;
@@ -19,8 +19,8 @@ in {
   config = mkIf cfg.enable {
     home-manager.users.${config.my.username} = {
       home.packages = [
-        # colemak
         # pkgs.haskellPackages.gotta-go-fast
+        adept
         pkgs.ttyper
       ];
       programs = {
@@ -30,12 +30,12 @@ in {
       };
 
       xdg.configFile = {
-        "colemak/level1.txt".source = ./level1.txt;
-        "colemak/level2.txt".source = ./level2.txt;
-        "colemak/level3.txt".source = ./level3.txt;
-        "colemak/level4.txt".source = ./level4.txt;
-        "colemak/level5.txt".source = ./level5.txt;
-        "colemak/level6.txt".source = ./all-words.txt;
+        "ttyper/language/level1.txt".source = ./level1.txt;
+        "ttyper/language/level2.txt".source = ./level2.txt;
+        "ttyper/language/level3.txt".source = ./level3.txt;
+        "ttyper/language/level4.txt".source = ./level4.txt;
+        "ttyper/language/level5.txt".source = ./level5.txt;
+        "ttyper/language/level6.txt".source = ./all-words.txt;
       };
     };
   };
