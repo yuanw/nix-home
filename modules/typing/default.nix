cbb9bc19c64456d54ecc8e9f3cecd7b35eb88e4b
yuanwang <yuan.wang@workiva.com>
2023-02-25 15:43:06 -0600

    wire ttyper language file correctly

--- modules/typing/default.nix
+++ modules/typing/default.nix
@@ -5,11 +5,11 @@ let
   cfg = config.modules.typing;
   adept = pkgs.writeShellScriptBin "adept" ''
     case "$1" in
-      "l1" | "level1" | "1" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/language/level1.txt;;
-      "l2" | "level2" | "2" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/language/level2.txt;;
-      "l3" | "level3" | "3" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/language/level3.txt;;
-      "l4" | "level4" | "4" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/language/level4.txt;;
-      "l5" | "level5" | "5" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/language/level5.txt;;
+      "l1" | "level1" | "1" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/level1;;
+      "l2" | "level2" | "2" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/level2;;
+      "l3" | "level3" | "3" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/level3;;
+      "l4" | "level4" | "4" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/level4;;
+      "l5" | "level5" | "5" ) ttyper --language-file $XDG_CONFIG_HOME/ttyper/level5;;
       *) ttyper;;
     esac
   '';
@@ -30,11 +30,11 @@ in {
       };
 
       xdg.configFile = {
-        "ttyper/language/level1.txt".source = ./level1.txt;
-        "ttyper/language/level2.txt".source = ./level2.txt;
-        "ttyper/language/level3.txt".source = ./level3.txt;
-        "ttyper/language/level4.txt".source = ./level4.txt;
-        "ttyper/language/level5.txt".source = ./level5.txt;
+        "ttyper/level1".source = ./level1.txt;
+        "ttyper/level2".source = ./level2.txt;
+        "ttyper/level3".source = ./level3.txt;
+        "ttyper/level4".source = ./level4.txt;
+        "ttyper/level5".source = ./level5.txt;
       };
     };
   };
