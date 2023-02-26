6d68d958aeead5c25f46607f2c3e54321a8794fb
yuanwang <yuan.wang@workiva.com>
2023-02-26 13:31:14 -0600

    wip

--- hs-land/hi-chew/release.nix
+++ hs-land/hi-chew/release.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, lib, turtle }:
+{ mkDerivation, base, dhall, lib, text, turtle, vector }:
 mkDerivation {
   pname = "hi-chew";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base turtle ];
+  executableHaskellDepends = [ base dhall text turtle vector ];
   license = "unknown";
   mainProgram = "hi-chew";
 }
