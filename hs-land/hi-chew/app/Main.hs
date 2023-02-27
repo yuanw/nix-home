ddb1117a15a7c575d4fd47bcb5662c8568d0a342
yuanwang <yuan.wang@workiva.com>
2023-02-27 08:54:10 -0600

    wip

--- hs-land/hi-chew/app/Main.hs
+++ hs-land/hi-chew/app/Main.hs
@@ -21,22 +21,39 @@ data Project = Project
     { name :: Text
     , repoUrl :: Text
     , checkoutCommand :: Maybe Text
+    , devLog :: Text
+    , prodLog :: Text
     }
     deriving (Generic, Show)
 
 instance FromDhall Project
 
+
 toMap :: Vector Project -> Map.Map Text Project
 toMap = Map.fromList .  map (\p -> (name p, p))  . V.toList
 
 actions :: Project -> Text
-actions project = "gum choose repo" <> if (isJust . checkoutCommand) project then " checkout" else ""
+actions project = "gum choose repo log" <> if (isJust . checkoutCommand) project then " checkout" else ""
+
+openRepo :: Project -> Shell (Either Line Line)
+openRepo project = inshellWithErr (format ("open -a firefox -g "%s)  (repoUrl project)) empty
+
+openLog :: Project -> Shell (Either Line Line)
+openLog project = do
+  projectEnv <- inshell "gum choose dev prod" empty
+  case (lineToText projectEnv) of
+    "dev" -> inshellWithErr ( format ("open -a firefox -g "%s) (devLog project)  ) empty
+    "prod" -> inshellWithErr ( format ("open -a firefox -g "%s) (prodLog project)  ) empty
+
 
 work :: Map.Map Text Project -> Shell (Either Line Line)
 work projectMap = do
-  repo <- (inshell ( foldr (\a b -> b <> " " <>  a)  "gum choose " (Map.keys projectMap ) ) empty)
+  repo <- inshell ( foldr (\a b -> b <> " " <>  a)  "gum choose " (Map.keys projectMap ) ) empty
   action <- inshell (actions . fromJust . flip Map.lookup projectMap . lineToText $ repo) empty
-  inshellWithErr (format ("open -a firefox -g "%s)  ( repoUrl . fromJust . flip Map.lookup projectMap . lineToText $ repo)) empty
+  -- inshellWithErr (format ("open -a firefox -g "%s)  ( repoUrl . fromJust . flip Map.lookup projectMap . lineToText $ repo)) empty
+  case (lineToText action) of
+    "repo" -> openRepo ( fromJust . flip Map.lookup projectMap . lineToText $ repo)
+    "log" -> openLog ( fromJust . flip Map.lookup projectMap . lineToText $ repo)
 
 main :: IO ()
 main = do
