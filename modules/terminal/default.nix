4da5048d9c5085788bbbdf9d10b18ae80a5b9a11
yuanwang <yuan.wang@workiva.com>
2023-03-08 09:53:39 -0600

    add zellij

--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -93,7 +93,7 @@ in {
           sessionVariables = {
             # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux#configuration-variables
             # automatically start tmux
-            ZSH_TMUX_AUTOSTART = "true";
+            # ZSH_TMUX_AUTOSTART = "true";
             ZSH_TMUX_CONFIG = "$XDG_CONFIG_HOME/tmux/tmux.conf";
           };
           shellAliases = {
@@ -163,6 +163,9 @@ in {
               Random r "run-shell 'tat random'" Emacs e "run-shell 'temacs'" ${tmuxMenuSeperator} \
               Exit q detach"
           '';
+        };
+        programs.zellij = {
+          enable = true;
         };
 
         # https://github.com/alacritty/alacritty/blob/master/alacritty.yml#L1
