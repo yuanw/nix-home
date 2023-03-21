d0867e8e0e4286a605aa202843807ecbdcd760a2
yuanwang <yuan.wang@workiva.com>
2023-03-21 12:09:43 -0600

    add repl-flake

--- modules/common.nix
+++ modules/common.nix
@@ -29,7 +29,7 @@
     };
     # Avoid unwanted garbage collection when using nix-direnv
     extraOptions = ''
-      experimental-features = nix-command flakes
+      experimental-features = nix-command flakes repl-flake
       keep-outputs          = true
       keep-derivations      = true
       fallback              = true
