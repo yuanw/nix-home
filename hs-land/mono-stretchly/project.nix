10471f31b760511d248f7f9138ca97fe2447b0a2
yuanwang <yuan.wang@workiva.com>
2023-03-26 13:35:30 -0600

    add project

---
+++ hs-land/mono-stretchly/project.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, base, data-default, lens, lib, monomer, text
+, text-show
+}:
+mkDerivation {
+  pname = "mono-stretchly";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base data-default lens monomer text text-show
+  ];
+  license = lib.licenses.mit;
+  mainProgram = "mono-stretchly";
+}
