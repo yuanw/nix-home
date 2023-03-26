489d6156d0a6efdc63b906626636ca106b6a14c7
yuanwang <yuan.wang@workiva.com>
2023-03-26 15:06:12 -0600

    try how to bundle things

--- hs-land/mono-stretchly/app/Main.hs
+++ hs-land/mono-stretchly/app/Main.hs
@@ -54,9 +54,9 @@ main = do
     config = [
       appWindowTitle "Hello world",
       --how to bundle a png
-      appWindowIcon "/Users/yuanwang/workspace/nix-home/hs-land/mono-stretchly/assets/images/icon.png",
+      appWindowIcon "/Users/yuanwang/workspace/nix-home/hs-land/mono-stretchly/data/assets/images/icon.png",
       appTheme darkTheme,
-      appFontDef "Regular" "/Users/yuanwang/workspace/nix-home/hs-land/mono-stretchly/assets/fonts/Roboto-Regular.ttf",
+      appFontDef "Regular" "/Users/yuanwang/workspace/nix-home/hs-land/mono-stretchly/data/assets/fonts/Roboto-Regular.ttf",
       appInitEvent AppInit
       ]
     model = AppModel 0
