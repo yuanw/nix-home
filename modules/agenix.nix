0d3d00ec64f673b9d5ae0110d62b4c97a25e5137
yuanwang <yuan.wang@workiva.com>
2023-04-16 14:46:17 -0600

    option needs default ?

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -2,24 +2,31 @@
 let
 
   cfg = config.modules.secrets.agenix;
+
 in with lib;
 with builtins; {
 
-  options.modules.secrets.agenix = {
-    options = {
+  options = {
+    modules.secrets.agenix = {
       enable = mkEnableOption "agenix";
-      isDarwin = mkOption { type = types.bool; };
-      isNixOS = mkOption { type = types.bool; };
 
+      isDarwin = mkOption {
+        type = types.bool;
+        default = !pkgs.stdenv.isLinux;
+      };
+      isNixOS = mkOption {
+        type = types.bool;
+        default = pkgs.stdenv.isLinux;
+      };
     };
   };
-
   config = mkIf cfg.enable (mkMerge [
     {
       assertions = [{
-        assertion = let os = [ cfg.isDarwin cfg.isNixOS ]; in count id os <= 1;
+        assertion = let enabled = [ cfg.isDarwin cfg.isNixOS ];
+        in count id enabled <= 1;
         message =
-          "Only one of 'programs.git.difftastic.enable' or 'programs.git.diff-so-fancy.enable' can be set to true at the same time.";
+          "Only one of 'modules.secrets.agenix.isDarwin' or 'modules.secrets.agenix.isNixOS' can be set to true at the same time.";
       }];
 
       environment.systemPackages = with pkgs; [ agenix rage ];
