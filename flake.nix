66e9c9019046e6739bff6fdb058605916d4acc61
yuanwang <yuan.wang@workiva.com>
2023-04-20 12:34:55 -0600

    how

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    inputs.devenv.url = "github:cachix/devenv/latest";
+    devenv.url = "github:cachix/devenv/latest";
     nix-colors.url = "github:misterio77/nix-colors";
     hosts.url = "github:StevenBlack/hosts";
     devshell.url = "github:numtide/devshell";
