8ccc332bc6b4046b9455308f15e26f2f8d77d94f
yuanw <me@yuanwang.ca>
2023-05-14 13:56:06 -0600

    format

--- modules/adguard.nix
+++ modules/adguard.nix
@@ -1,3 +1,56 @@
 { modulesPath, ... }:
 
-{ }
+{
+
+  imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+
+  documentation.enable = false;
+  environment.systemPackages = [ pkgs.bind ];
+  networking.firewall = {
+    allowedTCPPorts = [ 53 ];
+    allowedUDPPortRanges = [{
+      from = 53;
+      to = 53;
+    }];
+  };
+
+  services.adguardhome = {
+    enable = true;
+    openFirewall = true;
+    mutableSettings = true;
+    # corresponds to /var/lib/AdGuardHome/AdGuardHome.yaml
+    settings = {
+      block_auth_min = 10;
+      debug_pprof = true;
+      dns = {
+        bind_hosts = [ "0.0.0.0" ];
+        # query logging
+        querylog_enabled = true;
+        querylog_file_enabled = true;
+        querylog_interval = "24h";
+        querylog_size_memory = 1000; # entries
+        anonymize_client_ip = false; # for now
+
+        # adguard
+        protection_enabled = true;
+        blocking_mode = "default"; # NXDOMAIN
+        filtering_enabled = true;
+
+        # upstream DNS
+        upstream_dns = [
+          "8.8.8.8" # google
+          "8.8.8.4" # google
+        ];
+        # if upstream has any hostnames
+        bootstrap_dns = [ "192.168.1.1" ]; # ask the gateway
+        # caching
+        cache_size = 536870912; # 512 MB
+        cache_ttl_min = 1800; # 30 min
+        cache_optimistic = true; # return stale and then refresh
+      };
+    };
+  };
+
+  system.stateVersion = "22.11";
+
+}
