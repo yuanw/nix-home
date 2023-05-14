b33ad8dc1434bbcd90e6793580000114dbfaefdb
yuanw <me@yuanwang.ca>
2023-05-14 13:33:51 -0600

    commentted out

--- hosts/asche.nix
+++ hosts/asche.nix
@@ -30,45 +30,45 @@
       };
     };
   };
-    services.adguardhome = {
-    enable = true;
-    openFirewall = true;
-    mutableSettings = true;
-    # corresponds to /var/lib/AdGuardHome/AdGuardHome.yaml
-    settings = {
-      block_auth_min = 10;
-      users = [{
-        name = "test";
-        password =
-          "$2b$05$QBELT1NbqYE3/NgBnz6XmutfJ2CU62Zd3n3F/.80KzeE0ZpuIzTke";
-      }];
-      dns = {
-        bind_hosts = [ "0.0.0.0" ];
-        # query logging
-        querylog_enabled = true;
-        querylog_file_enabled = true;
-        querylog_interval = "24h";
-        querylog_size_memory = 1000; # entries
-        anonymize_client_ip = false; # for now
+    # services.adguardhome = {
+    # enable = true;
+    # openFirewall = true;
+    # mutableSettings = true;
+    # # corresponds to /var/lib/AdGuardHome/AdGuardHome.yaml
+    # settings = {
+    #   block_auth_min = 10;
+    #   users = [{
+    #     name = "test";
+    #     password =
+    #       "$2b$05$QBELT1NbqYE3/NgBnz6XmutfJ2CU62Zd3n3F/.80KzeE0ZpuIzTke";
+    #   }];
+    #   dns = {
+    #     bind_hosts = [ "0.0.0.0" ];
+    #     # query logging
+    #     querylog_enabled = true;
+    #     querylog_file_enabled = true;
+    #     querylog_interval = "24h";
+    #     querylog_size_memory = 1000; # entries
+    #     anonymize_client_ip = false; # for now
 
-        # adguard
-        protection_enabled = true;
-        blocking_mode = "default"; # NXDOMAIN
-        filtering_enabled = true;
+    #     # adguard
+    #     protection_enabled = true;
+    #     blocking_mode = "default"; # NXDOMAIN
+    #     filtering_enabled = true;
 
-        # upstream DNS
-        upstream_dns = [
-          "8.8.8.8" # google
-          "8.8.8.4" # google
-        ];
-        # if upstream has any hostnames
-        bootstrap_dns = [ "192.168.1.1" ]; # ask the gateway
-        # caching
-        cache_size = 536870912; # 512 MB
-        cache_ttl_min = 1800; # 30 min
-        cache_optimistic = true; # return stale and then refresh
-      };
-    };
+    #     # upstream DNS
+    #     upstream_dns = [
+    #       "8.8.8.8" # google
+    #       "8.8.8.4" # google
+    #     ];
+    #     # if upstream has any hostnames
+    #     bootstrap_dns = [ "192.168.1.1" ]; # ask the gateway
+    #     # caching
+    #     cache_size = 536870912; # 512 MB
+    #     cache_ttl_min = 1800; # 30 min
+    #     cache_optimistic = true; # return stale and then refresh
+    #   };
+    # };
   };
 
   services.openssh.enable = true;
