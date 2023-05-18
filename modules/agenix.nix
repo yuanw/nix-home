0d2bc1463fb986dfcb73eaba7a9691bbb037ec3c
yuanw <me@yuanwang.ca>
2023-05-17 19:27:05 -0600

    how is this

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -63,11 +63,11 @@ with builtins; {
 
     {
       age = {
-        secrets.secret1 = {
-          file = ../secrets/secret1.age;
-          mode = "770";
-          owner = "yuanwang";
-          group = "admin";
+         secrets.secret1 = {
+           file = ../secrets/secret1.age;
+        #   mode = "770";
+        #   owner = "yuanwang";
+        #   group = "admin";
         };
         secrets.adguard = {
           file = ../secrets/adguard.age;
