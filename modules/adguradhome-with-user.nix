01ff7928148ead6a0a1e4e57ede38af8b327f2af
yuanw <me@yuanwang.ca>
2023-05-19 13:51:15 -0600

    how is this

--- modules/adguradhome-with-user.nix
+++ modules/adguradhome-with-user.nix
@@ -151,22 +151,22 @@ in
       };
 
       preStart = optionalString (cfg.settings != null) ''
-        echo >&2 "setting up adguard..."
+        echo "setting up adguard..."
         user_conf="$(mktemp)"
         conf_merge="$(mktemp)"
 
         if    [ -e "$STATE_DIRECTORY/AdGuardHome.yaml" ] \
            && [ "${toString cfg.mutableSettings}" = "1" ]; then
           # Writing directly to AdGuardHome.yaml results in empty file
-          echo >&2 "if"
+          echo "if"
                  printf '{"users": [ "name": "%s",' "${cfg.user}" >> $user_conf
-          printf 'password" :"%s"]}' "$(cat ${cfg.passwordFile}  | ${pkgs.mkpasswd}/bin/mkpasswd -m bcrypt)" >> $user_conf
+          printf 'password" :%s]}' "$(cat ${cfg.passwordFile}  | ${pkgs.mkpasswd}/bin/mkpasswd -m bcrypt)" >> $user_conf
           ${pkgs.yaml-merge}/bin/yaml-merge "$user_conf" "${configFile}" > "$conf_merge"
           ${pkgs.yaml-merge}/bin/yaml-merge "$STATE_DIRECTORY/AdGuardHome.yaml" "$conf_merge" > "$STATE_DIRECTORY/AdGuardHome.yaml.tmp"
           mv "$STATE_DIRECTORY/AdGuardHome.yaml.tmp" "$STATE_DIRECTORY/AdGuardHome.yaml"
         else
           printf '{"users": [ "name": "%s",' "${cfg.user}" >> $user_conf
-          printf 'password" :"%s"]}' "$(cat ${cfg.passwordFile}  | ${pkgs.mkpasswd}/bin/mkpasswd -m bcrypt)" >> $user_conf
+          printf 'password" :%s]}' "$(cat ${cfg.passwordFile}  | ${pkgs.mkpasswd}/bin/mkpasswd -m bcrypt)" >> $user_conf
           ${pkgs.yaml-merge}/bin/yaml-merge "$user_conf" "${configFile}" > "$conf_merge"
           cp --force "$config_merge" "$STATE_DIRECTORY/AdGuardHome.yaml"
           chmod 600 "$STATE_DIRECTORY/AdGuardHome.yaml"
