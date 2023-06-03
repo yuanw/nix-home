ba71e51fbb5a73567b3da9cb05f0c4fca79f96ac
yuanwang <yuan.wang@workiva.com>
2023-06-03 07:02:32 -0600

    fix helix warning

--- modules/helix.nix
+++ modules/helix.nix
@@ -30,20 +30,23 @@
           };
         };
       };
-      languages = [
+      languages =
+
         {
-          name = "nix";
-          auto-format = true;
-          formatter = { command = "alejandra"; };
-          language-server = { command = "nil"; };
-        }
-        {
-          name = "html";
-          auto-format = false;
-        }
-      ];
-      # themes = {
-      # };
+          language = [
+            {
+              name = "nix";
+              auto-format = true;
+              formatter = { command = "alejandra"; };
+              language-server = { command = "nil"; };
+            }
+            {
+              name = "html";
+              auto-format = false;
+            }
+          ];
+
+        };
     };
   };
 }
