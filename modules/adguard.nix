c6a00275a11f4604be3f3b2800490b20bb9b22a8
yuanw <me@yuanwang.ca>
2023-06-18 14:52:45 -0600

    wip

--- modules/adguard.nix
+++ modules/adguard.nix
@@ -20,25 +20,24 @@
     # 53 for dns
     # 443 for https
     # 853 for DNS-over-TLS, DNS-over-QUIC port
-    allowedTCPPorts = [ 53 443 ];
-    allowedUDPPortRanges = [
-      {
-        from = 53;
-        to = 53;
-      }
-      # {
-      #   from = 853;
-      #   to = 853;
-      # }
-    ];
+    allowedTCPPorts = [ 53 443  ];
+    # 51820 for wireguard
+    allowedUDPPorts = [ 53 51820 ];
+    # allowedUDPPortRanges = [
+    #   {
+    #     from = 53;
+    #     to = 53;
+    #   }
+    #   # {
+    #   #   from = 853;
+    #   #   to = 853;
+    #   # }
+    # ];
   };
  # enable NAT
   networking.nat.enable = true;
   networking.nat.externalInterface = "eth0";
   networking.nat.internalInterfaces = [ "wg0" ];
-  networking.firewall = {
-    allowedUDPPorts = [ 51820 ];
-  };
   services.fail2ban.enable = true;
   # https://nlnetlabs.nl/documentation/unbound/unbound.conf/
   services.unbound = {
@@ -51,7 +50,48 @@
       };
     };
   };
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the server's end of the tunnel interface.
+      ips = [ "10.100.0.1/24" ];
 
+      # The port that WireGuard listens to. Must be accessible by the client.
+      listenPort = 51820;
+
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      # This undoes the above command
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      '';
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = config.age.secrets.wireguard-server-private-key.path;
+
+      peers = [
+        # List of allowed peers.
+        # { # Feel free to give a meaning full name
+        #   # Public key of the peer (not a file path).
+        #   publicKey = "{client public key}";
+        #   # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+        #   allowedIPs = [ "10.100.0.2/32" ];
+        # }
+        # { # John Doe
+        #   publicKey = "{john doe's public key}";
+        #   allowedIPs = [ "10.100.0.3/32" ];
+        # }
+      ];
+    };
+  };
   age = {
     secrets = {
       adguard = {
@@ -70,6 +110,9 @@
         mode = "770";
         path = "/opt/adguradhome/my.crt";
         owner = "adguardhome";
+      };
+      wireguard-server-private-key = {
+        file = ../secrets/wireguard-server-private.age;
       };
 
     };
