f58b8457964229bd9bbf1d80cffb261ec090331f
yuanwang <yuan.wang@workiva.com>
2023-06-19 08:31:14 -0600

    add tree-sitter

--- modules/editor.nix
+++ modules/editor.nix
@@ -83,6 +83,7 @@ with lib; {
           editorconfig-core-c # per-project style config
           # :tools lookup & :lang org +roam
           sqlite
+          tree-sitter
           (tree-sitter.withPlugins (p: [ p.tree-sitter-c p.tree-sitter-java  ]))
           wordnet
           # :lang latex & :lang org (latex previews)
