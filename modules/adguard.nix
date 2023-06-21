0aaad64dd78e6377e61dce78e4694fd7fde99e3b
yuanwang <yuan.wang@workiva.com>
2023-06-21 09:28:19 -0600

    try wg-quick

--- modules/adguard.nix
+++ modules/adguard.nix
@@ -26,9 +26,12 @@
     # ];
   };
   # enable NAT
-  # networking.nat.enable = true;
-  # networking.nat.externalInterface = "eth0";
-  # networking.nat.internalInterfaces = [ "wg0" ];
+  networking.nat = {
+    enable = true;
+    enableIPv6 = true;
+    externalInterface = "eth0";
+    internalInterfaces = [ "wg0" ];
+  };
   # https://nlnetlabs.nl/documentation/unbound/unbound.conf/
   services.unbound = {
     enable = true;
@@ -40,7 +43,40 @@
       };
     };
   };
+  networking.wg-quick.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
+      address = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
+      # The port that WireGuard listens to - recommended that this be changed from default
+      listenPort = 51820;
+      # Path to the server's private key
+      privateKeyFile = config.age.secrets.wireguard-server-private-key.path;
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      postUp = ''
+        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+      '';
 
+      # Undo the above
+      preDown = ''
+        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+      '';
+
+      peers = [
+        { # peer0
+          publicKey = "HVyTUtl0/JpL7jewFimxhb97Aku8uWLBblX9B2/VChs=";
+          allowedIPs = [ "10.0.0.2/32" "fdc9:281f:04d7:9ee9::2/128" ];
+        }
+        # More peers can be added here.
+      ];
+    };
+  };
   # boot.kernel.sysctl."net.ipv4.ip_forward" = "1";
   # boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = "1";
   # networking.dhcpcd = {
