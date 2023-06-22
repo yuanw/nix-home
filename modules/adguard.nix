677d036dddb2526857309f464ca62f93ee5bb082
yuanwang <yuan.wang@workiva.com>
2023-06-22 06:52:04 -0600

    use the right external interface

--- modules/adguard.nix
+++ modules/adguard.nix
@@ -26,11 +26,15 @@
     # ];
   };
   # enable NAT
+  # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/networking/nat.nix#L35
   networking.nat = {
     enable = true;
-    enableIPv6 = true;
-    externalInterface = "eth0";
+    # enableIPv6 = true;
+    # https://www.baeldung.com/linux/network-interface-configure
+    externalInterface = "ens5";
     internalInterfaces = [ "wg0" ];
+    # internalIPs = [ "10.42.0.0/16" ];
+    # internalIPv6s = [ "fd42::/16" ];
   };
   # https://nlnetlabs.nl/documentation/unbound/unbound.conf/
   services.unbound = {
@@ -47,31 +51,27 @@
     # "wg0" is the network interface name. You can name the interface arbitrarily.
     wg0 = {
       # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
-      address = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
+      # address = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
+      address = [ "10.0.0.1/24"
+                ];
       # The port that WireGuard listens to - recommended that this be changed from default
       listenPort = 51820;
       # Path to the server's private key
       privateKeyFile = config.age.secrets.wireguard-server-private-key.path;
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       postUp = ''
-        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+         ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ens5 -j MASQUERADE
       '';
 
       # Undo the above
       preDown = ''
-        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
-        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
-        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o ens5 -j MASQUERADE
       '';
 
       peers = [
         { # peer0
           publicKey = "HVyTUtl0/JpL7jewFimxhb97Aku8uWLBblX9B2/VChs=";
-          allowedIPs = [ "10.0.0.2/32" "fdc9:281f:04d7:9ee9::2/128" ];
+          allowedIPs = [ "10.0.0.2/32" ];
         }
         # More peers can be added here.
       ];
