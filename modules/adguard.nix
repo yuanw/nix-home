40346ebcf664e2e53b98305fac5374e58cb97250
yuanwang <yuan.wang@workiva.com>
2023-06-22 12:22:30 -0600

    wip

--- modules/adguard.nix
+++ modules/adguard.nix
@@ -32,7 +32,7 @@
     # enableIPv6 = true;
     # https://www.baeldung.com/linux/network-interface-configure
     externalInterface = "ens5";
-    internalInterfaces = [ "wg1" ];
+    internalInterfaces = [ "wg0" ];
     # internalIPs = [ "10.42.0.0/16" ];
     # internalIPv6s = [ "fd42::/16" ];
   };
@@ -47,36 +47,72 @@
       };
     };
   };
-  networking.wg-quick.interfaces = {
+    networking.wg-quick.interfaces = {
     # "wg0" is the network interface name. You can name the interface arbitrarily.
-    wg1 = {
+    wg0 = {
       # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
-      # address = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
-      address = [ "10.100.0.1/24"
-                ];
+      address = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
       # The port that WireGuard listens to - recommended that this be changed from default
       listenPort = 51820;
       # Path to the server's private key
-      privateKeyFile = config.age.secrets.wireguard-server-private-key.path;
+      privateKeyFile = "/root/wireguard-keys/privatekey";
+
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       postUp = ''
-         ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ens5 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.0.0.1/24 -o ens5 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -A FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/ip6tables -t nat -A POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o ens5  -j MASQUERADE
       '';
 
       # Undo the above
       preDown = ''
-        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o ens5 -j MASQUERADE
+        ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.0.0.1/24 -o eth0 -j MASQUERADE
+        ${pkgs.iptables}/bin/ip6tables -D FORWARD -i wg0 -j ACCEPT
+        ${pkgs.iptables}/bin/ip6tables -t nat -D POSTROUTING -s fdc9:281f:04d7:9ee9::1/64 -o ens5  -j MASQUERADE
       '';
 
       peers = [
         { # peer0
-          publicKey = "HVyTUtl0/JpL7jewFimxhb97Aku8uWLBblX9B2/VChs=";
-          allowedIPs = [ "10.100.0.2/32" ];
+           publicKey = "HVyTUtl0/JpL7jewFimxhb97Aku8uWLBblX9B2/VChs=";
+          # presharedKeyFile = "/root/wireguard-keys/preshared_from_peer0_key";
+          allowedIPs = [ "10.0.0.2/32" "fdc9:281f:04d7:9ee9::2/128" ];
         }
         # More peers can be added here.
       ];
     };
   };
+  # networking.wg-quick.interfaces = {
+  #   # "wg0" is the network interface name. You can name the interface arbitrarily.
+  #   wg1 = {
+  #     # Determines the IP/IPv6 address and subnet of the client's end of the tunnel interface
+  #     # address = [ "10.0.0.1/24" "fdc9:281f:04d7:9ee9::1/64" ];
+  #     address = [ "10.100.0.1/24"
+  #               ];
+  #     # The port that WireGuard listens to - recommended that this be changed from default
+  #     listenPort = 51820;
+  #     # Path to the server's private key
+  #     privateKeyFile = config.age.secrets.wireguard-server-private-key.path;
+  #     # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+  #     postUp = ''
+  #        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o ens5 -j MASQUERADE
+  #     '';
+
+  #     # Undo the above
+  #     preDown = ''
+  #       ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o ens5 -j MASQUERADE
+  #     '';
+
+  #     peers = [
+  #       { # peer0
+  #         publicKey = "HVyTUtl0/JpL7jewFimxhb97Aku8uWLBblX9B2/VChs=";
+  #         allowedIPs = [ "10.100.0.2/32" ];
+  #       }
+  #       # More peers can be added here.
+  #     ];
+  #   };
+  # };
   # boot.kernel.sysctl."net.ipv4.ip_forward" = "1";
   # boot.kernel.sysctl."net.ipv6.conf.all.forwarding" = "1";
   # networking.dhcpcd = {
