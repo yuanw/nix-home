55cd8dd4a5562a724b75320bff1c203f8aa65384
yuanw <me@yuanwang.ca>
2023-07-08 08:08:29 -0600

    fix ci pipeline

--- flake.nix
+++ flake.nix
@@ -158,7 +158,7 @@
         };
       };
 
-      asche = self.nixosConfigurations.asche.system;
+      asche = self.nixosConfigurations.asche;
       yuanw = self.darwinConfigurations.yuanw.system;
       wf17084 = self.darwinConfigurations.wf17084.system;
       wk01174 = self.darwinConfigurations.WK01174.system;
