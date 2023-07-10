b8b722c6bc4efebccd76efcc8b36ac86c6d692ec
yuanwang <yuan.wang@workiva.com>
2023-07-10 15:40:07 -0600

    wip

--- modules/wm/yabai.nix
+++ modules/wm/yabai.nix
@@ -154,6 +154,9 @@ in
 
       extraConfig = ''
         # rules
+        yabai -m signal --add event=window_focused action="sketchybar --trigger window_focus"
+        yabai -m signal --add event=window_created action="sketchybar --trigger windows_on_spaces"
+        yabai -m signal --add event=window_destroyed action="sketchybar --trigger windows_on_spaces"
         yabai -m rule --add app='System Preferences' manage=off
         # Any other arbitrary config here
       '';
