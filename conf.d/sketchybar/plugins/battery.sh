19aa79e7cf81836578fca8a0af029ff4d6fc8d63
yuanwang <yuan.wang@workiva.com>
2023-07-11 08:08:19 -0600

    fix script

--- conf.d/sketchybar/plugins/battery.sh
+++ conf.d/sketchybar/plugins/battery.sh
@@ -4,7 +4,7 @@ source "$CONFIG_DIR/icons.sh"
 source "$CONFIG_DIR/colors.sh"
 
 BATTERY_INFO="$(pmset -g batt)"
-PERCENTAGE=$(echo "$BATTERY_INFO" | grep -Eo "\d+%" | cut -d% -f1)
+PERCENTAGE=$(echo "$BATTERY_INFO" | rg -o "\d+%" | cut -d% -f1)
 CHARGING=$(echo "$BATTERY_INFO" | grep 'AC Power')
 
 if [ $PERCENTAGE = "" ]; then
