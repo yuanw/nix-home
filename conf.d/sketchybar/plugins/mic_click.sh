da4164bc35474544e226aa7d0cdd1fd866518f97
yuanwang <yuan.wang@workiva.com>
2023-07-11 14:12:27 -0600

    fix icon in both places

--- conf.d/sketchybar/plugins/mic_click.sh
+++ conf.d/sketchybar/plugins/mic_click.sh
@@ -4,8 +4,8 @@ MIC_VOLUME=$(osascript -e 'input volume of (get volume settings)')
 
 if [[ $MIC_VOLUME -eq 0 ]]; then
   osascript -e 'set volume input volume 50'
-  sketchybar -m --set mic icon=
+  sketchybar -m --set mic icon=􀊰
 elif [[ $MIC_VOLUME -gt 0 ]]; then
   osascript -e 'set volume input volume 0'
-  sketchybar -m --set mic icon=
+  sketchybar -m --set mic icon=􀊲
 fi
