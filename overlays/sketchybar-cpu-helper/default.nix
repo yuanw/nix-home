2c9e4b13374cd1318ad818ed7b87c903e403e077
yuanwang <yuan.wang@workiva.com>
2023-07-15 08:09:37 -0600

    wip

--- overlays/sketchybar-cpu-helper/default.nix
+++ overlays/sketchybar-cpu-helper/default.nix
@@ -1,8 +1,6 @@
 { lib,  pkgs }:
 let
 stdenv = pkgs.clangStdenv;
-  # https://stackoverflow.com/questions/51161225/how-can-i-make-macos-frameworks-available-to-clang-in-a-nix-environment
-  frameworks = pkgs.darwin.apple_sdk.frameworks;
 in
 stdenv.mkDerivation rec {
   pname = "cpu-helper";
@@ -10,8 +8,6 @@ stdenv.mkDerivation rec {
 
   src = ./helper;
 
-  # buildInputs = [  ];
-  # https://github.com/Homebrew/homebrew-core/blob/2f089a6f55a7496248be39f4d883094fd79cbdc4/Formula/ical-buddy.rb#L24
   buildPhase = ''
     make
   '';
@@ -21,8 +17,8 @@ stdenv.mkDerivation rec {
   '';
 
   meta = with lib; {
-    description = "Get events and tasks from the macOS calendar database";
-    homepage = "https://hasseg.org/icalBuddy/";
+    description = "cpu helper";
+    homepage = "https://github.com/FelixKratz/dotfiles/tree/master/.config/sketchybar/helper";
     platforms = platforms.darwin;
     license = licenses.mit;
   };
