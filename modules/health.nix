0d54b5bc14f37538c66f81a63ab6098426bd5e94
yuanwang <yuan.wang@workiva.com>
2023-07-18 11:17:05 -0600

    add log

--- modules/health.nix
+++ modules/health.nix
@@ -1,19 +1,27 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-let homeDir = config.my.homeDirectory;
-    cfg = config.modules.health;
-in {
+let
+  homeDir = config.my.homeDirectory;
+  cfg = config.modules.health;
+in
+{
   options.modules.health = { enable = mkEnableOption "health"; };
 
   config = mkIf cfg.enable {
     launchd.user.agents.stretchly = {
       path = [ config.environment.systemPath ];
-      serviceConfig.ProgramArguments =
-        [
-          "${pkgs.haskellPackages.mono-stretchly}/bin/mono-strecthly" ];
-      serviceConfig.RunAtLoad = false;
-      serviceConfig.StartCalendarInterval = [ {Minute = 15;} ];
+
+      serviceConfig = {
+        StandardOutPath = "/tmp/strecthly.log";
+        StandardErrorPath = "/tmp/strecthly.log";
+        ProgramArguments =
+          [
+            "${pkgs.haskellPackages.mono-stretchly}/bin/mono-strecthly"
+          ];
+        RunAtLoad = false;
+        StartCalendarInterval = [{ Minute = 15; }];
+      };
       # serviceConfig.UserName = cfg.user;
     };
     home-manager.users.${config.my.username} = {
