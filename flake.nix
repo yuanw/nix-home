b192cd83a3ebdc3532f622eaa7a16233e9995736
yuanwang <yuan.wang@workiva.com>
2023-07-30 07:20:29 -0600

    config treefmt

--- flake.nix
+++ flake.nix
@@ -9,6 +9,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     devenv.url = "github:cachix/devenv/latest";
+    flake-parts = {
+    url = "github:hercules-ci/flake-parts";
+    inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     nix-colors.url = "github:misterio77/nix-colors";
     hosts.url = "github:StevenBlack/hosts";
     flake-utils.url = "github:numtide/flake-utils";
@@ -27,147 +31,170 @@
       inputs.darwin.follows = "darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+     treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
-  outputs =
-    inputs@{ self
-    , nixpkgs-stable
-    , nixpkgs
-    , darwin
-    , home-manager
-    , nur
-    , emacs
-    , flake-utils
-    , hosts
-    , agenix
-    , nix-colors
-    , astro-nvim
-    , ...
-    }:
-    let
-      inherit (flake-utils.lib) eachDefaultSystem eachSystem;
-      overlays = [
-        emacs.overlay
-        nur.overlay
-        agenix.overlays.default
-        (final: prev: {
-          stable = nixpkgs-stable.legacyPackages.${prev.system};
-          mesa = nixpkgs-stable.legacyPackages.${prev.system}.mesa;
-          # use this variant if unfree packages are needed:
-          # unstable = import nixpkgs-unstable {
-          #   inherit system;
-          #   config.allowUnfree = true;
-          # };
 
-        })
-        (final: prev: {
-          reiryoku-firmware = inputs.reiryoku.packages.${prev.system}.firmware;
-          devenv = inputs.devenv.packages.${prev.system}.devenv;
-        })
-        (import ./hs-land/overlay.nix)
-        (import ./overlays)
+  inputs @ { flake-parts, systems, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+  "aarch64-darwin"
+  "aarch64-linux"
+  "x86_64-darwin"
+  "x86_64-linux"
+];
+      imports = [
+        ./devshell.nix
+        ./docs
+        ./homeConfigurations
+        ./nixosConfigurations
+        ./nixosModules
+        ./packages
+        inputs.treefmt-nix.flakeModule
       ];
+      perSystem.treefmt.imports = [ ./treefmt.nix ];
+    };
 
-      # https://github.com/shaunsingh/nix-darwin-dotfiles/blob/main/flake.nix
-      mkSystemConfig =
-        { system
-        , modules
-        , isDarwin ? nixpkgs.lib.hasSuffix "-darwin" system
-        , isNixOS ? !isDarwin
-        , ...
-        }:
-        (if isDarwin then
-          darwin.lib.darwinSystem
-        else
-          nixpkgs.lib.nixosSystem) {
-          inherit system;
-          specialArgs = { inherit nix-colors isNixOS isDarwin astro-nvim; };
-          modules = modules ++ [{ nixpkgs.overlays = overlays; } ./modules]
-            ++ (if isDarwin then
-            ([
-              agenix.darwinModules.age
-              home-manager.darwinModules.home-manager
-              ./macintosh.nix
-            ]) else
-            ([
-              ./nixos_system.nix
-              hosts.nixosModule
-              {
-                networking.stevenBlackHosts = {
-                  enable = true;
-                  blockFakenews = true;
-                  blockGambling = true;
-                  blockPorn = true;
-                  blockSocial = false;
-                };
-              }
-              agenix.nixosModules.age
-              home-manager.nixosModules.home-manager
-            ]));
+  # outputs =
+  #   inputs@{ self
+  #   , nixpkgs-stable
+  #   , nixpkgs
+  #   , darwin
+  #   , home-manager
+  #   , nur
+  #   , emacs
+  #   , flake-utils
+  #   , hosts
+  #   , agenix
+  #   , nix-colors
+  #   , astro-nvim
+  #   , ...
+  #   }:
+  #   let
+  #     inherit (flake-utils.lib) eachDefaultSystem eachSystem;
+  #     overlays = [
+  #       emacs.overlay
+  #       nur.overlay
+  #       agenix.overlays.default
+  #       (final: prev: {
+  #         stable = nixpkgs-stable.legacyPackages.${prev.system};
+  #         mesa = nixpkgs-stable.legacyPackages.${prev.system}.mesa;
+  #         # use this variant if unfree packages are needed:
+  #         # unstable = import nixpkgs-unstable {
+  #         #   inherit system;
+  #         #   config.allowUnfree = true;
+  #         # };
 
-        };
-    in
-    {
-      nixosConfigurations.aws = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          { nixpkgs.overlays = [ agenix.overlays.default ]; }
-          agenix.nixosModules.age
-          ./modules/aws.nix
-          ./modules/agenix.nix
-        ];
-      };
+  #       })
+  #       (final: prev: {
+  #         reiryoku-firmware = inputs.reiryoku.packages.${prev.system}.firmware;
+  #         devenv = inputs.devenv.packages.${prev.system}.devenv;
+  #       })
+  #       (import ./hs-land/overlay.nix)
+  #       (import ./overlays)
+  #     ];
 
-      nixosConfigurations.adguard = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          { nixpkgs.overlays = [ agenix.overlays.default ]; }
-          agenix.nixosModules.age
-          ./modules/aws.nix
-          ./modules/adguradhome-with-user.nix
-          ./modules/adguard.nix
-          ./modules/agenix.nix
-        ];
-      };
+  #     # https://github.com/shaunsingh/nix-darwin-dotfiles/blob/main/flake.nix
+  #     mkSystemConfig =
+  #       { system
+  #       , modules
+  #       , isDarwin ? nixpkgs.lib.hasSuffix "-darwin" system
+  #       , isNixOS ? !isDarwin
+  #       , ...
+  #       }:
+  #       (if isDarwin then
+  #         darwin.lib.darwinSystem
+  #       else
+  #         nixpkgs.lib.nixosSystem) {
+  #         inherit system;
+  #         specialArgs = { inherit nix-colors isNixOS isDarwin astro-nvim; };
+  #         modules = modules ++ [{ nixpkgs.overlays = overlays; } ./modules]
+  #           ++ (if isDarwin then
+  #           ([
+  #             agenix.darwinModules.age
+  #             home-manager.darwinModules.home-manager
+  #             ./macintosh.nix
+  #           ]) else
+  #           ([
+  #             ./nixos_system.nix
+  #             hosts.nixosModule
+  #             {
+  #               networking.stevenBlackHosts = {
+  #                 enable = true;
+  #                 blockFakenews = true;
+  #                 blockGambling = true;
+  #                 blockPorn = true;
+  #                 blockSocial = false;
+  #               };
+  #             }
+  #             agenix.nixosModules.age
+  #             home-manager.nixosModules.home-manager
+  #           ]));
 
-      nixosConfigurations.asche = mkSystemConfig {
-        system = "x86_64-linux";
-        modules = [ ./machines/asche/configuration.nix ./hosts/asche.nix ];
-      };
+  #       };
+  #   in
+  #   {
+  #     nixosConfigurations.aws = nixpkgs.lib.nixosSystem {
+  #       system = "x86_64-linux";
+  #       modules = [
+  #         { nixpkgs.overlays = [ agenix.overlays.default ]; }
+  #         agenix.nixosModules.age
+  #         ./modules/aws.nix
+  #         ./modules/agenix.nix
+  #       ];
+  #     };
 
-      darwinConfigurations = {
-        yuanw = mkSystemConfig {
-          system = "x86_64-darwin";
-          modules = [ ./hosts/yuan-mac.nix ];
-        };
-        WK01174 = mkSystemConfig {
-          system = "aarch64-darwin";
-          modules = [ ./hosts/wk01174.nix ];
-        };
-        wf17084 = mkSystemConfig {
-          system = "x86_64-darwin";
-          modules = [ ./hosts/wf17084.nix ];
-        };
-      };
+  #     nixosConfigurations.adguard = nixpkgs.lib.nixosSystem {
+  #       system = "x86_64-linux";
+  #       modules = [
+  #         { nixpkgs.overlays = [ agenix.overlays.default ]; }
+  #         agenix.nixosModules.age
+  #         ./modules/aws.nix
+  #         ./modules/adguradhome-with-user.nix
+  #         ./modules/adguard.nix
+  #         ./modules/agenix.nix
+  #       ];
+  #     };
 
-      asche = self.nixosConfigurations.asche.config.system.build.toplevel;
-      yuanw = self.darwinConfigurations.yuanw.system;
-      wf17084 = self.darwinConfigurations.wf17084.system;
-      wk01174 = self.darwinConfigurations.WK01174.system;
-      adguard = self.nixosConfigurations.adguard.system;
-      aws = self.nixosConfigurations.aws.system;
+  #     nixosConfigurations.asche = mkSystemConfig {
+  #       system = "x86_64-linux";
+  #       modules = [ ./machines/asche/configuration.nix ./hosts/asche.nix ];
+  #     };
 
-    } // eachDefaultSystem (system:
-    let pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          awscli
-          lego
-          terraform
-          nixfmt
-          treefmt
-        ];
-      };
-    });
+  #     darwinConfigurations = {
+  #       yuanw = mkSystemConfig {
+  #         system = "x86_64-darwin";
+  #         modules = [ ./hosts/yuan-mac.nix ];
+  #       };
+  #       WK01174 = mkSystemConfig {
+  #         system = "aarch64-darwin";
+  #         modules = [ ./hosts/wk01174.nix ];
+  #       };
+  #       wf17084 = mkSystemConfig {
+  #         system = "x86_64-darwin";
+  #         modules = [ ./hosts/wf17084.nix ];
+  #       };
+  #     };
+
+  #     asche = self.nixosConfigurations.asche.config.system.build.toplevel;
+  #     yuanw = self.darwinConfigurations.yuanw.system;
+  #     wf17084 = self.darwinConfigurations.wf17084.system;
+  #     wk01174 = self.darwinConfigurations.WK01174.system;
+  #     adguard = self.nixosConfigurations.adguard.system;
+  #     aws = self.nixosConfigurations.aws.system;
+
+  #   } // eachDefaultSystem (system:
+  #   let pkgs = import nixpkgs { inherit system; };
+  #   in {
+  #     devShells.default = pkgs.mkShell {
+  #       buildInputs = with pkgs; [
+  #         awscli
+  #         lego
+  #         terraform
+  #         nixfmt
+  #         treefmt
+  #       ];
+  #     };
+  #   });
 }
