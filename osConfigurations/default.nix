a3fc05316a7f9b58606d3f99a777acd32fd6ec82
yuanwang <yuan.wang@workiva.com>
2023-07-31 13:12:39 -0600

    use mkSystemConfig

--- osConfigurations/default.nix
+++ osConfigurations/default.nix
@@ -13,7 +13,7 @@
       linux = { ... }: { };
       # nix-darwin specific configuration
       darwin.imports = [
-        ../macintosh.nix
+        # ../macintosh.nix
       ];
     };
     nixosConfigurations = {
@@ -50,9 +50,10 @@
         system = "x86_64-darwin";
         modules = [ ../hosts/yuan-mac.nix ];
       };
-      WK01174 = self.nixos-flake.lib.mkMacosSystem "aarch64-darwin" {
-        imports = [
-          self.nixosModules.darwin
+      WK01174 = self.nixos-flake.lib.mkSystemConfig  "aarch64-darwin" {
+        modules = [
+         ../hosts/wk01174.nix
+          # self.nixosModules.darwin
         ];
       };
       # WK01174 =   inputs.darwin.lib.darwinSystem {
--- systemModule.nix
+++ systemModule.nix
@@ -9,6 +9,29 @@ let
       rosettaPkgs = import inputs.nixpkgs { system = "x86_64-darwin"; };
     };
   };
+   nix-colors  = inputs.nix-colors;
+   astro-nvim  = inputs.astro-nvim  ;
+      overlays = [
+        inputs.emacs.overlay
+         inputs.nur.overlay
+         inputs.agenix.overlays.default
+        (final: prev: {
+          stable =  inputs. nixpkgs-stable.legacyPackages.${prev.system};
+          mesa =  inputs. nixpkgs-stable.legacyPackages.${prev.system}.mesa;
+          # use this variant if unfree packages are needed:
+          # unstable = import nixpkgs-unstable {
+          #   inherit system;
+          #   config.allowUnfree = true;
+          # };
+
+        })
+        (final: prev: {
+          reiryoku-firmware = inputs.reiryoku.packages.${prev.system}.firmware;
+          devenv = inputs.devenv.packages.${prev.system}.devenv;
+        })
+        (import ./hs-land/overlay.nix)
+        (import ./overlays)
+      ];
 in
 {
 
@@ -16,7 +39,43 @@ in
     flake = {
       nixos-flake.lib = rec {
         inherit specialArgsFor;
+ mkSystemConfig =
+        { system
+        , modules
+        , isDarwin ? inputs.nixpkgs.lib.hasSuffix "-darwin" system
+        , isNixOS ? !isDarwin
+        , ...
+        }:
+        (if isDarwin then
+          inputs.darwin.lib.darwinSystem
+        else
+          inputs.nixpkgs.lib.nixosSystem) {
+          inherit system;
+          specialArgs = { inherit nix-colors isNixOS isDarwin astro-nvim; };
+          modules = modules ++ [{ nixpkgs.overlays = overlays; } ./modules]
+            ++ (if isDarwin then
+            ([
+               inputs.agenix.darwinModules.age
+               inputs.home-manager.darwinModules.home-manager
+              ./macintosh.nix
+            ]) else
+            ([
+              ./nixos_system.nix
+               inputs.hosts.nixosModule
+              {
+                networking.stevenBlackHosts = {
+                  enable = true;
+                  blockFakenews = true;
+                  blockGambling = true;
+                  blockPorn = true;
+                  blockSocial = false;
+                };
+              }
+              inputs. agenix.nixosModules.age
+               inputs.home-manager.nixosModules.home-manager
+            ]));
 
+        };
         mkLinuxSystem = system: mod: inputs.nixpkgs.lib.nixosSystem {
           inherit system;
           # Arguments to pass to all modules.
