34ba25995183f93ba44952ce8e44cb131308d8a3
yuanwang <yuan.wang@workiva.com>
2023-07-31 13:18:35 -0600

    fix func apply error

--- osConfigurations/default.nix
+++ osConfigurations/default.nix
@@ -2,20 +2,20 @@
 {
   flake = {
     # All nixos/nix-darwin configurations are kept here.
-    nixosModules = {
-      # Common nixos/nix-darwin configuration shared between Linux and macOS.
-      common = { pkgs, ... }: {
-        environment.systemPackages = with pkgs; [
-          hello
-        ];
-      };
-      # NixOS specific configuration
-      linux = { ... }: { };
-      # nix-darwin specific configuration
-      darwin.imports = [
-        # ../macintosh.nix
-      ];
-    };
+    # nixosModules = {
+    #   # Common nixos/nix-darwin configuration shared between Linux and macOS.
+    #   common = { pkgs, ... }: {
+    #     environment.systemPackages = with pkgs; [
+    #       hello
+    #     ];
+    #   };
+    #   # NixOS specific configuration
+    #   linux = { ... }: { };
+    #   # nix-darwin specific configuration
+    #   darwin.imports = [
+    #     # ../macintosh.nix
+    #   ];
+    # };
     nixosConfigurations = {
       aws = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -50,7 +50,8 @@
         system = "x86_64-darwin";
         modules = [ ../hosts/yuan-mac.nix ];
       };
-      WK01174 = self.nixos-flake.lib.mkSystemConfig "aarch64-darwin" {
+      WK01174 = self.nixos-flake.lib.mkSystemConfig {
+        system = "aarch64-darwin";
         modules = [
           ../hosts/wk01174.nix
           # self.nixosModules.darwin
