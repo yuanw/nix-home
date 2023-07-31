919b06972d6de218214e68ab9902640fba39583b
yuanwang <yuan.wang@workiva.com>
2023-07-31 14:41:41 -0600

    how is this

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,11 @@
-{ lib, isDarwin, isNixOS, ... }:
-
+{ ... }:
 {
-  imports = import ./modules.nix { inherit lib isDarwin isNixOS; };
-
+  flake.nixosModules = {
+    common = ./common.nix;
+    # desktop = ./desktop.nix;
+    # gnome = ./gnome.nix;
+    # gotosocial = ./gotosocial.nix;
+    # nix-remote-builders = ./nix-remote-builders.nix;
+    # server = ./server.nix;
+  };
 }
