450d36cb0e515991ff5aa092beec3908850b279e
yuanwang <yuan.wang@workiva.com>
2023-07-31 13:14:50 -0600

    remove old func

--- systemModule.nix
+++ systemModule.nix
@@ -76,22 +76,6 @@ in
               ]));
 
           };
-        mkLinuxSystem = system: mod: inputs.nixpkgs.lib.nixosSystem {
-          inherit system;
-          # Arguments to pass to all modules.
-          specialArgs = specialArgsFor.nixos;
-          modules = [ mod ];
-        };
-
-        mkMacosSystem = system: mod: inputs.nix-darwin.lib.darwinSystem {
-          inherit system;
-          specialArgs = specialArgsFor.darwin;
-          modules = [
-            inputs.agenix.darwinModules.age
-            inputs.home-manager.darwinModules.home-manager
-            mod
-          ];
-        };
 
       };
     };
