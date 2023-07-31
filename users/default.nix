9a93ef2e1335b3515b117f675937dc8125671111
yuanwang <yuan.wang@workiva.com>
2023-07-31 10:58:12 -0600

    add

---
+++ users/default.nix
@@ -0,0 +1,44 @@
+{ lib, ... }:
+let
+  userSubmodule = lib.types.submodule {
+    options = {
+      name = lib.mkOption {
+        type = lib.types.str;
+      };
+      email = lib.mkOption {
+        type = lib.types.str;
+      };
+      sshKeys = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        description = ''
+          SSH public keys
+        '';
+      };
+    };
+  };
+  peopleSubmodule = lib.types.submodule {
+    options = {
+      users = lib.mkOption {
+        type = lib.types.attrsOf userSubmodule;
+      };
+      myself = lib.mkOption {
+        type = lib.types.str;
+        description = ''
+          The name of the user that represents myself.
+
+          Admin user in all contexts.
+        '';
+      };
+    };
+  };
+in
+{
+  options = {
+    people = lib.mkOption {
+      type = peopleSubmodule;
+    };
+  };
+  config = {
+    people = import ./config.nix;
+  };
+}
