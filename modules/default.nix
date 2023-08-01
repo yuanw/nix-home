fddad2230a8b6e84727ea73469a592a922e5d7b1
yuanwang <yuan.wang@workiva.com>
2023-08-01 08:57:26 -0600

    how is this

--- modules/default.nix
+++ modules/default.nix
@@ -27,16 +27,20 @@
       ./wm/yabai.nix
       ../macintosh.nix
     ];
-    wk011474 = {flake, ...}: {
-     flake.config.my = {
-    username = "yuanwang";
-    name = "Yuan Wang";
-    email = "yuan.wang@workiva.com";
-    hostname = "WK01174";
-    gpgKey = "19AD3F6B1A5BF3BF";
-    homeDirectory = "/Users/yuanwang";
-    };
-    };
+    wk011474.imports = [
+      ({ flake, ... }: {
+        flake.config.my = {
+          username = "yuanwang";
+          name = "Yuan Wang";
+          email = "yuan.wang@workiva.com";
+          hostname = "WK01174";
+          gpgKey = "19AD3F6B1A5BF3BF";
+          homeDirectory = "/Users/yuanwang";
+        };
+      })
+
+    ];
+
 
     # desktop = ./desktop.nix;
     # gnome = ./gnome.nix;
--- users/default.nix
+++ users/default.nix
@@ -1,6 +1,6 @@
 { lib, ... }:
 let
-   mkOptStr = value:
+  mkOptStr = value:
     lib.mkOption {
       type = with lib.types; uniq str;
       default = value;
@@ -8,60 +8,15 @@ let
 
   # copied from https://github.com/cmacrae/config
   mailAddr = name: domain: "${name}@${domain}";
-
-  mySubmodule = lib.types.submodule {
-    options = {
-      username = lib.mkOption {
-        type = lib.types.str;
-      };
-      email = lib.mkOption {
-        type = lib.types.str;
-      };
-      gpgKey = lib.mkOption {
-        type = lib.types.str;
-        description = ''
-          GPP key
-        '';
-      };
-      homeDirectory = lib.mkOption {
-        type = lib.types.str;
-        description = ''
-        '';
-      };
-      font = lib.mkOption {
-        type = lib.types.str;
-        description = ''
-        '';
-        default = "PragmataPro";
-      };
-      hostname = lib.mkOption {
-        type = lib.types.str;
-        description = ''
-        '';
-      };
-      name = lib.mkOption {
-        type = lib.types.str;
-        description = ''
-        '';
-      };
-
-      sshKeys = lib.mkOption {
-        type = lib.types.listOf lib.types.str;
-        description = ''
-          SSH public keys
-        '';
-      };
-    };
-  };
 in
 {
   options.my = {
-      username = mkOptStr "yuanwang";
-      name = mkOptStr "Yuan Wang";
-      email = mkOptStr (mailAddr "me" "yuanwang.ca");
-      hostname = mkOptStr "yuan-mac";
-      gpgKey = mkOptStr "BF2ADAA2A98F45E7";
-      homeDirectory = mkOptStr "/Users/yuanwang";
-      font = mkOptStr "PragmataPro";
+    username = mkOptStr "yuanwang";
+    name = mkOptStr "Yuan Wang";
+    email = mkOptStr (mailAddr "me" "yuanwang.ca");
+    hostname = mkOptStr "yuan-mac";
+    gpgKey = mkOptStr "BF2ADAA2A98F45E7";
+    homeDirectory = mkOptStr "/Users/yuanwang";
+    font = mkOptStr "PragmataPro";
   };
- }
+}
