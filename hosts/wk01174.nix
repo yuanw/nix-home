c2758f31531d362eeda8adc8326f8e62ab115271
yuanwang <yuan.wang@workiva.com>
2023-08-01 07:08:43 -0600

    how is this

--- hosts/wk01174.nix
+++ hosts/wk01174.nix
@@ -1,6 +1,14 @@
 { pkgs, flake, ... }: {
 
-   environment.systemPath = [
+  flake.config.my = {
+    username = "yuanwang";
+    name = "Yuan Wang";
+    email = "yuan.wang@workiva.com";
+    hostname = "WK01174";
+    gpgKey = "19AD3F6B1A5BF3BF";
+    homeDirectory = "/Users/yuanwang";
+  };
+  environment.systemPath = [
     "/opt/homebrew/bin"
     "/opt/homebrew/sbin"
   ];
