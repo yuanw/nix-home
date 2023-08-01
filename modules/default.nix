0ea1986d6426f64a1721e7e0dc461637f489f9ff
yuanwang <yuan.wang@workiva.com>
2023-08-01 09:19:00 -0600

    move things

--- modules/default.nix
+++ modules/default.nix
@@ -26,16 +26,8 @@
       ./brew.nix
       ./wm/yabai.nix
       ../macintosh.nix
-      ({  ... }: {
-        my = {
-          username = "yuanwang";
-          name = "Yuan Wang";
-          email = "yuan.wang@workiva.com";
-          hostname = "WK01174";
-          gpgKey = "19AD3F6B1A5BF3BF";
-          homeDirectory = "/Users/yuanwang";
-        };
-      })
+      # ({  ... }: {
+      #        })
     ];
 
 
