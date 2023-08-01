78ced6d6e2dd3c8cd160e100f63c917dc946c2b8
yuanwang <yuan.wang@workiva.com>
2023-08-01 10:04:16 -0600

    delete some old files

--- users/config.nix
+++
@@ -1,8 +0,0 @@
-{
-  username = "yuanwang";
-  name = "Yuan Wang";
-  email = "yuan.wang@workiva.com";
-  hostname = "WK01174";
-  gpgKey = "19AD3F6B1A5BF3BF";
-  homeDirectory = "/Users/yuanwang";
-}
