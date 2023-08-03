91ea270b9137f2f1fa5b98ba084e257f0e5eba27
yuanwang <yuan.wang@workiva.com>
2023-08-03 08:53:21 -0600

    how is this

--- devshell.nix
+++ devshell.nix
@@ -7,6 +7,7 @@
       # See https://haskell.flake.page/devshell#composing-devshells
       inputsFrom = [
         config.treefmt.build.devShell
+        config.pre-commit.devShell
       ];
     };
   };
