581f03c4e05eeed1aa5c16a8c004e65e7aeef8dc
yuanwang <yuan.wang@workiva.com>
2023-08-04 12:06:37 -0600

    comment out adguard and unbound for now

--- hosts/asche.nix
+++ hosts/asche.nix
@@ -30,55 +30,55 @@
       };
     };
   };
-  services.adguardhome-with-user = {
-    # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/networking/adguardhome.nix#L135
-    # enable = true;
-    # openFirewall = true;
-    mutableSettings = false;
-    user = "test";
-    passwordFile = config.age.secrets.adguard.path;
-    # corresponds to /var/lib/AdGuardHome/AdGuardHome.yaml
-    settings = {
-      block_auth_min = 10;
-      debug_pprof = true;
-      dns = {
-        bind_hosts = [ "0.0.0.0" ];
-        # query logging
-        querylog_enabled = true;
-        querylog_file_enabled = true;
-        querylog_interval = "24h";
-        querylog_size_memory = 1000; # entries
-        anonymize_client_ip = false; # for now
+  # services.adguardhome-with-user = {
+  #   # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/networking/adguardhome.nix#L135
+  #   # enable = true;
+  #   # openFirewall = true;
+  #   mutableSettings = false;
+  #   user = "test";
+  #   passwordFile = config.age.secrets.adguard.path;
+  #   # corresponds to /var/lib/AdGuardHome/AdGuardHome.yaml
+  #   settings = {
+  #     block_auth_min = 10;
+  #     debug_pprof = true;
+  #     dns = {
+  #       bind_hosts = [ "0.0.0.0" ];
+  #       # query logging
+  #       querylog_enabled = true;
+  #       querylog_file_enabled = true;
+  #       querylog_interval = "24h";
+  #       querylog_size_memory = 1000; # entries
+  #       anonymize_client_ip = false; # for now
 
-        # adguard
-        protection_enabled = true;
-        blocking_mode = "default"; # NXDOMAIN
-        filtering_enabled = true;
+  #       # adguard
+  #       protection_enabled = true;
+  #       blocking_mode = "default"; # NXDOMAIN
+  #       filtering_enabled = true;
 
-        # upstream DNS
-        upstream_dns = [
-          "8.8.8.8" # google
-          "8.8.8.4" # google
-        ];
-        # if upstream has any hostnames
-        bootstrap_dns = [ "192.168.1.1" ]; # ask the gateway
-        # caching
-        cache_size = 536870912; # 512 MB
-        cache_ttl_min = 1800; # 30 min
-        cache_optimistic = true; # return stale and then refresh
-      };
-    };
-  };
-  services.unbound = {
-    # enable = true;
-    settings = {
-      server = {
-        hide-identity = "yes";
-        hide-version = "yes";
-        port = 5335;
-      };
-    };
-  };
+  #       # upstream DNS
+  #       upstream_dns = [
+  #         "8.8.8.8" # google
+  #         "8.8.8.4" # google
+  #       ];
+  #       # if upstream has any hostnames
+  #       bootstrap_dns = [ "192.168.1.1" ]; # ask the gateway
+  #       # caching
+  #       cache_size = 536870912; # 512 MB
+  #       cache_ttl_min = 1800; # 30 min
+  #       cache_optimistic = true; # return stale and then refresh
+  #     };
+  #   };
+  # };
+  # services.unbound = {
+  #   # enable = true;
+  #   settings = {
+  #     server = {
+  #       hide-identity = "yes";
+  #       hide-version = "yes";
+  #       port = 5335;
+  #     };
+  #   };
+  # };
   services.openssh.enable = true;
   services.autorandr = {
     enable = true;
