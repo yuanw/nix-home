5c9006c34e7c66cb9664bc195257bde65b7a1482
yuanwang <yuan.wang@workiva.com>
2023-08-09 08:55:25 -0600

    add some comments

--- treefmt.nix
+++ treefmt.nix
@@ -5,6 +5,6 @@
   programs.ormolu.enable = true;
   programs.cabal-fmt.enable = true;
   programs.hlint.enable = true;
-  programs.shellcheck.enable = false;
+  programs.shellcheck.enable = true;
   programs.shfmt.enable = true;
 }
