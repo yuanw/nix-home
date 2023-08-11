6328da0807f5f898d832cc7c6129a9f7327f55ea
yuanwang <yuan.wang@workiva.com>
2023-08-11 07:51:18 -0600

    how is this

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       url = "github:AstroNvim/AstroNvim";
       flake = false;
     };
-    haskell-flake.url = "github:srid/haskell-flake";
+    haskell-flake.url = "github:yuanwang-wf/haskell-flake";
     nur.url = "github:nix-community/NUR";
     emacs.url = "github:nix-community/emacs-overlay";
     agenix = {
@@ -36,7 +36,6 @@
     treefmt-nix.url = "github:numtide/treefmt-nix";
     pre-commit.url = "github:cachix/pre-commit-hooks.nix";
     pre-commit.inputs.nixpkgs.follows = "nixpkgs";
-
   };
 
 
