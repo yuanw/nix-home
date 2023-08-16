ff762a58d3f2a6e00d421483f28f92c555cf5294
yuanwang <yuan.wang@workiva.com>
2023-08-16 12:25:10 -0600

    wip

--- modules/wm/sketchybar/plugins/ical.sh
+++ modules/wm/sketchybar/plugins/ical.sh
@@ -14,19 +14,19 @@ list_events() {
   args+=(--remove '/ical.event\.*/')
 
   # Displays the day in the first row of the list
-  DATE=$(date -v+${DAY_COUNT}d +"%d.%b")
-  args+=(--clone ical.day.$DAY_COUNT ical.template
-    --set ical.day.$DAY_COUNT icon="$(date -v+${DAY_COUNT}d +"%a %d.%b")"
-    icon.color=$GREEN
+  DATE=$(date -v+"${DAY_COUNT}"d +"%d.%b")
+  args+=(--clone ical.day."$DAY_COUNT" ical.template
+    --set ical.day."$DAY_COUNT" icon="$(date -v+"${DAY_COUNT}"d +"%a %d.%b")"
+    icon.color="$GREEN"
     click_script="sketchybar --set $NAME popup.drawing=off"
     position=popup.ical
     drawing=on)
   if [ "${DAY_COUNT}" == "0" ]; then
-    args+=(--set ical.day.$DAY_COUNT drawing=off)
-    EVENTS="$(icalBuddy -eed -n -nc -nrd -npn -ea -df "" -tf "%H.%S" -iep datetime,title -b '' -ps "|$SEP|" eventsFrom:$DATE to:$DATE)"
+    args+=(--set ical.day."$DAY_COUNT" drawing=off)
+    EVENTS="$(icalBuddy -eed -n -nc -nrd -npn -ea -df "" -tf "%H.%S" -iep datetime,title -b '' -ps "|$SEP|" eventsFrom:"$DATE" to:"$DATE")"
   else
-    args+=(--set ical.day.$DAY_COUNT drawing=on)
-    EVENTS="$(icalBuddy -eed -nc -nrd -npn -ea -df "" -tf "%H.%S" -iep datetime,title -b '' -ps "|$SEP|" eventsFrom:$DATE to:$DATE)"
+    args+=(--set ical.day."$DAY_COUNT" drawing=on)
+    EVENTS="$(icalBuddy -eed -nc -nrd -npn -ea -df "" -tf "%H.%S" -iep datetime,title -b '' -ps "|$SEP|" eventsFrom:"$DATE" to:"$DATE")"
   fi
 
   # Displays the events of the day (time and title)
@@ -40,14 +40,14 @@ list_events() {
       time="No events"
       title=":)"
     fi
-    args+=(--clone ical.event.$EVENT_COUNT ical.template
-      --set ical.event.$EVENT_COUNT label="$title"
+    args+=(--clone ical.event."$EVENT_COUNT" ical.template
+      --set ical.event."$EVENT_COUNT" label="$title"
       icon="$time"
-      icon.color=$YELLOW
+      icon.color="$YELLOW"
       click_script="sketchybar --set $NAME popup.drawing=off"
       position=popup.ical
       drawing=on)
-  done <<<"$(echo "$EVENTS")"
+  done <<<"$EVENTS"
 }
 
 mouse_clicked() {
@@ -56,7 +56,7 @@ mouse_clicked() {
   if [ "$BUTTON" = "left" ]; then
     ((DAY_COUNT++))
   else
-    if [ $DAY_COUNT -gt 0 ]; then
+    if [ "$DAY_COUNT" -gt 0 ]; then
       ((DAY_COUNT--))
     else
       return
@@ -72,9 +72,9 @@ update() {
   EVENTS="$(icalBuddy -eed -n -nc -nrd -npn -ea -df "" -tf "%H.%S" -iep datetime -b '' -ps "|$SEP|" eventsToday)"
   # Comment this if-section out if you don't want the time of the next event next to the icon
   if [ "${EVENTS}" != "" ]; then
-    args+=(--set $NAME label="$(echo "${EVENTS}" | head -n1)")
+    args+=(--set "$NAME" label="$(echo "${EVENTS}" | head -n1)")
   else
-    args+=(--set $NAME label="")
+    args+=(--set "$NAME" label="")
   fi
 
   sketchybar -m "${args[@]}" >/dev/null
