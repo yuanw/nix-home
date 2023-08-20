c91620c98da10d79edb549f6341ecc3f8aec0e6d
yuanwang <yuan.wang@workiva.com>
2023-08-20 07:59:23 -0600

    wip

--- modules/editor/doom/config/init.el
+++ modules/editor/doom/config/init.el
@@ -21,13 +21,13 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company +childframe)           ; the ultimate code completion backend
+       ;; (company +childframe)           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       ;; (corfu
-       ;;  +icons
-       ;;  +orderless)
+       (corfu
+        +icons
+        +orderless)
        ( vertico +icons  )           ; the search engine of the future
 
 
