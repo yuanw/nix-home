123c30d65906c74c2ab2390da1b8fec5e36aedac
yuanwang <yuan.wang@workiva.com>
2023-08-26 08:09:47 -0600

    wip

--- modules/editor/doom/config/abbrevs.el
+++ modules/editor/doom/config/abbrevs.el
@@ -2,14 +2,10 @@
 ;;
 ;; Copyright (C) 2023 Yuan Wang
 ;;
-;; Author: Yuan Wang <yuanwang@WK01174>
-;; Maintainer: Yuan Wang <yuanwang@WK01174>
 ;; Created: August 26, 2023
 ;; Modified: August 26, 2023
 ;; Version: 0.0.1
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/yuanwang-wf/abbrevs
-;; Package-Requires: ((emacs "24.3"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
