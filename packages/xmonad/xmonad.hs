0745f3a001aedbe544c45e6ba55c060f62f4c9d7
yuanw <me@yuanwang.ca>
2023-09-01 19:38:05 -0600

    let's use alt as mod key, so it matchs yabai

--- packages/xmonad/xmonad.hs
+++ packages/xmonad/xmonad.hs
@@ -47,7 +47,7 @@ myBorderWidth = 1
 -- ("right alt"), which does not conflict with emacs keybindings. The
 -- "windows key" is usually mod4Mask.
 --
-myModMask = mod4Mask
+myModMask = mod1Mask
 
 -- NOTE: from 0.9.1 on numlock mask is set automatically. The numlockMask
 -- setting should be removed from configs.
