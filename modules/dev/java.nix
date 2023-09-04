be8bcfdad4eca5e399963b71cdbf81169c5449d8
yuanwang <yuan.wang@workiva.com>
2023-09-04 13:44:29 -0600

    format

--- modules/dev/java.nix
+++ modules/dev/java.nix
@@ -6,10 +6,17 @@ in {
   options.modules.dev.java = { enable = mkEnableOption "java"; };
 
   config = mkIf cfg.enable {
-    home-manager.users.${config.my.username}.home.packages = with pkgs; [
-      lombok
+    home-manager.users.${config.my.username} = {
+      home.packages = with pkgs; [
+        lombok
 
-      google-java-format
-    ];
+        google-java-format
+      ];
+      programs = {
+        # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/development/libraries/java/lombok/default.nix#L26
+        zsh = { sessionVariables = { LOMBOK_DIR = "${pkgs.lombok}/out/share"; }; };
+      };
+
+    };
   };
 }
