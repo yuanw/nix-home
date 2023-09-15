b4e252a4a7a93955a41b81f661ea6f16e0da1226
yuanwang <yuan.wang@workiva.com>
2023-09-15 11:34:17 -0600

    wip

--- modules/home.nix
+++ modules/home.nix
@@ -27,13 +27,15 @@
     bat = {
       enable = true;
       themes =
-        builtins.readFile (pkgs.fetchFromGitHub
-          {
-            owner = "dracula";
-            repo = "sublime"; # Bat uses sublime syntax for its themes
-            rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
-            sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
-          } + "/Dracula.tmTheme");
+        {
+          dracula = builtins.readFile (pkgs.fetchFromGitHub
+            {
+              owner = "dracula";
+              repo = "sublime"; # Bat uses sublime syntax for its themes
+              rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+              sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+            } + "/Dracula.tmTheme");
+        };
       extraPackages = with pkgs.bat-extras; [ batdiff batman batgrep batwatch ];
     };
     dircolors = {
