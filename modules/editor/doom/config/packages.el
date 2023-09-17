f0ab89d3cb7745885f31bc3ffc25da6f91f5d051
yuanwang <yuan.wang@workiva.com>
2023-09-17 15:19:14 -0600

    what

--- modules/editor/doom/config/packages.el
+++ modules/editor/doom/config/packages.el
@@ -60,10 +60,8 @@
 (package! eglot-java)
 (package! justl)
 (package! atomic-chrome)
-(package! shell-maker
-  :recipe (:host github :repo "xenodium/chatgpt-shell" :files ("shell-maker.el")))
-(package! chatgpt-shell
-  :recipe (:host github :repo "xenodium/chatgpt-shell" :files ("chatgpt-shell.el")))
+(package! shell-maker)
+(package! chatgpt-shell)
 ;https://github.com/emacs-tree-sitter/tree-sitter-langs/pull/110
 ;; (package! tree-sitter-langs
 ;;   :pin "388dcbbc237b2b1360e5b148e67b158eecede805"
