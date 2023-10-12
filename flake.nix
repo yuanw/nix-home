6bb7e9d7034309c3af34f8cb34ffaef638d0f2e7
yuanwang <yuan.wang@workiva.com>
2023-10-12 12:32:42 -0600

    comment out mesa overlay. since it is not working

--- flake.nix
+++ flake.nix
@@ -60,34 +60,34 @@
         inputs.haskell-flake.flakeModule
       ];
       perSystem = { config, system, pkgs, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            (_final: _prev: {
-              # https://gitlab.freedesktop.org/mesa/mesa/-/issues/8634
-              mesa = if _prev.stdenv.isDarwin then inputs.nixpkgs-stable.legacyPackages.${_prev.system}.mesa else
-              inputs.nixpkgs.legacyPackages.${_prev.system}.mesa;
-            })
-          ];
-          config = {
-            allowUnsupportedSystem = true;
-          };
+        # _module.args.pkgs = import inputs.nixpkgs {
+        #   inherit system;
+        #   overlays = [
+        #     (_final: _prev: {
+        #       # https://gitlab.freedesktop.org/mesa/mesa/-/issues/8634
+        #       mesa = if _prev.stdenv.isDarwin then inputs.nixpkgs-stable.legacyPackages.${_prev.system}.mesa else
+        #       inputs.nixpkgs.legacyPackages.${_prev.system}.mesa;
+        #     })
+        #   ];
+        #   config = {
+        #     allowUnsupportedSystem = true;
+        #   };
+      };
+      haskellProjects.default = {
+        projectRoot = ./packages;
+        settings = { };
+        # overrides = self: super: { };
+        autoWire = [ "packages" "apps" "checks" ]; # Wire all but the devShell
+        devShell = {
+          hlsCheck.enable = false;
         };
-        haskellProjects.default = {
-          projectRoot = ./packages;
-          settings = { };
-          # overrides = self: super: { };
-          autoWire = [ "packages" "apps" "checks" ]; # Wire all but the devShell
-          devShell = {
-            hlsCheck.enable = false;
-          };
-        };
-
-        treefmt.imports = [ ./treefmt.nix ];
-        # https://github.com/cachix/pre-commit-hooks.nix/blame/30d1c34bdbfe3dd0b8fbdde3962180c56cf16f12/flake-module.nix
-        pre-commit.settings.hooks.treefmt.enable = true;
-
       };
 
+      treefmt.imports = [ ./treefmt.nix ];
+      # https://github.com/cachix/pre-commit-hooks.nix/blame/30d1c34bdbfe3dd0b8fbdde3962180c56cf16f12/flake-module.nix
+      pre-commit.settings.hooks.treefmt.enable = true;
+
     };
+
+};
 }
