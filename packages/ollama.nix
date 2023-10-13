e8c90696bdfafab108def104eb0fec43b1d45a76
yuanwang <yuan.wang@workiva.com>
2023-10-13 15:25:34 -0600

    update sha256

--- packages/ollama.nix
+++ packages/ollama.nix
@@ -13,7 +13,7 @@ buildGoModule rec {
     owner = "jmorganca";
     repo = "ollama";
     rev = "v${version}";
-    sha256 = lib.fakeSha256;
+    sha256 = "sha256-mRKgj+WkdaAaFxAdiR/ya5sGXDJe2AeL/ViSpd/6im4=";
   };
 
   buildInputs = lib.optionals stdenv.isDarwin (with darwin.apple_sdk_11_0.frameworks; [
