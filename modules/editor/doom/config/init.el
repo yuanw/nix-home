34a832a517b4026c3f4728cb5f1a2eb96ad37480
yuanwang <yuan.wang@workiva.com>
2023-10-15 18:30:23 -0600

    add flag for scheme to pick an implem

--- modules/editor/doom/config/init.el
+++ modules/editor/doom/config/init.el
@@ -188,7 +188,7 @@
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (scheme)   ; a fully conniving family of lisps
+       (scheme +chicken)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
