bffd79f07b1dd0c62c73aaf167dfb9867123d808
yuanwang <yuan.wang@workiva.com>
2023-10-27 13:31:35 -0600

    add comment

--- modules/common.nix
+++ modules/common.nix
@@ -55,6 +55,7 @@
         # https://github.com/NixOS/nixpkgs/pull/257760/files
         ollama = inputs.ollama-nixpkgs.legacyPackages.${_prev.system}.ollama;
         stable = inputs.nixpkgs-stable.legacyPackages.${_prev.system};
+        # https://nixpk.gs/pr-tracker.html?pr=263500
         udiskie = inputs.nixpkgs-master.legacyPackages.${_prev.system}.udiskie;
         # https://gitlab.freedesktop.org/mesa/mesa/-/issues/8634
         # mesa = if _prev.stdenv.isDarwin then inputs.nixpkgs-stable.legacyPackages.${_prev.system}.mesa else
