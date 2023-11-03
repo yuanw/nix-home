de73cda9fcf7000e62013aa7a0fcd5959f96aff0
yuanwang <yuan.wang@workiva.com>
2023-11-03 07:15:37 -0600

    how is this

--- modules/editor/doom/config/init.el
+++ modules/editor/doom/config/init.el
@@ -135,7 +135,7 @@
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
-       (dart +lsp   )   ; paint ui and not much else
+       (dart +lsp +eglot   )   ; paint ui and not much else
 
        dhall
        ;;elixir            ; erlang done right
@@ -149,7 +149,7 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       (haskell +lsp +tree-sitter )    ; a language that's lazier than I am
+       (haskell +lsp +eglot +tree-sitter )    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
