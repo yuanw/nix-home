ad217c95041c1d2a3ede507252985f0308787f25
yuanwang <yuan.wang@workiva.com>
2023-11-03 08:48:14 -0600

    how about turn off lsp for haskell

--- modules/editor/doom/config/init.el
+++ modules/editor/doom/config/init.el
@@ -149,7 +149,7 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       (haskell +lsp +eglot +tree-sitter )    ; a language that's lazier than I am
+       (haskell +tree-sitter )    ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
        ;;json              ; At least it ain't XML
