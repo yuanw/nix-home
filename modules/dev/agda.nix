a07514683e000ccdfd83dcc130dbde4c08848f18
yuanwang <yuan.wang@workiva.com>
2023-11-04 13:49:31 -0600

    wip

--- modules/dev/agda.nix
+++ modules/dev/agda.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, inputs, lib, pkgs, ... }:
 
 with lib;
 let cfg = config.modules.dev.agda;
@@ -6,13 +6,25 @@ in {
   options.modules.dev.agda = { enable = mkEnableOption "agda"; };
 
   config = mkIf cfg.enable {
-    # nixpkgs = {
-    #   overlays = [
-    #     inputs.agda.overlay
-    #   ];
-    # };
+    nixpkgs = {
+      overlays = [
+        inputs.agda.overlay
+      ];
+    };
     home-manager.users.${config.my.username} = {
-      home.packages = [ (pkgs.agda.withPackages (p: [ p.standard-library ])) ];
+      home.packages = [
+        (pkgs.agda.withPackages (p: [
+          (p.standard-library.overrideAttrs (_oldAttrs: {
+            version = "1.7.4";
+            src = pkgs.fetchFromGitHub {
+              repo = "agda-stdlib";
+              owner = "agda";
+              rev = "e0879db86e16c81b3c018ff46ba799f875dcc539";
+              sha256 = "XNWvlI3zfMs9nf0q1sT9rsisBGZOz+/aRmek3ot++k4=";
+            };
+          }))
+        ]))
+      ];
       programs = {
         zsh = { sessionVariables = { AGDA_DIR = "$XDG_CONFIG_HOME/agda"; }; };
       };
