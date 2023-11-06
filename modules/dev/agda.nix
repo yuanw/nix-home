9c3a8d19d9d364098aa76723db55f39a84c4c99f
yuanwang <yuan.wang@workiva.com>
2023-11-06 08:55:51 -0600

    update

--- modules/dev/agda.nix
+++ modules/dev/agda.nix
@@ -1,4 +1,4 @@
-{ config, inputs, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 let cfg = config.modules.dev.agda;
@@ -6,24 +6,17 @@ in {
   options.modules.dev.agda = { enable = mkEnableOption "agda"; };
 
   config = mkIf cfg.enable {
-    nixpkgs = {
-      overlays = [
-        inputs.agda.overlay
-      ];
-    };
+    # nixpkgs = {
+    #   overlays = [
+    #     inputs.agda.overlay
+    #   ];
+    # };
     home-manager.users.${config.my.username} = {
+      # https://agda.readthedocs.io/en/v2.6.4/tools/package-system.html
       home.file = { "agda/defaults".text = "standard-library"; };
       home.packages = [
         (pkgs.agda.withPackages (p: [
-          (p.standard-library.overrideAttrs (_oldAttrs: {
-            version = "1.7.4";
-            src = pkgs.fetchFromGitHub {
-              repo = "agda-stdlib";
-              owner = "agda";
-              rev = "e0879db86e16c81b3c018ff46ba799f875dcc539";
-              sha256 = "XNWvlI3zfMs9nf0q1sT9rsisBGZOz+/aRmek3ot++k4=";
-            };
-          }))
+          p.standard-library
         ]))
       ];
       programs = {
