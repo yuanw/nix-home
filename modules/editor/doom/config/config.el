470e861564eb64c67ffbc436404b0ff6faa974d9
yuanwang <yuan.wang@workiva.com>
2023-11-10 15:34:01 -0600

    wip

--- modules/editor/doom/config/config.el
+++ modules/editor/doom/config/config.el
@@ -64,7 +64,6 @@
   :config
   (map! :n "e" 'justl-exec-recipe))
 
-(unpin! haskell-mode)
 ;; (use-package! chatgpt-shell
 ;;   :requires shell-maker
 ;;   :config
--- modules/editor/doom/config/packages.el
+++ modules/editor/doom/config/packages.el
@@ -48,6 +48,8 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+;
+(unpin! haskell-mode)
 (package! interaction-log)
 (package! evil-motion-trainer
   :recipe (:host github :repo "martinbaillie/evil-motion-trainer"))
