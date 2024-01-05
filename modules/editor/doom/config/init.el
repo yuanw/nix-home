268dfeea423333084d0bc20cafa467dcb337a4db
yuanwang <yuan.wang@workiva.com>
2024-01-05 07:21:43 -0600

    update emacs setup

--- modules/editor/doom/config/init.el
+++ modules/editor/doom/config/init.el
@@ -53,7 +53,8 @@
        ;;unicode           ; extended unicode support for various languages
        ( vc-gutter +diff-hl)         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       ;;window-select     ; visually switch windows
+       (window-select
+        +switch-window)   ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        zen               ; distraction-free coding or writing
 
