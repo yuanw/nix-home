7b5a1f0f2065b80a8f3eee142222f8309d2faafb
yuanwang <yuan.wang@workiva.com>
2024-01-13 14:12:48 -0600

    turn off zellij for now

--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -184,7 +184,7 @@ in
               Exit q detach"
           '';
         };
-        zellij = { enable = true; };
+        zellij = { enable = false; };
 
         # https://github.com/alacritty/alacritty/blob/master/alacritty.yml#L1
         alacritty = {
