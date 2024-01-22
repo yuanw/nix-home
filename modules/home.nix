3f8a90919e73bec4c3aee7ba214c0501e72a125f
yuanwang <yuan.wang@workiva.com>
2024-01-22 16:07:57 -0600

    export SDKMAN_DIR="$HOME/.sdkman"             [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh" drop sdkman

--- modules/home.nix
+++ modules/home.nix
@@ -201,9 +201,6 @@
           lib.mkBefore ''
             export PATH=$PATH:/usr/local/bin:/usr/local/sbin/:$HOME/.local/bin
 
-            #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-            export SDKMAN_DIR="$HOME/.sdkman"
-            [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
           ''
         else
           lib.mkBefore "";
