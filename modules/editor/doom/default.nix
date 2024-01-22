7c807089b5b983afc30631d7c8a494e907c3ed89
yuanwang <yuan.wang@workiva.com>
2024-01-22 11:12:13 -0600

    how is this

--- modules/editor/doom/default.nix
+++ modules/editor/doom/default.nix
@@ -68,7 +68,7 @@ let
         org
         org-roam
         yaml-mode
-        tree-sitter
+        # tree-sitter
         (epkgs.tree-sitter-langs.withPlugins (p: epkgs.tree-sitter-langs.plugins ++ [
           p.tree-sitter-markdown
           p.tree-sitter-java
@@ -154,8 +154,6 @@ with lib; {
           editorconfig-core-c # per-project style config
           # :tools lookup & :lang org +roam
           sqlite
-          # tree-sitter
-          #(tree-sitter.withPlugins (p: [ p.tree-sitter-c p.tree-sitter-java ]))
           # wordnet
           # :lang latex & :lang org (latex previews)
           texlive.combined.scheme-medium
