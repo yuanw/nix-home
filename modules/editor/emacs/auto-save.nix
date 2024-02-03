7a7ae3cc27685d8d9afd049146d5d79e95518dc3
yuanwang <yuan.wang@workiva.com>
2024-02-03 14:20:30 -0600

    add hash

--- modules/editor/emacs/auto-save.nix
+++ modules/editor/emacs/auto-save.nix
@@ -1,6 +1,5 @@
 { trivialBuild
 , fetchFromGitHub
-, lib
 ,
 }:
 trivialBuild rec {
@@ -11,7 +10,7 @@ trivialBuild rec {
     owner = "manateelazycat";
     repo = "auto-save";
     rev = "0fb3c0f38191c0e74f00bae6adaa342de3750e83";
-    sha256 = lib.fakeHash;
+    sha256 = "sha256-MCa28kGMBKLA/WqcDgJVtbul//R80nwWuI757wc12KI=";
   };
 
 }
