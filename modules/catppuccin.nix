0ab2b71761823983b33bb05627db525119e1f698
yuanwang <yuan.wang@workiva.com>
2024-02-09 22:13:12 -0600

    wip

--- modules/catppuccin.nix
+++ modules/catppuccin.nix
@@ -1,21 +1,7 @@
 # https://git.sr.ht/~rycee/configurations/tree/master/item/user/catppuccin.nix
-{ lib, pkgs, ... }:
+{ pkgs, ... }:
 
-let
-
-  inherit (builtins) fromJSON readFile;
-
-  catppuccinJson = fromJSON (readFile (pkgs.fetchurl {
-    url =
-      "https://raw.githubusercontent.com/catppuccin/palette/af11bc6d132d2e85cc192a9237f86fa9746c92c0/palette.json";
-    sha256 = "sha256-AojVV7p4nm+1tSK9KN02YLwm14fkRr2pDRPUWYYkPeA=";
-  }));
-
-  loadTheme = name: catppuccinJson.${name};
-
-in
 {
-  lib.catppuccin = loadTheme "mocha";
 
   catppuccin.flavour = "mocha";
   programs.bat.catppuccin.enable = true;
