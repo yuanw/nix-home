e47f309dac7f3cec6cd05d145b03fe1e746cce73
yuanwang <yuan.wang@workiva.com>
2024-02-11 06:50:17 -0600

    use config

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -153,7 +153,7 @@ with lib; {
     {
       services.emacs = mkIf cfg.usePackage {
         enable = cfg.enableService;
-        package = home-manager.users.${config.my.username}.programs.emacs.finalPackage;
+        package = config.programs.emacs.finalPackage;
       };
       # https://www.reddit.com/r/NixOS/comments/vh2kf7/home_manager_mkoutofstoresymlink_issues/
       # config.lib.file.mkOutOfStoreSymlink is provided by the home-manager module,
