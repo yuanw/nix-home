6c7791fd4683e004001fb3edb47fbc5c7324fbb6
yuanwang <yuan.wang@workiva.com>
2024-02-11 07:35:31 -0600

    how is this

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -177,14 +177,14 @@ with lib; {
             usePackageVerbose = false;
 
             earlyInit = ''
-                        ;; Disable some GUI distractions. We set these manually to avoid starting
-                        ;; the corresponding minor modes.
-                        (push '(menu-bar-lines . 0) default-frame-alist)
-                        (push '(tool-bar-lines . nil) default-frame-alist)
-                        (push '(vertical-scroll-bars . nil) default-frame-alist)
+              ;; Disable some GUI distractions. We set these manually to avoid starting
+              ;; the corresponding minor modes.
+              (push '(menu-bar-lines . 0) default-frame-alist)
+              (push '(tool-bar-lines . nil) default-frame-alist)
+              (push '(vertical-scroll-bars . nil) default-frame-alist)
 
-                        ;; Set up fonts early.
-                          (set-face-attribute 'default nil
+              ;; Set up fonts early.
+              (set-face-attribute 'default nil
               :font "PragmataPro Mono Liga"
               :height 180
               :weight 'medium)
