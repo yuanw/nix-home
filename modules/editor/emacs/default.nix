5ddf4ca5f016aac504f9f38f5f80e7d862b3085f
yuanwang <yuan.wang@workiva.com>
2024-02-13 16:51:34 -0600

    how is this

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -342,6 +342,280 @@ with lib; {
             '';
 
             postlude = ''
+              
+(use-package corfu
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  (corfu-separator ?\s)          ;; Orderless field separator
+  (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  (corfu-quit-no-match t)       ;; Never quit, even if there is no match
+  (corfu-preview-current nil)    ;; Disable current candidate preview
+  (corfu-preselect 'prompt)      ;; Preselect the prompt
+  (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  (corfu-scroll-margin 5)        ;; Use scroll margin
+
+  ;; Enable Corfu only for certain modes.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  ;; Recommended: Enable Corfu globally.  This is recommended since Dabbrev can
+  ;; be used globally (M-/).  See also the customization variable
+  ;; `global-corfu-modes' to exclude certain modes.
+  :config
+  (global-corfu-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; TAB cycle if there are only few candidates
+  (setq completion-cycle-threshold 3)
+
+  ;; Emacs 28: Hide commands in M-x which do not apply to the current mode.
+  ;; Corfu commands are hidden, since they are not supposed to be used via M-x.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (setq tab-always-indent 'complete))
+
+;; Add extensions
+(use-package cape
+
+  ;; Bind dedicated completion commands
+  ;; Alternative prefix keys: C-c p, M-p, M-+, ...
+  :bind (("C-c p p" . completion-at-point) ;; capf
+         ("C-c p t" . complete-tag)        ;; etags
+         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
+         ("C-c p h" . cape-history)
+         ("C-c p f" . cape-file)
+         ("C-c p k" . cape-keyword)
+         ("C-c p s" . cape-elisp-symbol)
+         ("C-c p e" . cape-elisp-block)
+         ("C-c p a" . cape-abbrev)
+         ("C-c p l" . cape-line)
+         ("C-c p w" . cape-dict)
+         ("C-c p :" . cape-emoji)
+         ("C-c p \\" . cape-tex)
+         ("C-c p _" . cape-tex)
+         ("C-c p ^" . cape-tex)
+         ("C-c p &" . cape-sgml)
+         ("C-c p r" . cape-rfc1345))
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+  ;;(add-to-list 'completion-at-point-functions #'cape-history)
+  ;;(add-to-list 'completion-at-point-functions #'cape-keyword)
+  ;;(add-to-list 'completion-at-point-functions #'cape-tex)
+  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+  ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+  ;;(add-to-list 'completion-at-point-functions #'cape-line)
+)
+
+(use-package kind-icon
+:after corfu
+;:custom
+; (kind-icon-blend-background t)
+; (kind-icon-default-face 'corfu-default) ; only needed with blend-background
+:config
+(add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+(use-package consult
+  ;; Replace bindings. Lazily loaded due by `use-package'.
+  :bind  (([remap repeat-complex-command] . consult-complex-command)
+         ([remap switch-to-buffer] . consult-buffer)
+         ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
+         ([remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
+         ([remap project-switch-to-buffer] . consult-project-buffer)
+         ([remap bookmark-jump] . consult-bookmark)
+
+         ;; ([remap find-file] . consult-find)
+         ;; ([Remap imenu] . consult-imenu)
+         ([remap yank-pop] . consult-yank-pop)
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h"   . consult-history)
+         ("C-c K"   . consult-kmacro)
+         ;; ("C-c i"   . consult-info)
+          ;; M-s bindings (search-map)
+       ("M-s f"   . consult-find)
+       ("M-s M-g" . consult-grep)
+       ("M-s g"   . consult-ripgrep)
+       ("M-s G"   . consult-git-grep)
+       ("M-s r"   . consult-ripgrep)
+       ("M-s l"   . consult-line)
+       ("M-s L"   . consult-line-multi)
+       ("M-s k"   . consult-keep-lines)
+       ("M-s u"   . consult-focus-lines)
+         )
+  ;; Enable automatic preview at point in the *Completions* buffer. This is
+  ;; relevant when you use the default completion UI.
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+
+  :custom
+  ;; (consult-preview-key "M-i")
+  (consult-narrow-key "<")
+
+  :custom-face
+  (consult-file ((t (:inherit font-lock-string-face))))
+
+  :functions
+  (consult-register-format
+   consult-register-window
+   consult-xref)
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Optionally configure the register formatting. This improves the register
+  ;; preview for `consult-register', `consult-register-load',
+  ;; `consult-register-store' and the Emacs built-ins.
+  (setq register-preview-delay 0.5
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+  (use-package consult-xref)
+  (use-package consult-register)
+
+  (consult-customize
+   consult-theme
+   :preview-key '(:debounce 0.2 any)
+   consult-ripgrep
+   consult-git-grep
+   consult-grep
+   consult-bookmark
+   consult-recent-file
+   consult-xref
+   consult--source-bookmark
+   consult--source-file-register
+   consult--source-recent-file
+   consult--source-project-recent-file
+   :preview-key '(:debounce 0.4 any))
+
+  )
+
+;; Enable vertico
+   (use-package vertico
+      :config
+      (vertico-mode)
+
+      ;; Different scroll margin
+      (setq vertico-scroll-margin 0)
+
+      ;; Show more candidates
+      ;; (setq vertico-count 20)
+
+      ;; Grow and shrink the Vertico minibuffer
+      (setq vertico-resize t)
+      ;;(setq vertico-preselect 'prompt)
+
+
+      ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+       (setq vertico-cycle t))
+
+    (use-package vertico-directory
+    :bind (:map vertico-map
+                ("RET" . vertico-directory-enter)
+                ("DEL" . vertico-directory-delete-char)
+                ("M-DEL" . vertico-directory-delete-word))
+    ;; Tidy shadowed file names
+    :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
+
+    )
+
+  ;; (use-package vertico-quick
+  ;;   :demand t
+  ;;   :after vertico
+  ;;   :bind (
+  ;;          :map vertico-map
+  ;;          ("M-q" . vertico-quick-insert)
+  ;;          ("C-q" . vertico-quick-exit))
+  ;;   :init
+  ;;   (progn
+  ;;     (setq vertico-quick1 "haio")
+  ;;     (setq vertico-quick2 "luy")))
+
+  ;; (use-package vertico-multiform
+  ;;   :demand t
+  ;;   :after vertico
+  ;;   :config
+  ;;   (progn
+  ;;     (vertico-multiform-mode)
+
+  ;;     (setq vertico-multiform-commands
+  ;;           '(;; show grep results in a dedicated buffer:
+  ;;             (consult-ripgrep buffer)))))
+
+    ;; Persist history over Emacs restarts. Vertico sorts by history position.
+    (use-package savehist
+      :config
+      (savehist-mode))
+
+    ;; A few more useful configurations...
+    (use-package emacs
+      :init
+      ;; Add prompt indicator to `completing-read-multiple'.
+      ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+      (defun crm-indicator (args)
+        (cons (format "[CRM%s] %s"
+                      (replace-regexp-in-string
+                       "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                       crm-separator)
+                      (car args))
+              (cdr args)))
+      (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+      ;; Do not allow the cursor in the minibuffer prompt
+      (setq minibuffer-prompt-properties
+            '(read-only t cursor-intangible t face minibuffer-prompt))
+      (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+      ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+      ;; Vertico commands are hidden in normal buffers.
+       (setq read-extended-command-predicate
+             #'command-completion-default-include-p)
+
+      ;; Enable recursive minibuffers
+      ;;(setq enable-recursive-minibuffers t)
+)
+
+(use-package consult-dir
+ :bind (("M-g d" . consult-dir)
+        :map minibuffer-local-filename-completion-map
+        ("M-g d" . consult-dir)
+        ("M-s f" . consult-dir-jump-file)
+        ;; :map embark-become-file+buffer-map
+        ;; ("d" . consult-dir)
+        )
+ :init
+ (use-package vertico
+   :bind (:map vertico-map
+          ("M-g d" . consult-dir)
+          ("M-s f" . consult-dir-jump-file)
+          ))
+  :config
+ (add-to-list 'consult-dir-sources 'consult-dir--source-tramp-ssh t)
+ (setq consult-dir-shadow-filenames nil))
               ;; Minimising & quitting Emacs way too many times without wanting to.
               (global-unset-key "\C-z")
               (global-unset-key "\C-x\C-c")
@@ -607,12 +881,12 @@ with lib; {
                 '';
               };
               marginalia = {
-                enable = true;
+                enable = false;
                 config = "(marginalia-mode)";
               };
 
               embark = {
-                enable = true;
+                enable = false;
                 command = [ "embark-prefix-help-command" ];
                 bind = {
                   "C-." = "embark-act";
@@ -629,7 +903,7 @@ with lib; {
                 '';
               };
               vertico = {
-                enable = true;
+                enable = false;
                 command = [ "vertico-mode" "vertico-next" ];
                 config = ''(vertico-mode)
                    (use-package vertico-directory
@@ -643,14 +917,14 @@ with lib; {
               };
 
               corfu = {
-                enable = true;
+                enable = false;
                 config = ''
                   (global-corfu-mode)
                 '';
               };
 
               consult = {
-                enable = true;
+                enable = false;
                 hook = [ "(completion-list-mode . consult-preview-at-point-mode)" ];
                 extraConfig = ''
                             :bind (;; C-c bindings in `mode-specific-map'
@@ -729,7 +1003,7 @@ with lib; {
                 '';
               };
               embark-consult = {
-                enable = true;
+                enable = false;
                 after = [ "embark" "consult" ];
               };
 
@@ -812,7 +1086,7 @@ with lib; {
                                                            (interactive)
                                                            (split-window-right)
                                                            (windmove-right)) "split right")
-                                                        ("s" (lambda ()
+                                                        ("s" w(lambda ()
                                                            (interactive)
                                                            (split-window-below)
                                                            (windmove-down)) "below")
