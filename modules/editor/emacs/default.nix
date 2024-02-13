9b85bfb07c6e7b339057845a3a6a84d86aa52372
yuanwang <yuan.wang@workiva.com>
2024-02-13 14:50:29 -0600

    what

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -652,6 +652,60 @@ with lib; {
               consult = {
                 enable = true;
                 hook = [ "(completion-list-mode . consult-preview-at-point-mode)" ];
+                extraConfig = ''
+                            :bind (;; C-c bindings in `mode-specific-map'
+                  ("C-c M-x" . consult-mode-command)
+                  ("C-c h" . consult-history)
+                  ("C-c k" . consult-kmacro)
+                  ("C-c m" . consult-man)
+                  ("C-c i" . consult-info)
+                  ([remap Info-search] . consult-info)
+                  ;; C-x bindings in `ctl-x-map'
+                  ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+                  ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+                  ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+                  ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+                  ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+                  ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+                  ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+                  ;; Custom M-# bindings for fast register access
+                  ("M-#" . consult-register-load)
+                  ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+                  ("C-M-#" . consult-register)
+                  ;; Other custom bindings
+                  ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+                  ;; M-g bindings in `goto-map'
+                  ("M-g e" . consult-compile-error)
+                  ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+                  ("M-g g" . consult-goto-line)             ;; orig. goto-line
+                  ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+                  ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+                  ("M-g m" . consult-mark)
+                  ("M-g k" . consult-global-mark)
+                  ("M-g i" . consult-imenu)
+                  ("M-g I" . consult-imenu-multi)
+                  ;; M-s bindings in `search-map'
+                  ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+                  ("M-s c" . consult-locate)
+                  ("M-s g" . consult-grep)
+                  ("M-s G" . consult-git-grep)
+                  ("M-s r" . consult-ripgrep)
+                  ("M-s l" . consult-line)
+                  ("M-s L" . consult-line-multi)
+                  ("M-s k" . consult-keep-lines)
+                  ("M-s u" . consult-focus-lines)
+                  ;; Isearch integration
+                  ("M-s e" . consult-isearch-history)
+                  :map isearch-mode-map
+                  ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+                  ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+                  ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+                  ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+                  ;; Minibuffer history
+                  :map minibuffer-local-map
+                  ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+                  ("M-r" . consult-history))  
+                '';
 
                 config = ''
                                (setq consult-narrow-key "<")
