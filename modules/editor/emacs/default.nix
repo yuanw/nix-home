f7fe234edba14b894e91603b23960c227e0c41df
yuanwang <yuan.wang@workiva.com>
2024-02-14 11:42:08 -0600

    let's not delete Trailing ws

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -781,7 +781,7 @@ with lib; {
                        (auto-save-enable)
 
                   (setq auto-save-silent t)   ; quietly save
-                  (setq auto-save-delete-trailing-whitespace t)  ; automatically delete spaces at the end of the line when saving
+                  (setq auto-save-delete-trailing-whitespace nil)  ; automatically delete spaces at the end of the line when saving
 
                   ;;; custom predicates if you don't want auto save.
                   ;;; disable auto save mode when current filetype is an gpg file.
