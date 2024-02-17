48550e89f1994cd054abe59326cbe440714cc311
yuanwang <yuan.wang@workiva.com>
2024-02-17 07:19:42 -0600

    wip

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -864,80 +864,42 @@ with lib; {
               vertico = {
                 enable = true;
                 config = ''
-                    (vertico-mode)
-                    ;; Different scroll margin
-                    (setq vertico-scroll-margin 0)
+                   (vertico-mode)
+                   ;; Different scroll margin
+                   (setq vertico-scroll-margin 0)
 
-                    ;; Show more candidates
-                    ;; (setq vertico-count 20)
+                   ;; Show more candidates
+                   ;; (setq vertico-count 20)
 
-                    ;; Grow and shrink the Vertico minibuffer
-                    (setq vertico-resize t)
+                   ;; Grow and shrink the Vertico minibuffer
+                   (setq vertico-resize t)
 
-                    ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-                    (setq vertico-cycle t)
+                   ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+                   (setq vertico-cycle t)
 
-                    (use-package vertico-directory
-                    :bind (:map vertico-map
-                                ("RET" . vertico-directory-enter)
-                                ("DEL" . vertico-directory-delete-char)
-                                ("M-DEL" . vertico-directory-delete-word))
-                    ;; Tidy shadowed file names
-                    :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
+                   (use-package vertico-directory
+                   :bind (:map vertico-map
+                               ("RET" . vertico-directory-enter)
+                               ("DEL" . vertico-directory-delete-char)
+                               ("M-DEL" . vertico-directory-delete-word))
+                   ;; Tidy shadowed file names
+                   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
 
-                    )
+                   )
 
-                   (use-package vertico-quick
-                     :demand t
-                    :after vertico
-                    :bind (
-                            :map vertico-map
-                            ("M-q" . vertico-quick-insert)
-                            ("C-q" . vertico-quick-exit))
-                     :init
-                     (progn
-                       (setq vertico-quick1 "haio")
-                       (setq vertico-quick2 "luy")))
+                  (use-package vertico-quick
+                    :demand t
+                   :after vertico
+                   :bind (
+                           :map vertico-map
+                           ("M-q" . vertico-quick-insert)
+                           ("C-q" . vertico-quick-exit))
+                    :init
+                    (progn
+                      (setq vertico-quick1 "haio")
+                      (setq vertico-quick2 "luy")))
 
-                  ;; (use-package vertico-multiform
-                  ;;   :demand t
-                  ;;   :after vertico
-                  ;;   :config
-                  ;;   (progn
-                  ;;     (vertico-multiform-mode)
 
-                  ;;     (setq vertico-multiform-commands
-                  ;;           '(;; show grep results in a dedicated buffer:
-                  ;;             (consult-ripgrep buffer)))))
-
-                    ;; Persist history over Emacs restarts. Vertico sorts by history position.
-               
-                    ;; A few more useful configurations...
-                    (use-package emacs
-                      :init
-                      ;; Add prompt indicator to `completing-read-multiple'.
-                      ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
-                      (defun crm-indicator (args)
-                        (cons (format "[CRM%s] %s"
-                                      (replace-regexp-in-string
-                                       "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
-                                       crm-separator)
-                                      (car args))
-                              (cdr args)))
-                      (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
-
-                      ;; Do not allow the cursor in the minibuffer prompt
-                      (setq minibuffer-prompt-properties
-                            '(read-only t cursor-intangible t face minibuffer-prompt))
-                      (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-                      ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
-                      ;; Vertico commands are hidden in normal buffers.
-                       (setq read-extended-command-predicate
-                             #'command-completion-default-include-p)
-
-                      ;; Enable recursive minibuffers
-                      ;;(setq enable-recursive-minibuffers t)
                 '';
               };
 
