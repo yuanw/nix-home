04163ba15356ee617340566ba66726327e8dd69f
yuanwang <yuan.wang@workiva.com>
2024-02-17 17:54:18 -0600

    move cape

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -380,48 +380,7 @@ with lib; {
 
         
 
-              ;; Add extensions
-              (use-package cape
-
-                ;; Bind dedicated completion commands
-                ;; Alternative prefix keys: C-c p, M-p, M-+, ...
-                :bind (("C-c p p" . completion-at-point) ;; capf
-                       ("C-c p t" . complete-tag)        ;; etags
-                       ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
-                       ("C-c p h" . cape-history)
-                       ("C-c p f" . cape-file)
-                       ("C-c p k" . cape-keyword)
-                       ("C-c p s" . cape-elisp-symbol)
-                       ("C-c p e" . cape-elisp-block)
-                       ("C-c p a" . cape-abbrev)
-                       ("C-c p l" . cape-line)
-                       ("C-c p w" . cape-dict)
-                       ("C-c p :" . cape-emoji)
-                       ("C-c p \\" . cape-tex)
-                       ("C-c p _" . cape-tex)
-                       ("C-c p ^" . cape-tex)
-                       ("C-c p &" . cape-sgml)
-                       ("C-c p r" . cape-rfc1345))
-                :init
-                ;; Add to the global default value of `completion-at-point-functions' which is
-                ;; used by `completion-at-point'.  The order of the functions matters, the
-                ;; first function returning a result wins.  Note that the list of buffer-local
-                ;; completion functions takes precedence over the global list.
-                (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-                (add-to-list 'completion-at-point-functions #'cape-file)
-                (add-to-list 'completion-at-point-functions #'cape-elisp-block)
-                ;;(add-to-list 'completion-at-point-functions #'cape-history)
-                ;;(add-to-list 'completion-at-point-functions #'cape-keyword)
-                ;;(add-to-list 'completion-at-point-functions #'cape-tex)
-                ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
-                ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
-                ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
-                ;;(add-to-list 'completion-at-point-functions #'cape-dict)
-                ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
-                ;;(add-to-list 'completion-at-point-functions #'cape-line)
-              )
-
-
+     
 
 
           
@@ -1201,6 +1160,8 @@ with lib; {
                   ''("\\.markdown\\'"    . markdown-mode)''
                 ];
                 config = ''
+                  (setq markdown-command "multimarkdown")
+
                   (markdown-command "pandoc -f markdown_github+smart")
                   (markdown-command-needs-filename t)
                   (markdown-enable-math t)
@@ -1211,9 +1172,7 @@ with lib; {
                   (markdown-header-face-3 ((t (:inherit markdown-header-face :height 1.4))))
                   (markdown-header-face-4 ((t (:inherit markdown-header-face :height 1.2))))
                 '';
-                init = ''
-                  (setq markdown-command "multimarkdown")
-                '';
+
               };
 
 
@@ -1419,6 +1378,33 @@ with lib; {
                   "(purescript-mode . (lambda () (electric-indent-local-mode -1)))"
                 ];
               };
+              cape = {
+                enable = true;
+                # Alternative prefix keys: C-c p, M-p, M-+, ...
+                bind = {
+                  "C-c p p" = "completion-at-point";
+                  "C-c p t" = "complete-tag";
+                };
+                init = ''
+                  ;; Add to the global default value of `completion-at-point-functions' which is
+                  ;; used by `completion-at-point'.  The order of the functions matters, the
+                  ;; first function returning a result wins.  Note that the list of buffer-local
+                  ;; completion functions takes precedence over the global list.
+                  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+                  (add-to-list 'completion-at-point-functions #'cape-file)
+                  (add-to-list 'completion-at-point-functions #'cape-elisp-block)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-history)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-keyword)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-tex)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-elisp-symbol)
+                  ;;(add-to-list 'completion-at-point-functions #'cape-line)
+                '';
+              };
+
 
               vterm = {
                 enable = true;
