691de1088a7c7881cd4c869e3dca8465c8738447
yuanwang <yuan.wang@workiva.com>
2024-02-18 16:45:34 -0600

    move tmux related config out

--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -3,13 +3,6 @@
 with lib;
 let
   cfg = config.modules.terminal;
-  tmuxMenuSeperator = "''";
-  tat = pkgs.writeShellScriptBin "tat" (builtins.readFile ./tat);
-  td = pkgs.writeShellScriptBin "td" (builtins.readFile ./ta);
-  temacs = pkgs.writeShellScriptBin "temacs" ''
-    (tmux has-session -t emacs && tmux switch-client -t emacs) || (tmux new-session -Ad -s emacs && tmux send-keys -t emacs "emacsclient -c -a 'emacs'" "C-m" )'';
-  tkill = pkgs.writeShellScriptBin "tkill"
-    "tmux list-sessions -F '#{?session_attached,,#{session_name}}' | sed '/^$/d' | fzf --reverse --header kill-sessions --preview 'tmux capture-pane -pt {}'  | xargs tmux kill-session -t";
 in
 {
 
@@ -17,11 +10,6 @@ in
   # imports = [ nix-colors.homeManagerModule ];
   options.modules.terminal = {
     enable = mkEnableOption "terminal";
-    mainWorkspaceDir = mkOption {
-      default = "$HOME/workspace";
-      type = types.str;
-      description = "directory for prefix+m to point to";
-    };
 
   };
 
@@ -105,87 +93,8 @@ in
             };
           };
         };
-        zsh = {
-          sessionVariables = {
-            # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux#configuration-variables
-            # automatically start tmux
-            ZSH_TMUX_AUTOSTART = "true";
-            ZSH_TMUX_CONFIG = "$XDG_CONFIG_HOME/tmux/tmux.conf";
-          };
-          shellAliases = {
-            # tkill =
-            #   "tmux list-sessions -F '#{?session_attached,,#{session_name}}' | sed '/^$/d' | fzf --reverse --header kill-session --preview 'tmux capture-pane -pt {}'  | xargs tmux kill-session -t";
 
-            # tkill =
-            #   "for s in $(tmux list-sessions | awk '{print $1}' | rg ':' -r '' | fzf); do tmux kill-session -t $s; done;";
-          };
-          initExtra = mkAfter ''
-            function zt {
-               z $1 && tat
-            }
-          '';
-          oh-my-zsh = { plugins = [ "tmux" ]; };
-        };
-        tmux = {
-          aggressiveResize = true;
-          baseIndex = 1;
-          enable = true;
-          terminal = "screen-256color";
-          clock24 = true;
-          plugins = with pkgs;
-            [
-              # bind is u
-              # https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/misc/tmux-plugins/default.nix#L269
-              tmuxPlugins.fzf-tmux-url
-            ];
-          customPaneNavigationAndResize = true;
-          escapeTime = 0;
-          historyLimit = 50000;
-          keyMode = "vi";
-          shortcut = "Space";
-          extraConfig = ''
-            set -g status-justify "left"
-            set -g status "on"
-            set -g status-left-style none
-            set -g message-command-style "fg=colour146,bg=colour60"
-            set -g status-right-style "none"
-            set -g pane-active-border-style "fg=colour117"
-            set -g status-style bg=colour60
-            set -g message-style "fg=colour146,bg=colour60"
-            set -g pane-border-style "fg=colour60"
-            set -g status-right-length "100"
-            set -g status-left-length "100"
-            setw -g window-status-activity-style "none"
-            setw -g window-status-separator ""
-            setw -g window-status-style "none,fg=colour60,bg=colour60"
-            set -g status-left "#[fg=colour232,bg=colour117] #{?client_prefix,#[fg=white],} #S #[fg=colour117,bg=colour60,nobold,nounderscore,noitalics]"
-            set -g status-right "#[fg=colour60,bg=colour60,nobold,nounderscore,noitalics]#[fg=colour146,bg=colour60] %Y-%m-%d  %H:%M #[fg=colour117,bg=colour60,nobold,nounderscore,noitalics]#[fg=colour232,bg=colour117] #h "
-            setw -g window-status-format "#[fg=colour60,bg=colour60] #I #[fg=colour60,bg=colour60] #W "
-            setw -g window-status-current-format "#[fg=colour60,bg=colour60,nobold,nounderscore,noitalics]#[fg=colour146,bg=colour60] #I #[fg=colour146,bg=colour60] #W #[fg=colour60,bg=colour60,nobold,nounderscore,noitalics]"
-            set -g mouse on
-            bind v split-window -h -c '#{pane_current_path}'
-            bind s split-window -v -c '#{pane_current_path}'
 
-            bind c new-window -c '#{pane_current_path}'
-
-            bind-key R source-file $XDG_CONFIG_HOME/tmux/tmux.conf \; display-message "$XDG_CONFIG_HOME/tmux/tmux.conf reloaded"
-
-            bind L switch-client -l
-            bind J display-popup -E "\
-                 tmux list-panes -a -F '#{?session_attached,,#S:#I.#P}' |\
-                 sed '/^$/d' |\
-                 fzf --reverse --header join-pane --preview 'tmux capture-pane -pt {}'  |\
-                 xargs tmux join-pane -v -s"
-            set-option -g renumber-windows on
-
-            # keep this at the bottom
-            bind-key Tab display-menu -T "#[align=centre]Sessions" "Switch" . 'choose-session -Zw' Last l "switch-client -l" ${tmuxMenuSeperator} \
-              "Open Main Workspace" m "display-popup -E \" td ${cfg.mainWorkspaceDir} \"" ${tmuxMenuSeperator} \
-              "Kill Current Session" k "run-shell 'tmux switch-client -n \; tmux kill-session -t #{session_name}'"  "Kill Other Sessions" o "display-popup -E \"tkill \"" ${tmuxMenuSeperator} \
-              Random r "run-shell 'tat random'" Emacs e "run-shell 'temacs'" ${tmuxMenuSeperator} \
-              Exit q detach"
-          '';
-        };
         zellij = { enable = false; };
 
         # https://github.com/alacritty/alacritty/blob/master/alacritty.yml#L1
