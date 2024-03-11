b4639506345ab142954a62e808a2ae3b55a671c3
yuanwang <yuan.wang@workiva.com>
2024-03-11 07:06:26 -0600

    fake hash for now

--- packages/yabai.nix
+++ packages/yabai.nix
@@ -2,12 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "yabai";
-  version = "6.0.12";
+  version = "6.0.15";
 
   src = fetchzip {
     url =
       "https://github.com/koekeishiya/yabai/releases/download/v${version}/${pname}-v${version}.tar.gz";
-    hash = "sha256-wCxx/XqUrdD2xyoS6VCKMt6PhiQ8ALM6PHkv9lSCYsM=";
+    # hash = "sha256-wCxx/XqUrdD2xyoS6VCKMt6PhiQ8ALM6PHkv9lSCYsM=";
+    hash = lib.fakeHash;
   };
 
   installPhase = ''
