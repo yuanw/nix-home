28fb6e443028a8b0de5690a314c31c5853dd414c
yuanwang <yuan.wang@workiva.com>
2024-03-11 08:45:21 -0600

    use overridAttr

--- packages/default.nix
+++ packages/default.nix
@@ -3,13 +3,13 @@ _final: prev: {
   alerter = prev.callPackage ./alerter { };
   dart = prev.callPackage ./dart.nix { };
   hosts = prev.callPackage ./hosts.nix { };
-  jdt-language-server = prev.jdt-language-server.override {
+  jdt-language-server = prev.jdt-language-server.overrideAttr (_finalAttrs: _previousAttrs: {
     version = "1.33.0";
     src = prev.fetchurl {
       url = "https://download.eclipse.org/jdtls/milestones/1.33.0/jdt-language-server-1.33.0-202402151717.tar.gz";
       hash = "sha256-UZQQl3lFPmN6Azglf97xevwA6OehO/2bSM0bg93z8YY=";
     };
-  };
+  });
   sketchybar-app-font = prev.callPackage ./sketchybar-app-font.nix { };
   sf-symbols = prev.callPackage ./sf_symbols.nix { };
   font-hack-nerd-font = prev.callPackage ./font-hack-nerd-font.nix { };
