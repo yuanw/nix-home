427ee3f0d55f044710ac9ca5b5d8036b1abf22af
yuanwang <yuan.wang@workiva.com>
2024-03-12 13:26:18 -0600

    how is this

--- packages/yabai.nix
+++ packages/yabai.nix
@@ -75,7 +75,11 @@ in
 
       env = {
         # silence service.h error
-        NIX_CFLAGS_COMPILE = "-Wno-implicit-function-declaration";
+        NIX_CFLAGS_COMPILE = [
+          "-I${Kernel}/Library/Frameworks/Kernel.framework/Headers/"
+          "-Wno-implicit-function-declaration"
+
+        ];
       };
 
       postPatch = ''
