dc31f9c2734a714bed2dd47da48dcf5b7cf984dd
yuanwang <yuan.wang@workiva.com>
2024-03-24 14:21:40 -0600

    wip

--- tasks.py
+++ tasks.py
@@ -1,7 +1,22 @@
 from invoke import task
+from traceback import print_exc
 
+import logging
+log = logging.getLogger()
 
 @task
 def build(c, docs=False):
-    result = c.run("nix build .#wk01174", hide=True)
-    print(result.exited)
+    do(c)
+
+
+def do(c, count=0):
+    if count <= 100:
+        try:
+            result = c.run("nix build .#wk01174", hide=True)
+            print("done with " + str(count) + " try")
+        except Exception as e:
+            print("retry")
+            do(c, count=count+1)
+
+    else:
+        print("sad")
