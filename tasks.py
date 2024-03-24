4cf69ac0813c56584ebb67dcef2b3eb4ba7c3836
yuanwang <yuan.wang@workiva.com>
2024-03-24 11:25:06 -0600

    wip

--- tasks.py
+++ tasks.py
@@ -3,6 +3,7 @@ from invoke import task
 
 @task
 def build(c, docs=False):
-    result = c.run("nix build .#wk01174")
-    print(result.ok)
-    print(result.stdout.splitlines()[-1])
+    result = c.run("nix build .#wk01174", hide=True)
+    # print(type(result))
+    print(result.exited)
+    # print(result.stdout.splitlines()[-1])
