69f9e1ad16683e410dbe4270ff166fc675647cad
yuanwang <yuan.wang@workiva.com>
2024-03-30 12:22:49 -0600

    wip

--- packages/choose-mac.nix
+++ packages/choose-mac.nix
@@ -7,15 +7,16 @@
 , SkyLight
 , xcbuild
 }:
-stdenv.mkDerivation (finalAttrs: {
+stdenv.mkDerivation (_finalAttrs: {
   pname = "choose-mac";
   version = "1.3.1";
 
   src = fetchFromGitHub {
     owner = "chipsenkbeil";
     repo = "choose";
-    rev = "${finalAttrs.version}";
-    sha256 = "sha256-oR0GgMinKcBHaZWdE7O+mdbiLKKjkweECKbi80bjW+c=";
+    rev = "29b07318a8c1e929c8b84e0e7a936763eabf2f89";
+    sha256 = lib.fakeSha;
+    # sha256 = "sha256-oR0GgMinKcBHaZWdE7O+mdbiLKKjkweECKbi80bjW+c=";
   };
 
   buildInputs = [
