0d4c309e0d79d9bf2aa59e5c8814c610eb89076e
yuanwang <yuan.wang@workiva.com>
2024-04-03 14:46:17 -0600

    pass hide

--- tasks.py
+++ tasks.py
@@ -5,14 +5,14 @@ import logging
 log = logging.getLogger()
 
 @task
-def build(c, docs=False):
-    do(c)
+def build(c, docs=False, hide=False):
+    do(c, hide=hide)
 
 
-def do(c, count=0):
+def do(c, count=0, hide=False):
     if count <= 100:
         try:
-            result = c.run("nix build .#wk01174", hide=True)
+            result = c.run("nix build .#wk01174", hide=hide)
             print("done with " + str(count) + " try")
         except Exception as e:
             print("retry")
