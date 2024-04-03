03da68478287f1d37a4e0a47d18e5892f18444a7
yuanwang <yuan.wang@workiva.com>
2024-04-03 13:00:35 -0600

    wrong choose pkg

--- modules/macos_packages.nix
+++ modules/macos_packages.nix
@@ -3,5 +3,4 @@
 with pkgs; [
   # alerter
   pinentry_mac
-  choose-mac
 ]
--- modules/wm/yabai.nix
+++ modules/wm/yabai.nix
@@ -73,7 +73,7 @@ in
       };
       home.packages = [
         pkgs.ical-buddy
-        pkgs.choose
+        pkgs.choose-mac
         pkgs.janky-borders
         (
           pkgs.writeShellScriptBin "app-launcher" ''
