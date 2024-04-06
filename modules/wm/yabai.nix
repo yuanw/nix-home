8199d9521c7b90fde8c854285148203f1e3d55aa
yuanwang <yuan.wang@workiva.com>
2024-04-05 19:13:41 -0600

    how is this

--- modules/wm/yabai.nix
+++ modules/wm/yabai.nix
@@ -58,150 +58,151 @@ in
     };
   };
 
-  config = mkIf cfg.enable (mkMerge [{
-    fonts.fonts = with pkgs; [
-      sketchybar-app-font
-      font-hack-nerd-font
-      sf-symbols
-      emacs-all-the-icons-fonts
-      iosevka
-    ];
+  config = mkMerge [
+    (mkIf cfg.enable {
+      fonts.fonts = with pkgs; [
+        sketchybar-app-font
+        font-hack-nerd-font
+        sf-symbols
+        emacs-all-the-icons-fonts
+        iosevka
+      ];
 
-    home-manager.users.${config.my.username} = {
-      programs.gh = {
-        enable = true;
-        settings = {
-          git_protocol = "https";
+      home-manager.users.${config.my.username} = {
+        programs.gh = {
+          enable = true;
+          settings = {
+            git_protocol = "https";
 
-          prompt = "enabled";
+            prompt = "enabled";
 
-          aliases = {
-            co = "pr checkout";
-            pv = "pr view";
+            aliases = {
+              co = "pr checkout";
+              pv = "pr view";
+            };
           };
         };
+        home.packages = [
+          pkgs.ical-buddy
+          pkgs.choose-mac
+          (
+            pkgs.writeShellScriptBin "app-launcher" ''
+              ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applications/Utilities/ ~/.nix-profile/Applications/ | \
+              grep '\.app$' | \
+              sed 's/\.app$//g' | \
+              choose | \
+              xargs -I {} open -a "{}.app"
+            ''
+          )
+          (
+            pkgs.writeShellScriptBin "yabai-next-window" ''
+              #
+              # yabai-next-window
+              #
+              # move to next window
+              #
+              WINDOW=$(yabai -m query --windows --window)
+              STACK_INDEX=$(echo "$WINDOW" | jq '.["stack-index"]')
+              if [[ $STACK_INDEX -gt 0 ]]; then
+                 LAST_STACK_INDEX=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
+                 if [[ "$STACK_INDEX" == "$LAST_STACK_INDEX" ]]; then
+                   yabai -m window --focus stack.first
+                 else
+                   yabai -m window --focus stack.next
+                 fi
+              else
+                 yabai - window --focus west
+              fi
+            ''
+          )
+
+        ];
+
+        xdg.configFile."sketchybar".source = ./sketchybar;
       };
-      home.packages = [
-        pkgs.ical-buddy
-        pkgs.choose-mac
-        (
-          pkgs.writeShellScriptBin "app-launcher" ''
-            ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applications/Utilities/ ~/.nix-profile/Applications/ | \
-            grep '\.app$' | \
-            sed 's/\.app$//g' | \
-            choose | \
-            xargs -I {} open -a "{}.app"
-          ''
-        )
-        (
-          pkgs.writeShellScriptBin "yabai-next-window" ''
-            #
-            # yabai-next-window
-            #
-            # move to next window
-            #
-            WINDOW=$(yabai -m query --windows --window)
-            STACK_INDEX=$(echo "$WINDOW" | jq '.["stack-index"]')
-            if [[ $STACK_INDEX -gt 0 ]]; then
-               LAST_STACK_INDEX=$(yabai -m query --windows --window stack.last | jq '.["stack-index"]')
-               if [[ "$STACK_INDEX" == "$LAST_STACK_INDEX" ]]; then
-                 yabai -m window --focus stack.first
-               else
-                 yabai -m window --focus stack.next
-               fi
-            else
-               yabai - window --focus west
-            fi
-          ''
-        )
-
-      ];
-
-      xdg.configFile."sketchybar".source = ./sketchybar;
-    };
-    services.skhd = {
-      enable = true;
-      skhdConfig = ''
-        # laucher configurations
-        ${laucherConfig}
-        # movement configurations
-        ${moveConfig}
-      '';
-    };
-
-
-    launchd.user.agents.skhd.serviceConfig = {
-      StandardOutPath = "/tmp/skhd.log";
-      StandardErrorPath = "/tmp/skhd.log";
-    };
-    launchd.user.agents.yabai.serviceConfig = {
-      StandardOutPath = "/tmp/yabai.log";
-      StandardErrorPath = "/tmp/yabai.log";
-    };
-
-    services.sketchybar = {
-      extraPackages = [
-        pkgs.jq
-        pkgs.gh
-        pkgs.ripgrep
-        pkgs.ical-buddy
-        pkgs.sketchybar-cpu-helper
-      ];
-      enable = true;
-    };
-    launchd.user.agents.sketchybar = {
-      serviceConfig = {
-        StandardOutPath = "/tmp/sketchybar.log";
-        StandardErrorPath = "/tmp/sketchybar.log";
+      services.skhd = {
+        enable = true;
+        skhdConfig = ''
+          # laucher configurations
+          ${laucherConfig}
+          # movement configurations
+          ${moveConfig}
+        '';
       };
-    };
 
-    services.yabai = {
-      enable = true;
-      package = pkgs.yabai;
-      enableScriptingAddition = true;
-      config = {
-        window_border = "off";
-        window_border_width = 2;
-        active_window_border_color = "0xff81a1c1";
-        normal_window_border_color = "0xff3b4252";
-        focus_follows_mouse = "autoraise";
-        mouse_follows_focus = "off";
-        mouse_drop_action = "stack";
-        window_placement = "second_child";
-        window_opacity = "off";
-        window_topmost = "off";
-        split_ratio = "0.50";
-        auto_balance = "on";
-        mouse_modifier = "fn";
-        mouse_action1 = "move";
-        mouse_action2 = "resize";
-        layout = "bsp";
-        top_padding = 45;
-        # bottom_padding = 5;
-        # left_padding = 10;
-        # right_padding = 10;
-        window_gap = 10;
+
+      launchd.user.agents.skhd.serviceConfig = {
+        StandardOutPath = "/tmp/skhd.log";
+        StandardErrorPath = "/tmp/skhd.log";
       };
-      # https://github.com/koekeishiya/yabai/blob/master/doc/yabai.asciidoc#signal
-      # https://felixkratz.github.io/SketchyBar/config/events#triggering-custom-events
-      # https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(from-HEAD)
-      extraConfig = ''
-        yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
-        sudo yabai --load-sa
-        # rules
-        yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
-        yabai -m rule --add app='System Preferences' manage=off
-        yabai -m rule --add app='mono-stretchly' manage=off
-        # Any other arbitrary config here
-        yabai -m signal --add event=window_focused action="sketchybar --trigger window_focus"
-        yabai -m signal --add event=window_resized action="sketchybar --trigger window_focus"
-        yabai -m signal --add event=window_moved action="sketchybar --trigger window_on_spaces"
-        yabai -m signal --add event=window_created action="sketchybar --trigger windows_on_spaces"
-        yabai -m signal --add event=window_destroyed action="sketchybar --trigger windows_on_spaces"
-      '';
-    };
-  }
+      launchd.user.agents.yabai.serviceConfig = {
+        StandardOutPath = "/tmp/yabai.log";
+        StandardErrorPath = "/tmp/yabai.log";
+      };
+
+      services.sketchybar = {
+        extraPackages = [
+          pkgs.jq
+          pkgs.gh
+          pkgs.ripgrep
+          pkgs.ical-buddy
+          pkgs.sketchybar-cpu-helper
+        ];
+        enable = true;
+      };
+      launchd.user.agents.sketchybar = {
+        serviceConfig = {
+          StandardOutPath = "/tmp/sketchybar.log";
+          StandardErrorPath = "/tmp/sketchybar.log";
+        };
+      };
+
+      services.yabai = {
+        enable = true;
+        package = pkgs.yabai;
+        enableScriptingAddition = true;
+        config = {
+          window_border = "off";
+          window_border_width = 2;
+          active_window_border_color = "0xff81a1c1";
+          normal_window_border_color = "0xff3b4252";
+          focus_follows_mouse = "autoraise";
+          mouse_follows_focus = "off";
+          mouse_drop_action = "stack";
+          window_placement = "second_child";
+          window_opacity = "off";
+          window_topmost = "off";
+          split_ratio = "0.50";
+          auto_balance = "on";
+          mouse_modifier = "fn";
+          mouse_action1 = "move";
+          mouse_action2 = "resize";
+          layout = "bsp";
+          top_padding = 45;
+          # bottom_padding = 5;
+          # left_padding = 10;
+          # right_padding = 10;
+          window_gap = 10;
+        };
+        # https://github.com/koekeishiya/yabai/blob/master/doc/yabai.asciidoc#signal
+        # https://felixkratz.github.io/SketchyBar/config/events#triggering-custom-events
+        # https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(from-HEAD)
+        extraConfig = ''
+          yabai -m signal --add event=dock_did_restart action="sudo yabai --load-sa"
+          sudo yabai --load-sa
+          # rules
+          yabai -m rule --add label="About This Mac" app="System Information" title="About This Mac" manage=off
+          yabai -m rule --add app='System Preferences' manage=off
+          yabai -m rule --add app='mono-stretchly' manage=off
+          # Any other arbitrary config here
+          yabai -m signal --add event=window_focused action="sketchybar --trigger window_focus"
+          yabai -m signal --add event=window_resized action="sketchybar --trigger window_focus"
+          yabai -m signal --add event=window_moved action="sketchybar --trigger window_on_spaces"
+          yabai -m signal --add event=window_created action="sketchybar --trigger windows_on_spaces"
+          yabai -m signal --add event=window_destroyed action="sketchybar --trigger windows_on_spaces"
+        '';
+      };
+    })
     (mkIf cfg.enableJankyborders.enable {
       launchd.user.agents.jankyborders = {
         path = [
@@ -213,5 +214,6 @@ in
         serviceConfig.RunAtLoad = true;
       };
     }
-    )]);
+    )
+  ];
 }
