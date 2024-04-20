706e8350d7c2fd255d112f9e8570d3e02315a58e
yuanwang <yuan.wang@workiva.com>
2024-04-20 12:11:03 -0600

    what

--- devshell.nix
+++ devshell.nix
@@ -2,7 +2,7 @@
   perSystem = { config, pkgs, ... }: {
     devShells.default = pkgs.mkShell {
       buildInputs = with pkgs; [
-        (python3.withPackages (ps: [ ps.invoke ps.argparse ]))
+        (python3.withPackages (ps: [ ps.invoke ]))
         nix-diff
         nix-tree
         awscli
