f3e68773614c3009db5de659da7525c8005d42b4
yuanwang <yuan.wang@workiva.com>
2024-05-17 11:28:02 -0600

    wip

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -973,6 +973,14 @@ with lib; {
                 config = ''
                   (setq eglot-java-server-install-dir "${pkgs.jdt-language-server}")
                   (setq eglot-java-eclipse-jdt-config-directory "${pkgs.jdt-language-server}/share/java/jdtls")
+                  (add-hook 'java-mode-hook 'eglot-java-mode)
+                  (with-eval-after-load 'eglot-java
+                    (define-key eglot-java-mode-map (kbd "C-c l n") #'eglot-java-file-new)
+                    (define-key eglot-java-mode-map (kbd "C-c l x") #'eglot-java-run-main)
+                    (define-key eglot-java-mode-map (kbd "C-c l t") #'eglot-java-run-test)
+                    (define-key eglot-java-mode-map (kbd "C-c l N") #'eglot-java-project-new)
+                    (define-key eglot-java-mode-map (kbd "C-c l T") #'eglot-java-project-build-task)
+                    (define-key eglot-java-mode-map (kbd "C-c l R") #'eglot-java-project-build-refresh))
                 '';
               };
               flycheck-eglot = {
