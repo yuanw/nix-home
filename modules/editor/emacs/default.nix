2b18508f937a316662d4285719b58da179b32d3c
yuanwang <yuan.wang@workiva.com>
2024-06-17 14:22:58 -0600

    Revert "debug with no init options"
    This reverts commit 2e4eb695c6e94a7adb691f864b3c0350b68266b3.

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -992,7 +992,15 @@ with lib; {
                   (add-to-list 'eglot-server-programs
                   '((java-mode java-ts-mode) .
 
-                  ("jdtls-with-lombok")
+                  ("jdtls-with-lombok"
+                  :initializationOptions
+                      (:settings
+                            (:java
+                             (:configuration
+                             (:runtimes [(:name "JavaSE-17" :path "${pkgs.jdk17}/zulu-17.jdk/Contents/Home")
+                             (:name "JavaSE-21" :path "${pkgs.jdk21}/zulu-21.jdk/Contents/Home" :default t)
+                                       ]))))
+                    )
                     )
                   ((go-mode go-dot-mod-mode go-dot-work-mode go-ts-mode go-mod-ts-mode)
                   . ("${pkgs.gopls}/bin/gopls"))
