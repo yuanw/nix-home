1383f8983c0b72a27a685d7cd3f42671334bebaa
yuanwang <yuan.wang@workiva.com>
2024-07-03 11:13:19 -0600

    try out prot-modeline with nix

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -84,6 +84,14 @@ with lib; {
             with epkgs;
             [
               epkgs.treesit-grammars.with-all-grammars
+              (epkgs.trivialBuild {
+                pname = "prot-common";
+                version = "0.0.1";
+                src = ./packages/prot-common.el;
+
+              }
+              )
+
             ];
           programs.emacs.package = emacsPatched;
           programs.emacs.enable = true;
@@ -270,6 +278,54 @@ with lib; {
                   (add-hook 'god-mode-enabled-hook 'god-update-cursor)
                   (add-hook 'god-mode-disabled-hook 'god-update-cursor)
                 '';
+              };
+              prot-modeline = {
+                enable = true;
+                package = epkgs:
+                  epkgs.trivialBuild {
+                    pname = "prot-modeline";
+                    version = "0.0.1";
+                    src = ./packages/prot-modeline.el;
+                  };
+                config = ''  (setq mode-line-compact nil) ; Emacs 28
+  (setq mode-line-right-align-edge 'right-margin) ; Emacs 30
+  (setq-default mode-line-format
+                '("%e"
+                  prot-modeline-kbd-macro
+                  prot-modeline-narrow
+                  prot-modeline-buffer-status
+                  prot-modeline-window-dedicated-status
+                  prot-modeline-input-method
+                  "  "
+                  prot-modeline-buffer-identification
+                  "  "
+                  prot-modeline-major-mode
+                  prot-modeline-process
+                  "  "
+                  prot-modeline-vc-branch
+                  "  "
+                  prot-modeline-eglot
+                  "  "
+                  prot-modeline-flymake
+                  "  "
+                  mode-line-format-right-align ; Emacs 30
+                  prot-modeline-notmuch-indicator
+                  "  "
+                  prot-modeline-misc-info))
+
+  (with-eval-after-load 'spacious-padding
+    (defun prot/modeline-spacious-indicators ()
+      "Set box attribute to `'prot-modeline-indicator-button' if spacious-padding is enabled."
+      (if (bound-and-true-p spacious-padding-mode)
+          (set-face-attribute 'prot-modeline-indicator-button nil :box t)
+        (set-face-attribute 'prot-modeline-indicator-button nil :box 'unspecified)))
+
+    ;; Run it at startup and then afterwards whenever
+    ;; `spacious-padding-mode' is toggled on/off.
+    (prot/modeline-spacious-indicators)
+
+    (add-hook 'spacious-padding-mode-hook #'prot/modeline-spacious-indicators))
+                  '';
               };
               autorevert = {
                 enable = true;
