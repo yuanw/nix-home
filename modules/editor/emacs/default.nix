87f794985f1a4888ffd19f68e8a7422949c31c51
yuanwang <yuan.wang@workiva.com>
2024-07-08 07:25:16 -0600

    try with menu

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -104,7 +104,7 @@ with lib; {
             earlyInit = ''
               ;; Disable some GUI distractions. We set these manually to avoid starting
               ;; the corresponding minor modes.
-              (push '(menu-bar-lines . 0) default-frame-alist)
+              ;; (push '(menu-bar-lines . 0) default-frame-alist)
               (push '(tool-bar-lines . nil) default-frame-alist)
               (push '(vertical-scroll-bars . nil) default-frame-alist)
               ;; no title bar
