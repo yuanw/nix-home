fb6264257781d9b513f19f24a4eb2036587cba7a
yuanwang <yuan.wang@workiva.com>
2024-11-08 11:50:37 -0600

    enable kotlin

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -1335,8 +1335,8 @@ with lib; {
                                     ((go-mode go-dot-mod-mode go-dot-work-mode go-ts-mode go-mod-ts-mode)
                                     . ("${pkgs.gopls}/bin/gopls"))
                                     )
-                  (add-hook 'java-mode-hook #'
-                  eglot-ensure)
+                  (add-hook 'kotlin-mode-hook #'eglot-ensure)
+                  (add-hook 'java-mode-hook #'eglot-ensure)
                   (add-hook 'java-ts-mode-hook #'eglot-ensure)
                 '';
               };
