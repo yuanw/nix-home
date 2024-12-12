02c8346f99ad065986d750ecac1993bd9159308d
yuanwang <yuan.wang@workiva.com>
2024-12-12 15:22:38 -0600

    how is this

--- treefmt.nix
+++ treefmt.nix
@@ -2,7 +2,7 @@
   projectRootFile = "flake.lock";
   programs.deadnix.enable = true;
   programs.nixpkgs-fmt.enable = true;
-  programs.nixpkgs-fmt.excludes = [ "/modules/private" ];
+  programs.nixpkgs-fmt.excludes = [ "/modules/private" "/hosts/wk01174.nix" ];
 
   programs.ormolu.enable = true;
   programs.cabal-fmt.enable = true;
