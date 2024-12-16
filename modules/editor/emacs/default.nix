3ec17e92b82553d3eadb4268d29a2cfa93fe2da2
yuanwang <yuan.wang@workiva.com>
2024-12-16 14:49:26 -0600

    add keyboard quit dwim

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -272,6 +272,31 @@ with lib;
               ;; removing 1 space at a time.
               (setq backward-delete-char-untabify-method 'hungry)
 
+              (defun prot/keyboard-quit-dwim ()
+                   "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+                   The generic `keyboard-quit' does not do the expected thing when
+                   the minibuffer is open.  Whereas we want it to close the
+                   minibuffer, even without explicitly focusing it.
+                   The DWIM behaviour of this command is as follows:
+
+                   - When the region is active, disable it.
+                   - When a minibuffer is open, but not focused, close the minibuffer.
+                   - When the Completions buffer is selected, close it.
+                   - In every other case use the regular `keyboard-quit'."
+               (interactive)
+               (cond
+                ((region-active-p)
+                 (keyboard-quit))
+                 ((derived-mode-p 'completion-list-mode)
+                 (delete-completion-window))
+                ((> (minibuffer-depth) 0)
+                (abort-recursive-edit))
+                (t
+                (keyboard-quit))))
+
+               (define-key global-map (kbd "C-g") #'prot/keyboard-quit-dwim)
+
+
 
               ;; WARNING: This will change your life
               ;; (OPTIONAL) Visualize tabs as a pipe character - "|"
