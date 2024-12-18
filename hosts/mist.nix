326573b1393b0b9ca7272956ab4b4d907e702b0a
yuanwang <yuan.wang@workiva.com>
2024-12-18 15:49:13 -0600

    add new host

---
+++ hosts/mist.nix
@@ -0,0 +1,81 @@
+{
+  pkgs,
+  inputs,
+  config,
+  ...
+}:
+{
+
+  imports = [
+    inputs.self.nixosModules.common
+    inputs.self.nixosModules.darwin
+
+  ];
+
+  my = {
+    username = "yuanw";
+    name = "Yuan Wang";
+  };
+
+  environment.systemPath = [
+    "/opt/homebrew/bin"
+    "/opt/homebrew/sbin"
+  ];
+  home-manager.users.${config.my.username}.programs = {
+    go = {
+      enable = true;
+      goPath = "go";
+    };
+    git = {
+      extraConfig = {
+        github.user = "yuanw";
+      };
+    };
+  };
+  modules = {
+    # common = {
+    #   enable = true;
+    #   supportLocalVirtualBuilder = true;
+    # };
+    ai.enable = true;
+    secrets.agenix = {
+      enable = true;
+    };
+    brew = {
+      enable = true;
+      # taps = [ "homebrew/core" "homebrew/cask" ];
+    };
+    browsers.firefox = {
+      enable = true;
+      pkg = pkgs.firefox-beta-bin;
+    };
+    editors.emacs = {
+      enable = true;
+      enableService = true;
+      enableAider = true;
+      # enableCopilot = true;
+      # lspStyle = "lsp-bridge";
+    };
+    health.enable = true;
+    typing.enable = true;
+    dev = {
+      agda.enable = false;
+      haskell.enable = false;
+      idris2.enable = false;
+      python.enable = true;
+      zig.enable = false;
+    };
+    zellij.enable = false;
+    tmux = {
+      enable = true;
+      mainWorkspaceDir = "$HOME/workspaces";
+    };
+    terminal = {
+      enable = true;
+    };
+    wm = {
+      yabai.enable = true;
+      yabai.enableJankyborders = true;
+    };
+  };
+}
