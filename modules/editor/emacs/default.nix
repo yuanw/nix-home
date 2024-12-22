0d960ed797f89fbdd2312debaf6c3b398be6a93a
yuanw <me@yuanwang.ca>
2024-12-22 17:39:39 -0600

    drop extra )

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -1121,7 +1121,7 @@ with lib;
   "org-habit-alert-face" = "((((background light)) (:background \"#f5f946\")))";
   "org-habit-alert-future-face" = "((((background light)) (:background \"#fafca9\")))";
   "org-habit-clear-face" = "((((background light)) (:background \"#8270f9\")))";
-  "org-habit-clear-future-face" = "((((background light)) (:background \"#d6e4fc\"))))";
+  "org-habit-clear-future-face" = "((((background light)) (:background \"#d6e4fc\")))";
   "org-habit-overdue-face" = "((((background light)) (:background \"#f9372d\")))";
   "org-habit-overdue-future-face" = "((((background light)) (:background \"#fc9590\")))";
   "org-habit-ready-face" = "((((background light)) (:background \"#4df946\")))";
