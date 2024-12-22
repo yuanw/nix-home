097f898c22f203e58c659125efe452cad10a6b47
yuanw <me@yuanwang.ca>
2024-12-21 18:57:41 -0600

    setup org-habit

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -1122,10 +1122,10 @@ with lib;
   "org-habit-alert-future-face" = "((((background light)) (:background \"#fafca9\")))";
   "org-habit-clear-face" = "((((background light)) (:background \"#8270f9\")))";
   "org-habit-clear-future-face" = "((((background light)) (:background \"#d6e4fc\"))))";
-  (org-habit-overdue-face ((((background light)) (:background "#f9372d"))))
-  (org-habit-overdue-future-face ((((background light)) (:background "#fc9590"))))
-  (org-habit-ready-face ((((background light)) (:background "#4df946"))))
-  (org-habit-ready-future-face ((((background light)) (:background "#acfca9")))))
+  "org-habit-overdue-face" = "((((background light)) (:background \"#f9372d\")))";
+  "org-habit-overdue-future-face" = "((((background light)) (:background \"#fc9590\")))";
+  "org-habit-ready-face" = "((((background light)) (:background \"#4df946\")))";
+  "org-habit-ready-future-face" = "((((background light)) (:background \"#acfca9\")))";
   };
             };
               org-extra = {
