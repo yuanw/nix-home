73f8fb4847ba525e9dc993d02dc005c068924a66
yuanwang <yuan.wang@workiva.com>
2024-12-30 17:45:56 -0600

    format

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -429,46 +429,46 @@ with lib;
                     ];
                   };
                 config = ''
-                       (setq mode-line-compact nil) ; Emacs 28
-                       (setq mode-line-right-align-edge 'right-margin) ; Emacs 30
-                       (setq-default mode-line-format
-                            '("%e"
-                              prot-modeline-kbd-macro
-                              prot-modeline-narrow
-                              prot-modeline-buffer-status
-                              prot-modeline-window-dedicated-status
-                              prot-modeline-input-method
-                              "  "
-                              prot-modeline-buffer-identification
-                              "  "
-                              prot-modeline-major-mode
-                              "  "
-                              mode-line-position
-                              "  "
-                              prot-modeline-process
-                              "  "
-                              prot-modeline-vc-branch
-                              "  "
-                              prot-modeline-eglot
-                              "  "
-                              prot-modeline-flymake
-                              "  "
-                              mode-line-format-right-align ; Emacs 30
-                              prot-modeline-notmuch-indicator
-                              "  "
-                              prot-modeline-misc-info))
-                                       
-                       (with-eval-after-load 'spacious-padding
-                         (defun prot/modeline-spacious-indicators ()
-                           "Set box attribute to `'prot-modeline-indicator-button' if spacious-padding is enabled."
-                           (if (bound-and-true-p spacious-padding-mode)
-                               (set-face-attribute 'prot-modeline-indicator-button nil :box t)
-                             (set-face-attribute 'prot-modeline-indicator-button nil :box 'unspecified)))
+                  (setq mode-line-compact nil) ; Emacs 28
+                  (setq mode-line-right-align-edge 'right-margin) ; Emacs 30
+                  (setq-default mode-line-format
+                       '("%e"
+                         prot-modeline-kbd-macro
+                         prot-modeline-narrow
+                         prot-modeline-buffer-status
+                         prot-modeline-window-dedicated-status
+                         prot-modeline-input-method
+                         "  "
+                         prot-modeline-buffer-identification
+                         "  "
+                         prot-modeline-major-mode
+                         "  "
+                         mode-line-position
+                         "  "
+                         prot-modeline-process
+                         "  "
+                         prot-modeline-vc-branch
+                         "  "
+                         prot-modeline-eglot
+                         "  "
+                         prot-modeline-flymake
+                         "  "
+                         mode-line-format-right-align ; Emacs 30
+                         prot-modeline-notmuch-indicator
+                         "  "
+                         prot-modeline-misc-info))
+                                  
+                  (with-eval-after-load 'spacious-padding
+                    (defun prot/modeline-spacious-indicators ()
+                      "Set box attribute to `'prot-modeline-indicator-button' if spacious-padding is enabled."
+                      (if (bound-and-true-p spacious-padding-mode)
+                          (set-face-attribute 'prot-modeline-indicator-button nil :box t)
+                        (set-face-attribute 'prot-modeline-indicator-button nil :box 'unspecified)))
 
-                         ;; Run it at startup and then afterwards whenever
-                         ;; `spacious-padding-mode' is toggled on/off.
-                         (prot/modeline-spacious-indicators)
-                         (add-hook 'spacious-padding-mode-hook #'prot/modeline-spacious-indicators))
+                    ;; Run it at startup and then afterwards whenever
+                    ;; `spacious-padding-mode' is toggled on/off.
+                    (prot/modeline-spacious-indicators)
+                    (add-hook 'spacious-padding-mode-hook #'prot/modeline-spacious-indicators))
                 '';
               };
               autorevert = {
@@ -730,7 +730,7 @@ with lib;
 
               evil = {
                 enable = true;
-                command = ["evil-mode"];
+                command = [ "evil-mode" ];
               };
 
               vertico = {
@@ -2019,11 +2019,11 @@ with lib;
                   "C-c p h" = "cape-history";
                 };
                 init = ''
-                ;; Add `completion-at-point-functions', used by `completion-at-point'.
-                (add-all-to-list 'completion-at-point-functions
-                   #'cape-dabbrev
-                   #'cape-file
-                   #'cape-abbrev)
+                  ;; Add `completion-at-point-functions', used by `completion-at-point'.
+                  (add-all-to-list 'completion-at-point-functions
+                     #'cape-dabbrev
+                     #'cape-file
+                     #'cape-abbrev)
                 '';
               };
 
@@ -2098,7 +2098,7 @@ with lib;
                 '';
                 config = "(spacious-padding-mode 1)";
               };
-              
+
               toggle-term = {
                 enable = false;
                 package =
