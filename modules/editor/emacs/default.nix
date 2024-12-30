89969ab2d9ea5fcf605d44dab80dfdf34a11e793
yuanwang <yuan.wang@workiva.com>
2024-12-30 12:52:41 -0600

    try custom Pragmata Pro font
    https://fsd.it/2024/02/16/now-you-can-customize-your-pragmata-pro-font/

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -160,7 +160,7 @@ with lib;
               (add-to-list 'default-frame-alist '(undecorated-round . t))
               ;; Set up fonts early.
               ;;--------------------
-              (let ((mono-spaced-font "PragmataPro Mono Liga")
+              (let ((mono-spaced-font "PragmataPro Mono Liga Serif")
                     (proportionately-spaced-font "PragmataPro Liga"))
                (set-face-attribute 'default nil :family mono-spaced-font :height 180)
                (set-face-attribute 'fixed-pitch nil :family mono-spaced-font :height 1.0)
