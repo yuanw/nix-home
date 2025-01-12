38b683f6a4e1bb4129840c264adcb3825f91bda3
yuanw <me@yuanwang.ca>
2025-01-11 18:33:56 -0600

    try time

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -1934,20 +1934,20 @@ with lib;
                 enable = true;
                 demand = true;
                 custom = ''
-                  (ef-themes-variable-pitch-ui t)
-                  (ef-themes-mixed-fonts t)
-                  (ef-themes-headings ; read the manual's entry of the doc string
-                   '((0 . (variable-pitch light 1.9))
-                     (1 . (variable-pitch light 1.8))
-                     (2 . (variable-pitch regular 1.7))
-                     (3 . (variable-pitch regular 1.6))
-                     (4 . (variable-pitch regular 1.5))
-                     (5 . (variable-pitch 1.4)) ; absence of weight means `bold'
-                     (6 . (variable-pitch 1.3))
-                     (7 . (variable-pitch 1.2))
-                     (agenda-date . (semilight 1.5))
-                     (agenda-structure . (variable-pitch light 1.9))
-                     (t . (variable-pitch 1.1))))
+                  #   (ef-themes-variable-pitch-ui t)
+                  #   (ef-themes-mixed-fonts t)
+                  #   (ef-themes-headings ; read the manual's entry of the doc string
+                  #    '((0 . (variable-pitch light 1.9))
+                  #      (1 . (variable-pitch light 1.8))
+                  #      (2 . (variable-pitch regular 1.7))
+                  #      (3 . (variable-pitch regular 1.6))
+                  #      (4 . (variable-pitch regular 1.5))
+                  #      (5 . (variable-pitch 1.4)) ; absence of weight means `bold'
+                  #      (6 . (variable-pitch 1.3))
+                  #      (7 . (variable-pitch 1.2))
+                  #      (agenda-date . (semilight 1.5))
+                  #      (agenda-structure . (variable-pitch light 1.9))
+                  #      (t . (variable-pitch 1.1))))
                 '';
                 config = ''
                   ;; Set color theme in early init to avoid flashing during start.
