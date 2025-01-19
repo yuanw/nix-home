7e8a1c530bc0ab99c4e255629205808b2d4eed18
yuanw <me@yuanwang.ca>
2025-01-19 16:43:22 -0600

    wip

--- modules/editor/emacs/packages/eaf/default.nix
+++ modules/editor/emacs/packages/eaf/default.nix
@@ -12,7 +12,6 @@
   lndir,
   pkg-config,
   # Native dependencies
-  nodejs,
   python3,
   wmctrl,
 
@@ -61,36 +60,22 @@ let
       ps.pymupdf
       ps.markdown
       ps.epc
+      # needs for eaf-browser
+      ps.pysocks
       # Wrap native dependencies in python env $PATH
       pkgs.aria2
     ])).override
       { ignoreCollisions = true; }
   );
 
-  node = "${nodejs}/bin/node";
-
   pname = "eaf";
   version = "20210309.0";
-
-  eaf-terminal = fetchFromGitHub {
-    owner = "emacs-eaf";
-    repo = "eaf-terminal";
-    rev = "b7ddb82baf9604cb52a83d6e0631e6a5edc415b1";
-    hash = "sha256-4TRIAcReJs9a91DMKD+puApmSstxYLgLHV300GFDufQ=";
-  };
 
   eaf-browser = fetchFromGitHub {
     owner = "emacs-eaf";
     repo = "eaf-browser";
     rev = "9761f7bd22aa69f144a0a032643b0834dde6cb60";
     hash = "sha256-jcCn16lXqcq1UcekekJiTfRBjjgaY0Hkz69ycElSzuA=";
-  };
-
-  eaf-pdf-viewer = fetchFromGitHub {
-    owner = "emacs-eaf";
-    repo = "eaf-pdf-viewer";
-    rev = "0b79e9000551200b6a0d2206de14b87d9ed3c9c9";
-    hash = "sha256-Co5RWWI0OC1w5JrCVpbwafuEEluh48JDzkMQp5QdIbE=";
   };
 
 in
@@ -113,12 +98,8 @@ melpaBuild {
       --replace '"wmctrl' '"${wmctrl}'
     sed -i s#'defcustom eaf-python-command .*'#'defcustom eaf-python-command "${pythonEnv.interpreter}"'# eaf.el
     mkdir app
-    cp -r ${eaf-terminal} app/terminal
-    cp -r ${eaf-pdf-viewer} app/pdf-viewer
     cp -r ${eaf-browser} app/browser
-    substituteInPlace app/terminal/buffer.py --replace-warn \
-      '"node"' \
-      '"${node}"'
+
   '';
 
   installPhase = ''
