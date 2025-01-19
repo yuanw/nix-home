2181922674b15eab78fb44dc70be7045c09e1db1
yuanw <me@yuanwang.ca>
2025-01-18 19:31:07 -0600

    wip

--- modules/editor/emacs/packages/eaf/default.nix
+++ modules/editor/emacs/packages/eaf/default.nix
@@ -38,7 +38,7 @@ let
           sed -i \
             '/\[tool.sip.project\]/a\
             verbose = true\
-            sip-include-dirs = [\"${pkgs.pyqt6}/${python.sitePackages}/PyQt6/bindings\"]' \
+            sip-include-dirs = [\"${prev.pyqt6}/${python.sitePackages}/PyQt6/bindings\"]' \
             pyproject.toml
 
             cat pyproject.toml
