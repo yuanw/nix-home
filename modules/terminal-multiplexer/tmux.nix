0da914699443193229e989f42df7b03802276961
yuanw <me@yuanwang.ca>
2025-03-01 20:10:43 -0600

    try autostart tmux

--- modules/terminal-multiplexer/tmux.nix
+++ modules/terminal-multiplexer/tmux.nix
@@ -103,7 +103,7 @@ with lib;
           sessionVariables = {
             # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux#configuration-variables
             # automatically start tmux
-            #ZSH_TMUX_AUTOSTART = "true";
+            ZSH_TMUX_AUTOSTART = "true";
             ZSH_TMUX_CONFIG = "$XDG_CONFIG_HOME/tmux/tmux.conf";
           };
           shellAliases = {
