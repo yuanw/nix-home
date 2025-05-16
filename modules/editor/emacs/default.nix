f8de4cfdd650f28e227750111a3ebb1c49f97a56
yuanwang <yuan.wang@workiva.com>
2025-05-15 19:51:11 -0600

    wip

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -1212,23 +1212,22 @@ with lib;
                 '';
 
                 config = ''
-                      (use-package consult-xref)
-                           
-                           
-                                (consult-customize
-                  consult-theme
-                  :preview-key '(:debounce 0.2 any)
-                  consult-ripgrep
-                  consult-git-grep
-                  consult-grep
-                  consult-bookmark
-                  consult-recent-file
-                  consult-xref
-                  consult--source-bookmark
-                  consult--source-file-register
-                  consult--source-recent-file
-                  consult--source-project-recent-file
-                  :preview-key '(:debounce 0.4 any))
+                  (use-package consult-xref)          
+                  (consult-customize
+                   consult-theme
+                   :preview-key '(:debounce 0.2 any)
+                   consult-ripgrep
+                   consult-git-grep
+                   consult-grep
+                   consult-man
+                   consult-bookmark
+                   consult-recent-file
+                   consult-xref
+                   consult--source-bookmark
+                   consult--source-file-register
+                   consult--source-recent-file
+                   consult--source-project-recent-file
+                   :preview-key '(:debounce 0.4 any))
 
                 '';
               };
