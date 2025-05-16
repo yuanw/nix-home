d9b35b8a8a55a30024d8290fc57801c99381a3bc
yuanwang <yuan.wang@workiva.com>
2025-05-15 19:04:30 -0600

    tweak consult

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -1135,6 +1135,7 @@ with lib;
                     (add-to-list 'savehist-additional-variables 'corfu-history))
                 '';
               };
+
               consult = {
                 enable = true;
                 hook = [ "(completion-list-mode . consult-preview-at-point-mode)" ];
@@ -1195,27 +1196,39 @@ with lib;
 
                 custom = ''
                   (consult-narrow-key "<")
+                '';
 
+                init = ''
+                  ;; Tweak the register preview for `consult-register-load',
+                  ;; `consult-register-store' and the built-in commands.  This improves the
+                  ;; register formatting, adds thin separator lines, register sorting and hides
+                  ;; the window mode line.
+                  (advice-add #'register-preview :override #'consult-register-window)
+                  (setq register-preview-delay 0.5)
+
+                  ;; Use Consult to select xref locations with preview
+                  (setq xref-show-xrefs-function #'consult-xref
+                        xref-show-definitions-function #'consult-xref)
                 '';
 
                 config = ''
-                                      (use-package consult-xref)
-                                           
-                                           
-                                                (consult-customize
-                                  consult-theme
-                                  :preview-key '(:debounce 0.2 any)
-                                  consult-ripgrep
-                                  consult-git-grep
-                                  consult-grep
-                                  consult-bookmark
-                                  consult-recent-file
+                      (use-package consult-xref)
+                           
+                           
+                                (consult-customize
+                  consult-theme
+                  :preview-key '(:debounce 0.2 any)
+                  consult-ripgrep
+                  consult-git-grep
+                  consult-grep
+                  consult-bookmark
+                  consult-recent-file
                   consult-xref
-                                  consult--source-bookmark
-                                  consult--source-file-register
-                                  consult--source-recent-file
-                                  consult--source-project-recent-file
-                                  :preview-key '(:debounce 0.4 any))
+                  consult--source-bookmark
+                  consult--source-file-register
+                  consult--source-recent-file
+                  consult--source-project-recent-file
+                  :preview-key '(:debounce 0.4 any))
 
                 '';
               };
