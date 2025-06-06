2ac6deeffb7244193eaad299ebd0d0566f46eeca
yuanwang <yuan.wang@workiva.com>
2025-06-06 10:38:56 -0600

    fix incorrect scope

--- modules/browsers/chromium.nix
+++ modules/browsers/chromium.nix
@@ -17,12 +17,13 @@ in
       type = types.package;
       default = pkgs.brave;
     };
+  };
 
-    config = mkIf cfg.enable {
-      home-manager.users.${config.my.username} = {
-        programs.chromium.enable = true;
-        programs.chromium.package = cfg.pkg;
-      };
+  config = mkIf cfg.enable {
+    home-manager.users.${config.my.username} = {
+      programs.chromium.enable = true;
+      programs.chromium.package = cfg.pkg;
     };
   };
+
 }
