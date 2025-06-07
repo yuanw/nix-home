f241ddd73ef3e1e00940f1cabac7944a3e088c39
yuanwang <yuan.wang@workiva.com>
2025-06-07 10:10:03 -0600

    Revert "how about this"
    This reverts commit d2d5fba8cf618ff5a31ba73cad160e76ec7a8452.

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -35,6 +35,13 @@ let
 
   emacsPackage = config.home-manager.users.${config.my.username}.programs.emacs.finalPackage;
 
+  pkgs' = pkgs.extend (
+    _final: prev: {
+      ld64 = prev.ld64.overrideAttrs (old: {
+        patches = old.patches ++ [ ./Dedupe-RPATH-entries.patch ];
+      });
+    }
+  );
 in
 with lib;
 {
@@ -47,7 +54,7 @@ with lib;
     pkg = mkOption {
       type = types.package;
       # https://github.com/NixOS/nixpkgs/issues/395169
-      default = pkgs.emacs-git;
+      default = if isDarwin then pkgs'.emacs-git else pkgs.emacs-git;
     };
 
     lspStyle = mkOption {
