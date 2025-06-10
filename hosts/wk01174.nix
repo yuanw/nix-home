6ca7db20ee4a55a32cbe692850f0c00c70ad68c6
yuanwang <yuan.wang@workiva.com>
2025-06-09 20:27:15 -0600

    try lsp-bridge

--- hosts/wk01174.nix
+++ hosts/wk01174.nix
@@ -59,7 +59,6 @@
         "sloth"
         # "mysql-shell"
         "viscosity"
-        #"brave-browser"
         # "zoom"
       ];
       brews = [
@@ -84,7 +83,7 @@
 
       #enableAider = true;
       # enableCopilot = true;
-      #lspStyle = "lsp-bridge";
+      lspStyle = "lsp-bridge";
     };
     # health.enable = true;
     typing.enable = true;
