ee0f9d49eff70c7921cbf447b6695caae219f053
yuanwang <yuan.wang@workiva.com>
2025-06-14 22:07:34 -0600

    wip

--- modules/macintosh.nix
+++ modules/macintosh.nix
@@ -115,13 +115,13 @@ in
     serviceConfig.KeepAlive.SuccessfulExit = false;
     command = "${nixPackage}/bin/nix-collect-garbage --delete-older-than 3d";
     serviceConfig.RunAtLoad = true;
-    # serviceConfig.StartCalendarInterval = [
-    #   {
-    #     Weekday = 7;
-    #     Hour = 3;
-    #     Minute = 15;
-    #   }
-    # ];
+    serviceConfig.StartCalendarInterval = [
+      {
+        Weekday = 7;
+        Hour = 3;
+        Minute = 15;
+      }
+    ];
     serviceConfig.StandardErrorPath = "/tmp/daemons-nix-gc.log";
     serviceConfig.StandardOutPath = "/tmp/daemons-nix-gc.log";
   };
@@ -134,9 +134,9 @@ in
   #   EOF
   # '';
 
-  launchd.user.agents.nix-gc = {
+  launchd.user.agents.user-nix-gc = {
     command = "${nixPackage}/bin/nix-collect-garbage  --delete-older-than 3d";
-    serviceConfig.RunAtLoad = false;
+    serviceConfig.RunAtLoad = true;
     serviceConfig.StartCalendarInterval = [
       {
         Weekday = 7;
