fbd2efb1af162b990d2d6f204222585b7088deb3
yuanwang <yuan.wang@workiva.com>
2025-06-16 22:13:43 -0600

    wip

--- modules/editor/emacs/packages/eaf-pdf-viewer.nix
+++ modules/editor/emacs/packages/eaf-pdf-viewer.nix
@@ -4,19 +4,18 @@
   lib,
   ...
 }:
-melpaBuild
-  (_finalAttrs: {
-    pname = "eaf-browser";
-    version = "0-unstable-2025-06-13";
+(melpaBuild (_finalAttrs: {
+  pname = "eaf-browser";
+  version = "0-unstable-2025-06-13";
 
-    src = fetchFromGitHub {
-      owner = "emacs-eaf";
-      repo = "eaf-pdf-viewer";
-      rev = "96fa83176f17a33e516bdb4e0abaa832e3328248";
-      sha256 = lib.fakeSha256;
-      #sha256 = "sha256-DsPrctB1bSGBPQLI2LsnSUtqnzWpZRrWrVZM8lS9fms=";
-    };
-  }).overrideAttrs
+  src = fetchFromGitHub {
+    owner = "emacs-eaf";
+    repo = "eaf-pdf-viewer";
+    rev = "96fa83176f17a33e516bdb4e0abaa832e3328248";
+    sha256 = lib.fakeSha256;
+    #sha256 = "sha256-DsPrctB1bSGBPQLI2LsnSUtqnzWpZRrWrVZM8lS9fms=";
+  };
+})).overrideAttrs
   {
     # Override genericBuild's postInstall, which tries to native-compile Elisp
     # files.
