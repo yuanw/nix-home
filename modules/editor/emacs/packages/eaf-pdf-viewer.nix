7acb30817f104d37b62d3d011a803ffd407d00fa
yuanwang <yuan.wang@workiva.com>
2025-06-16 22:33:42 -0600

    what

--- modules/editor/emacs/packages/eaf-pdf-viewer.nix
+++ modules/editor/emacs/packages/eaf-pdf-viewer.nix
@@ -1,8 +1,15 @@
 {
   melpaBuild,
   fetchFromGitHub,
+  writeText,
+  elpa2nix,
+  melpa2nix,
+
   ...
 }:
+let
+  inherit (lib) readFile;
+in
 (melpaBuild (_finalAttrs: {
   pname = "eaf-pdf-viewer";
   version = "0-unstable-2025-06-13";
@@ -14,6 +21,15 @@
     #sha256 = lib.fakeSha256;
     sha256 = "sha256-JEvIw9JL4P6+za9twhSBDlHLC3VABiR/Ovqjww8jU8E=";
   };
+
+  elpa2nix = writeText "elpa2nix.el" ''
+    ${readFile elpa2nix}
+    (defun byte-recompile-directory (&rest _))
+  '';
+  melpa2nix = writeText "melpa2nix.el" ''
+    ${readFile melpa2nix}
+    (defun byte-recompile-directory (&rest _))
+  '';
 })).overrideAttrs
   {
     # Override genericBuild's postInstall, which tries to native-compile Elisp
