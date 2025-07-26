c7902001bce07d58917b069acff1a53ac197b44d
yuanw <me@yuanwang.ca>
2025-07-26 16:59:10 -0600

    wip

--- hosts/default.nix
+++ hosts/default.nix
@@ -61,112 +61,135 @@
                   inputs.catppuccin.homeModules.catppuccin
                 ];
                 # users.johnw = import ./config/home.nix;
-
                 backupFileExtension = "hm-bak";
                 extraSpecialArgs = { inherit inputs; };
               };
             }
-
             ./asche
           ];
         }
       );
-    };
-    darwinConfigurations =
-      let
-        configure =
-          hostname: sys: loadPrivate: addtionsModule:
-          withSystem sys (
+      misfit = withSystem "x86_64-linux" (
+        {
+          config,
+          inputs',
+          system,
+          ...
+        }:
+        inputs.nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            isDarwin = false;
+            isNixOS = true;
+            packages = config.packages;
+            nurNoPkg = import inputs.nur {
+              nurpkgs = import inputs.nixpkgs { system = system; };
+            };
+            inherit inputs inputs';
+          };
+          modules = [
             {
-              config,
-              inputs',
-              system,
-              ...
-            }:
-            inputs.nix-darwin.lib.darwinSystem {
-              specialArgs = {
-                isDarwin = true;
-                isNixOS = false;
-                loadPrivate = loadPrivate;
-                nurNoPkg = import inputs.nur {
-                  nurpkgs = import inputs.nixpkgs { system = system; };
-                };
-                packages = config.packages;
-                inherit hostname inputs inputs';
-              };
-              modules = [
-                {
-                  nixpkgs.hostPlatform = system;
-                }
-                inputs.home-manager.darwinModules.home-manager
-                {
-                  home-manager = {
-                    useGlobalPkgs = true;
-                    useUserPackages = true;
-                    sharedModules = [
-                      inputs.betterfox.homeManagerModules.betterfox
-                      inputs.catppuccin.homeModules.catppuccin
-                    ];
-                    # users.johnw = import ./config/home.nix;
-
-                    backupFileExtension = "hm-bak";
-                    extraSpecialArgs = { inherit inputs; };
-                  };
-                }
-                addtionsModule
-              ];
+              nixpkgs.hostPlatform = system;
             }
-          );
-      in
-      {
-        ci = configure "ci" "aarch64-darwin" false ./yuan-mac.nix;
-        yuanw = configure "yuanw" "x86_64-darwin" false ./yuan-mac.nix;
-        mist = configure "mist" "aarch64-darwin" true ./mist.nix;
-        WK01174 = configure "WK01174" "aarch64-darwin" true ./wk01174.nix;
-      };
-  };
-  perSystem =
-    { system, ... }:
-    {
-      packages.asche = self.nixosConfigurations.asche.config.system.build.toplevel;
-      packages.yuanw = self.darwinConfigurations.yuanw.system;
-      packages.ci = self.darwinConfigurations.ci.system;
-      packages.wk01174 = self.darwinConfigurations.WK01174.system;
-      packages.mist = self.darwinConfigurations.mist.system;
+            ./misfit
+          ];
+        }
+      );
+      darwinConfigurations =
+        let
+          configure =
+            hostname: sys: loadPrivate: addtionsModule:
+            withSystem sys (
+              {
+                config,
+                inputs',
+                system,
+                ...
+              }:
+              inputs.nix-darwin.lib.darwinSystem {
+                specialArgs = {
+                  isDarwin = true;
+                  isNixOS = false;
+                  loadPrivate = loadPrivate;
+                  nurNoPkg = import inputs.nur {
+                    nurpkgs = import inputs.nixpkgs { system = system; };
+                  };
+                  packages = config.packages;
+                  inherit hostname inputs inputs';
+                };
+                modules = [
+                  {
+                    nixpkgs.hostPlatform = system;
+                  }
+                  inputs.home-manager.darwinModules.home-manager
+                  {
+                    home-manager = {
+                      useGlobalPkgs = true;
+                      useUserPackages = true;
+                      sharedModules = [
+                        inputs.betterfox.homeManagerModules.betterfox
+                        inputs.catppuccin.homeModules.catppuccin
+                      ];
+                      # users.johnw = import ./config/home.nix;
 
-      # packages.activate = pkgs.writeShellApplication
-      #   {
-      #     name = "activate";
-      #     text =
-      #       # TODO: Replace with deploy-rs or (new) nixinate
-      #       if system == "aarch64-darwin" || system == "x86_64-darwin" then
-      #         let
-      #           # This is used just to pull out the `darwin-rebuild` script.
-      #           # See also: https://github.com/LnL7/nix-darwin/issues/613
-      #           emptyConfiguration = darwinSystem { nixpkgs.hostPlatform = system; };
-      #         in
-      #         ''
-      #           HOSTNAME=$(hostname -s)
-      #           set -x
-      #           ${emptyConfiguration.system}/sw/bin/darwin-rebuild \
-      #             switch \
-      #             --flake .#"''${HOSTNAME}" \
-      #             "$@"
-      #           doom sync
-      #         ''
-      #       else
-      #         ''
-      #           HOSTNAME=$(hostname -s)
-      #           set -x
-      #           ${lib.getExe pkgs.nixos-rebuild} \
-      #             switch \
-      #             --flake .#"''${HOSTNAME}" \
-      #             --use-remote-sudo \
-      #             "$@"
-      #         '';
-
-      #   };
-
+                      backupFileExtension = "hm-bak";
+                      extraSpecialArgs = { inherit inputs; };
+                    };
+                  }
+                  addtionsModule
+                ];
+              }
+            );
+        in
+        {
+          ci = configure "ci" "aarch64-darwin" false ./yuan-mac.nix;
+          yuanw = configure "yuanw" "x86_64-darwin" false ./yuan-mac.nix;
+          mist = configure "mist" "aarch64-darwin" true ./mist.nix;
+          WK01174 = configure "WK01174" "aarch64-darwin" true ./wk01174.nix;
+        };
     };
+    perSystem =
+      { system, ... }:
+      {
+        packages.asche = self.nixosConfigurations.asche.config.system.build.toplevel;
+        packages.yuanw = self.darwinConfigurations.yuanw.system;
+        packages.ci = self.darwinConfigurations.ci.system;
+        packages.wk01174 = self.darwinConfigurations.WK01174.system;
+        packages.mist = self.darwinConfigurations.mist.system;
 
+        # packages.activate = pkgs.writeShellApplication
+        #   {
+        #     name = "activate";
+        #     text =
+        #       # TODO: Replace with deploy-rs or (new) nixinate
+        #       if system == "aarch64-darwin" || system == "x86_64-darwin" then
+        #         let
+        #           # This is used just to pull out the `darwin-rebuild` script.
+        #           # See also: https://github.com/LnL7/nix-darwin/issues/613
+        #           emptyConfiguration = darwinSystem { nixpkgs.hostPlatform = system; };
+        #         in
+        #         ''
+        #           HOSTNAME=$(hostname -s)
+        #           set -x
+        #           ${emptyConfiguration.system}/sw/bin/darwin-rebuild \
+        #             switch \
+        #             --flake .#"''${HOSTNAME}" \
+        #             "$@"
+        #           doom sync
+        #         ''
+        #       else
+        #         ''
+        #           HOSTNAME=$(hostname -s)
+        #           set -x
+        #           ${lib.getExe pkgs.nixos-rebuild} \
+        #             switch \
+        #             --flake .#"''${HOSTNAME}" \
+        #             --use-remote-sudo \
+        #             "$@"
+        #         '';
+
+        #   };
+
+      };
+
+  };
 }
