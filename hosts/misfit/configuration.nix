21a13c7d3bd70acc22d93db75b93b4051c4868ff
yuanw <me@yuanwang.ca>
2025-07-27 08:11:13 -0600

    disable system.copySystemConfiguration

--- hosts/misfit/configuration.nix
+++ hosts/misfit/configuration.nix
@@ -124,7 +124,8 @@
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+  # does not work with flake
+  # system.copySystemConfiguration = true;
 
   nix = {
     # package = pkgs.nixVersions.git;
