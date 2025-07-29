42013256b35bbe75c09d9943f4e5964e8fd9c4a8
yuanw <me@yuanwang.ca>
2025-07-28 19:13:16 -0600

    wip

--- hosts/misfit/isponsorblocktv.nix
+++ hosts/misfit/isponsorblocktv.nix
@@ -99,7 +99,16 @@ in
       uid = 199;
 
     };
+    virtualisation.containers.enable = true;
+    virtualisation.podman = {
+      enable = true;
 
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
     virtualisation.oci-containers.containers.isponsorblocktv = {
 
       image = "ghcr.io/dmunozv04/isponsorblocktv:${ver}";
