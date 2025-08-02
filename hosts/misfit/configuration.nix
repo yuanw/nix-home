66208930a3ed72be2e1a35e8672c5925b8b501f4
yuanw <me@yuanwang.ca>
2025-08-02 07:37:51 -0600

    wit

--- hosts/misfit/configuration.nix
+++ hosts/misfit/configuration.nix
@@ -78,7 +78,7 @@
       dnsProvider = "cloudflare";
       dnsResolver = "1.1.1.1:53";
       #dnsPropagationCheck = true;
-      webroot = "/var/lib/acme";
+      #webroot = "/var/lib/acme";
       environmentFile = config.age.secrets.namecheap.path;
     };
   };
