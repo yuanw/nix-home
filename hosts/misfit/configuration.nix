e2415269fac63a8fb23c3fafb074876eb6cb5104
yuanw <me@yuanwang.ca>
2025-08-02 07:41:34 -0600

    wip

--- hosts/misfit/configuration.nix
+++ hosts/misfit/configuration.nix
@@ -74,7 +74,7 @@
 
     certs."minilla.store" = {
       group = config.services.caddy.group;
-      domain = "*.minilla.store";
+      domain = "minilla.store";
       dnsProvider = "cloudflare";
       dnsResolver = "1.1.1.1:53";
       #dnsPropagationCheck = true;
