7cf223485de979cbb5ce47770b4a108c723e49e1
yuanw <me@yuanwang.ca>
2025-08-04 16:07:03 -0600

    wip

--- hosts/misfit/configuration.nix
+++ hosts/misfit/configuration.nix
@@ -57,6 +57,17 @@
     #   "luci"
     #   "default_config"
     # ];
+    extraComponents = [
+      # Components required to complete the onboarding
+      "analytics"
+      "google_translate"
+      "met"
+      "radio_browser"
+      "shopping_list"
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      "isal"
+    ];
     config = {
       http = {
         use_x_forwarded_for = true;
