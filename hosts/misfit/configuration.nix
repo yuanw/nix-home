98aa1ddbdcdbf64dcb29a3bdaa12006efe9249d4
yuanw <me@yuanwang.ca>
2025-08-16 13:19:57 -0600

    fix password

--- hosts/misfit/configuration.nix
+++ hosts/misfit/configuration.nix
@@ -13,6 +13,7 @@
   ];
 
   # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
