b8dff0fe014647b31a56037760b44195d9ec6c16
yuanwang <yuan.wang@workiva.com>
2025-09-10 17:49:22 -0600

    wip

--- packages/emacs/expreg.nix
+++ packages/emacs/expreg.nix
@@ -2,7 +2,7 @@
   melpaBuild,
   fetchFromGitHub,
   writeText,
-  libs,
+  lib,
 
   # Elisp dependencies
 
@@ -21,7 +21,9 @@ melpaBuild {
     repo = "home-row-expreg";
     #https://github.com/casouri/expreg/pull/9
     rev = "278c413dae4c5ea5fd9d8afe71ce2a8e4548238c";
-    sha256 = libs.fake256;
+
+    sha256 = lib.fakeSha256;
+
     # sha256 = "sha256-1s1Fo5oR5tUBC9JhXo76NBqqeoS+P7ID3PN7t3xLewI=";
 
   };
