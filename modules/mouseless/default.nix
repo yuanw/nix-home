43a6e16ce3b17e85a30e1ae303e66187ec678b31
yuanwang <yuan.wang@workiva.com>
2025-09-22 14:17:48 -0600

    add mouseless module

--- modules/mouseless/default.nix
+++ modules/mouseless/default.nix
@@ -1,4 +1,28 @@
-{ ... }:
+{ config, lib, ... }:
+
+with lib;
+let
+  cfg = config.modules.mouseless;
+in
 {
 
+  options.modules.wm.amethyst = {
+    enable = mkEnableOption "mouseless";
+  };
+  config = mkIf cfg.enable {
+    assertions = [
+      {
+        assertion = config.brew.enable;
+        message = "need homebrew to install mouseless (for now)";
+      }
+
+    ];
+    brew = {
+      casks = [
+        "mouseless@preview"
+
+      ];
+    };
+
+  };
 }
