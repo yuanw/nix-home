20fddc6e3dc494ac130ad4ca0f09b224319a7330
yuanw <me@yuanwang.ca>
2025-12-01 21:26:11 -0600

    wip

--- hosts/asche/configuration.nix
+++ hosts/asche/configuration.nix
@@ -80,6 +80,14 @@
   #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
   # };
 
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "yuanw" ];
+  };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   #environment.systemPackages = with pkgs; [
@@ -98,6 +106,7 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+
 
   # List services that you want to enable:
 
