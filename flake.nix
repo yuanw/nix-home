65d3e8894951c51689efaf08ad9283058ca3176b
yuanwang <yuan.wang@workiva.com>
2025-12-26 11:34:44 -0600

    wip

--- flake.nix
+++ flake.nix
@@ -71,11 +71,6 @@
     };
   };
 
-  nixConfig = {
-    extra-substituters = [ "https://cache.numtide.com" ];
-    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
-  };
-
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
