2767831bb676b321b6c86079c52ed5f246fdcd79
yuanw <me@yuanwang.ca>
2026-01-03 20:50:06 -0600

    add kernelParams for power management in sleep

--- hosts/asche/configuration.nix
+++ hosts/asche/configuration.nix
@@ -20,6 +20,7 @@
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [ "intel_iommu=on" ];
 
   networking.hostName = "asche"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
