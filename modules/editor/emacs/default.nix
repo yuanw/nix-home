ff262454e6205770479f1e8ab14e76cf3a97a99b
yuanwang <yuan.wang@workiva.com>
2026-03-09 10:13:20 -0600

    wip

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -2128,11 +2128,7 @@ with lib;
                       inherit (epkgs) melpaBuild magit;
                     });
                   config = ''
-                    (require 'magit-ai-sections)
-                    (require 'magit-ai-blame)
-                    (require 'magit-ai-diff)
-                    (require 'magit-ai-log)
-                    (add-hook 'magit-status-sections-hook #'magit-insert-ai-stats t)
+                    (magit-ai-mode 1)
                   '';
                 };
 
