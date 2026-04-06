073798bac1f134d467ce43f7f148e8350b343396
yuanwang <yuan.wang@workiva.com>
2026-04-06 15:00:21 -0600

    use null

--- hosts/yuan-mac.nix
+++ hosts/yuan-mac.nix
@@ -36,7 +36,7 @@
       enable = true;
       enableClaudeMem = true;
       # llm-agent.claude-code doesn't work in garnix ci
-      pkg = pkgs.claude-code;
+      pkg = null;
     };
 
     browsers.firefox = {
