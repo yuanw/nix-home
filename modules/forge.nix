1a55687955139ee2d0f2f3d53cf2d39fb40f13b4
yuanw <me@yuanwang.ca>
2026-04-05 18:18:59 -0600

    add forge zsh Support

--- modules/forge.nix
+++ modules/forge.nix
@@ -10,13 +10,23 @@ in
 {
   options.modules.forge = {
     enable = lib.mkEnableOption "forge";
+    enableZshIntegration = lib.mkEnableOption "forge zsh plugin and theme";
   };
 
   config = lib.mkIf cfg.enable {
     home-manager.users.${config.my.username} = {
       home.packages = [
         pkgs.llm-agents.forge
+      ]
+      ++ lib.optionals cfg.enableZshIntegration [
+        pkgs.fzf
+        pkgs.fd
       ];
+
+      programs.zsh.initContent = lib.mkIf cfg.enableZshIntegration ''
+        eval "$(forge zsh plugin)"
+        eval "$(forge zsh theme)"
+      '';
     };
   };
 }
