3f3aa266c34cb9ed839a984e2fa245904d5a476f
yuanwang <yuan.wang@workiva.com>
2026-04-12 15:23:16 -0600

    debug

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -2371,7 +2371,7 @@ with lib;
                   '';
                 };
                 direnv = {
-                  enable = true;
+                  enable = false;
                   functions = [ "direnv--maybe-update-environment" ];
                   preface = ''
                      (defconst emacs-binary-path (directory-file-name
