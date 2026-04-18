5d0149903f2804985def6b25815cf50d88ce8eaf
yuanwang <yuan.wang@workiva.com>
2026-04-17 18:07:59 -0600

    wip

--- packages/parakeet-mlx-server.nix
+++ packages/parakeet-mlx-server.nix
@@ -1,14 +1,34 @@
 {
+  lib,
   writers,
+  runCommand,
   parakeet-mlx,
+  ffmpeg,
+  makeWrapper,
 }:
 
-writers.writePython3Bin "parakeet-mlx-server" {
-  libraries = [ parakeet-mlx ];
-  flakeIgnore = [
-    "E501"
-    "E402"
-    "E265"
-    "W503"
-  ];
-} ./parakeet-mlx-server.py
+let
+  pythonScript = writers.writePython3Bin "parakeet-mlx-server" {
+    libraries = [ parakeet-mlx ];
+    flakeIgnore = [
+      "E501"
+      "E402"
+      "E265"
+      "W503"
+    ];
+  } ./parakeet-mlx-server.py;
+in
+
+runCommand "parakeet-mlx-server"
+  {
+    nativeBuildInputs = [ makeWrapper ];
+    meta = with lib; {
+      description = "Persistent HTTP server for parakeet-mlx speech-to-text";
+      platforms = [ "aarch64-darwin" ];
+    };
+  }
+  ''
+    mkdir -p $out/bin
+    makeWrapper ${pythonScript}/bin/parakeet-mlx-server $out/bin/parakeet-mlx-server \
+      --prefix PATH : ${lib.makeBinPath [ ffmpeg ]}
+  ''
