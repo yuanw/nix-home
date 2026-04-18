c73160b239d9ec975f76728718b3e0e501c459e1
yuanwang <yuan.wang@workiva.com>
2026-04-18 06:55:33 -0600

    clean up

--- modules/editor/emacs/default.nix
+++ modules/editor/emacs/default.nix
@@ -125,34 +125,10 @@ with lib;
                   preferLocalBuild = true;
                   allowSubstitutes = false;
                 })
-                whisper
               ];
             package = emacsPatched;
             enable = true;
             overrides = self: _super: {
-              # Override go-jira to use current master
-              # expand-region = _super.expand-region.overrideAttrs (_oldAttrs: {
-              #   #https://github.com/karthink/expand-region.el/blob/master/expand-region.el
-              #   version = "unstable-2024-01-03";
-              #   src = pkgs.fetchFromGitHub {
-              #     owner = "karthink";
-              #     repo = "expand-region";
-              #     rev = "a30ac37e4d3d8ca7b4e91f5300f30e187d56a785";
-              #     hash = lib.fakeHash;
-              #   };
-              # });
-              home-row-expreg = (
-                pkgs.callPackage "${packagePath}/expreg.nix" {
-                  inherit (pkgs)
-                    fetchFromGitHub
-                    writeText
-                    ;
-                  inherit lib;
-                  inherit (self)
-                    melpaBuild
-                    ;
-                }
-              );
               gptel = (
                 pkgs.callPackage "${packagePath}/gptel.nix" {
                   inherit (pkgs)
