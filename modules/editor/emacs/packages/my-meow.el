4e6aca045ad6582feaa70b09347587091272dc86
yuanw <me@yuanwang.ca>
2026-04-26 13:13:11 -0600

    wip

--- modules/editor/emacs/packages/my-meow.el
+++ modules/editor/emacs/packages/my-meow.el
@@ -1,6 +1,7 @@
 ;;; my-meow.el --- My Meow configuration package  -*- lexical-binding: t; -*-
 ;; Thin wrapper package so that (require 'my-meow) succeeds.
 ;; Actual config lives in configs/meow.el and is loaded by hm--load-external.
+(require 'meow)
 
 (provide 'my-meow)
 ;;; my-meow.el ends here
