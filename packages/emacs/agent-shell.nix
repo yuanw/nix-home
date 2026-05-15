16d7733f1bfdb08ff101bc8f59799593fb497b66
yuanwang <yuan.wang@workiva.com>
2026-05-15 15:27:01 -0600

    update

--- packages/emacs/agent-shell.nix
+++ packages/emacs/agent-shell.nix
@@ -10,13 +10,13 @@
 
 melpaBuild {
   pname = "agent-shell";
-  version = "0.50.1-unstable-2026-04-12";
+  version = "0-unstable-2026-05-15";
 
   src = fetchFromGitHub {
     owner = "xenodium";
     repo = "agent-shell";
-    rev = "eb5a379c1db4e395a04acd1f16a6dfb92e803d60";
-    sha256 = "sha256-BkWFrS++asLU33mVrFA5hMNqnTbWXqWIYXGHmhrYooE=";
+    rev = "46623144c75b97163c26be5549b9e0c4bc3052d4";
+    sha256 = "sha256-LIbMfash7UNfb7ArNQ0BD+xs09JIt0Q7PNnkVA2ak7A=";
   };
 
   packageRequires = [
