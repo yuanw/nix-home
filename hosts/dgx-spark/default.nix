7d1eeab979ba098516d68aaf12a1664d5f3c87f5
yuanw <me@yuanwang.ca>
2026-05-17 18:55:49 -0600

    try preservation again
    wip

--- hosts/dgx-spark/default.nix
+++ hosts/dgx-spark/default.nix
@@ -6,6 +6,7 @@
 {
   imports = [
     inputs.dgx-spark.nixosModules.dgx-spark
+    inputs.preservation.nixosModules.preservation
     inputs.disko.nixosModules.disko
     ./disk-config.nix
     ./configuration.nix
--- hosts/dgx-spark/disk-config.nix
+++ hosts/dgx-spark/disk-config.nix
@@ -2,12 +2,22 @@
 
 {
   disko.devices = {
+    nodev = {
+      "/" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "size=25%"
+          "mode=755"
+        ];
+      };
+    };
     disk.main = {
       device = lib.mkDefault "/dev/nvme0n1";
       type = "disk";
       content = {
         type = "gpt";
         partitions = {
+
           esp = {
             size = "1G";
             type = "EF00";
@@ -18,13 +28,35 @@
               mountOptions = [ "umask=0077" ];
             };
           };
+          swap = {
+            size = "16G";
+            content = {
+              type = "swap";
+              resumeDevice = true;
+            };
+          };
           root = {
             name = "root";
             size = "100%";
             content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+                "/persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
+              };
             };
           };
         };
