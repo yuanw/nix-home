27a0193c06ff1509de67e4be6217f11d89f5b0db
yuanw <me@yuanwang.ca>
2026-05-18 05:43:40 -0600

    Revert "try preservation again"
    This reverts commit 7d1eeab979ba098516d68aaf12a1664d5f3c87f5.

--- hosts/dgx-spark/default.nix
+++ hosts/dgx-spark/default.nix
@@ -6,7 +6,6 @@
 {
   imports = [
     inputs.dgx-spark.nixosModules.dgx-spark
-    inputs.preservation.nixosModules.preservation
     inputs.disko.nixosModules.disko
     ./disk-config.nix
     ./configuration.nix
