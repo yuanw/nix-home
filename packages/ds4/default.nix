1797e30bcd52672c221a6e2ab83cb7cf54f04943
yuanw <me@yuanwang.ca>
2026-05-18 14:29:17 -0600

    add downloadmodel

--- packages/ds4/default.nix
+++ packages/ds4/default.nix
@@ -2,8 +2,22 @@
   lib,
   fetchFromGitHub,
   cudaPackages,
+  writeShellScriptBin,
 }:
 
+let
+  # Wrapper for download_model.sh that defaults to the current working
+  # directory instead of the Nix store path (which is read-only).
+  downloadModel = writeShellScriptBin "ds4-download-model" ''
+    : "${""}{DS4_GGUF_DIR:=$PWD/gguf}
+    export DS4_GGUF_DIR
+
+    # Find the original download_model.sh shipped with ds4
+    script="${placeholder "out"}/share/ds4/download_model.sh"
+
+    exec "$script" "$@"
+  '';
+in
 cudaPackages.backendStdenv.mkDerivation {
   pname = "ds4";
   version = "0-unstable-2026-05-17";
@@ -39,8 +53,10 @@ cudaPackages.backendStdenv.mkDerivation {
 
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/bin
+    mkdir -p $out/bin $out/share/ds4
     cp ds4 ds4-server ds4-bench ds4-eval $out/bin/
+    cp download_model.sh $out/share/ds4/
+    cp ${downloadModel}/bin/ds4-download-model $out/bin/
     runHook postInstall
   '';
 
