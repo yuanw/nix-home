31e75c4936148c7ecd9c85b77ba9e2e3348b4dbc
yuanw <me@yuanwang.ca>
2026-05-21 06:11:39 -0600

    wip

--- packages/decord/default.nix
+++ packages/decord/default.nix
@@ -2,13 +2,15 @@
   lib,
   python3,
   fetchurl,
+  autoPatchelfHook,
+  zlib,
 }:
 
 let
   version = "3.3.0";
   wheel = fetchurl {
     url = "https://files.pythonhosted.org/packages/e8/37/947bc17d6a16f5c678ab2c6ba3330b20f617ec7652f103051881cf1d98d9/decord2-3.3.0-cp313-cp313-manylinux_2_28_aarch64.whl";
-    hash = "sha256-3q3Rf8HMMVUXD1ocT3VS0IUPLmdjJg9p4HMLEtpDQxM=";
+    hash = "sha256-3q3RfMALZVRe9zH7X1jgXWJdyNtfvaJe3JvTBGk0NBM=";
   };
 in
 
@@ -18,6 +20,12 @@ python3.pkgs.buildPythonPackage {
   format = "wheel";
 
   src = wheel;
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+
+  buildInputs = [ zlib ];
+
+  propagatedBuildInputs = with python3.pkgs; [ numpy ];
 
   doCheck = false;
 
