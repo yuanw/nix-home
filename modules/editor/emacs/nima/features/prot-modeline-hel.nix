902aa1aa7c85a690c8e2d9aba31cf81d66e000c4
yuan <me@yuanwang.ca>
2026-07-31 06:28:15 -0600

    add

---
+++ modules/editor/emacs/nima/features/prot-modeline-hel.nix
@@ -0,0 +1,7 @@
+{
+  # Load after both `prot-modeline' and `hel' feature files have contributed
+  # their configuration to generated default.el.
+  order = 100;
+
+  elispFile = ../../configs/prot-modeline-hel.el;
+}
