d57f5bf84e1a101bdf21efe1ce2889ee716e2d94
yuan <me@yuanwang.ca>
2026-07-31 06:43:27 -0600

    add test

--- modules/editor/emacs/nima/features/prot-modeline-hel.nix
+++ modules/editor/emacs/nima/features/prot-modeline-hel.nix
@@ -1,7 +1,13 @@
-{
+{ lib, ... }:
+
+let
+  feature = import ../lib/feature.nix { inherit lib; };
+in
+feature.mkElispFeature {
+  name = "prot-modeline-hel";
+  file = ../../configs/prot-modeline-hel.el;
+
   # Load after both `prot-modeline' and `hel' feature files have contributed
   # their configuration to generated default.el.
   order = 100;
-
-  elispFile = ../../configs/prot-modeline-hel.el;
 }
--- tests/flake-module.nix
+++ tests/flake-module.nix
@@ -4,5 +4,6 @@
     { pkgs, ... }:
     {
       checks.mergetools = pkgs.callPackage ./mergetools.nix { };
+      checks.nima-feature-helper = pkgs.callPackage ./nima-feature-helper.nix { };
     };
 }
