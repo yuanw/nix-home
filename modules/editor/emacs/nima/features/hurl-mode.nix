37443f6c54e5c859082d7d00cf0c5f2ac2e9cb78
yuan <me@yuanwang.ca>
2026-07-31 06:21:27 -0600

    what

--- modules/editor/emacs/nima/features/hurl-mode.nix
+++ modules/editor/emacs/nima/features/hurl-mode.nix
@@ -15,6 +15,9 @@ in
   elisp = ''
     (use-package hurl-mode
       :config
-      (add-to-list 'org-babel-load-languages '(hurl . t)))
+      ;; `org-babel-load-languages' is defined by Org/Babel, so do not touch it
+      ;; before Org has loaded.
+      (with-eval-after-load 'ob-core
+        (add-to-list 'org-babel-load-languages '(hurl . t))))
   '';
 }
