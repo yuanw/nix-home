c4a981ed1971a235a44adecb33ff27027ca74ae3
yuan <me@yuanwang.ca>
2026-08-02 12:10:18 -0600

    wip

--- modules/editor/emacs/nima/features/prelude.nix
+++ modules/editor/emacs/nima/features/prelude.nix
@@ -1,12 +1,7 @@
 {
-  lib,
-  myDefvar,
   ...
 }:
 
-let
-  vars = import ../lib/elisp-vars.nix { inherit lib; };
-in
 {
   # Make this Elisp config come first in generated default.el.
   order = -100;
@@ -16,11 +11,4 @@ in
     epkgs.use-package
   ];
 
-  # This feature needs access to Nix values, so use `elisp` instead of
-  # `elispFile`.  nima forbids setting both for one feature.
-  elisp = ''
-    ${vars.mkDefvars myDefvar}
-
-    ${builtins.readFile ./prelude.el}
-  '';
 }
