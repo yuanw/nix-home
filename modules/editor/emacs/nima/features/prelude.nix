17d71795fb5771947ce0bcfa671db28bfa397bc8
yuan <me@yuanwang.ca>
2026-08-03 07:12:37 -0600

    wip

--- modules/editor/emacs/nima/features/prelude.nix
+++ modules/editor/emacs/nima/features/prelude.nix
@@ -6,13 +6,4 @@
   # Make this Elisp config come first in generated default.el.
   order = options.features.valueMeta.attrs.${thisFeature}.configuration.options.order.default - 10000;
 
-  # This is also nima's default for ./prelude.nix, but keeping it explicit makes
-  # it obvious that ./prelude.el is included in generated default.el.
-  elispFile = ./prelude.el;
-
-  # Packages/features that later modules can assume are available.
-  epkgs = epkgs: [
-    epkgs.use-package
-  ];
-
 }
