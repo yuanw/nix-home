aa45d30588503265d1921f5e63d01779f43d427f
yuan <me@yuanwang.ca>
2026-08-03 06:40:30 -0600

    remove ef-themes from early-init

--- modules/editor/emacs/early-init.el
+++ modules/editor/emacs/early-init.el
@@ -85,32 +85,7 @@
 ;; Fix for Emacs 31: prefer .el files over potentially incompatible .elc files
 (setq load-prefer-newer t)
 
-(require 'ef-themes)
 
-;; If you like two specific themes and want to switch between them, you
-;; can specify them in `ef-themes-to-toggle' and then invoke the command
-;; `ef-themes-toggle'.  All the themes are included in the variable
-;; `ef-themes-collection'.
-(setq ef-themes-to-toggle '(ef-day ef-owl))
-(ef-themes-take-over-modus-themes-mode 1)
-(setq modus-themes-mixed-fonts t)
-(setq ef-themes-headings ; read the manual's entry or the doc string
-      '((0 variable-pitch light 1.9)
-        (1 variable-pitch light 1.8)
-        (2 variable-pitch regular 1.7)
-        (3 variable-pitch regular 1.6)
-        (4 variable-pitch regular 1.5)
-        (5 variable-pitch 1.4) ; absence of weight means `bold'
-        (6 variable-pitch 1.3)
-        (7 variable-pitch 1.2)
-        (t variable-pitch 1.1)))
-
-;; They are nil by default...
-(setq ef-themes-mixed-fonts t
-      ef-themes-variable-pitch-ui t)
-
-;; Disable all other themes to avoid awkward blending:
-(mapc #'disable-theme custom-enabled-themes)
 (setq org-startup-with-inline-images t)
 
 ;; Theme loading moved to init (after display initialization)
