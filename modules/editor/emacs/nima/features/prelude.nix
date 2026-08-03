505107cb2f86b373920f612f54b4880a61cb7203
yuan <me@yuanwang.ca>
2026-08-03 06:48:03 -0600

    wip

--- modules/editor/emacs/nima/features/prelude.nix
+++ modules/editor/emacs/nima/features/prelude.nix
@@ -1,11 +1,10 @@
 {
-  ...
+  thisFeature,
 }:
-
+{ options, ... }:
 {
   # Make this Elisp config come first in generated default.el.
-  order = -100;
-
+  order = options.features.valueMeta.attrs.${thisFeature}.configuration.options.order.default - 10000;
   # Packages/features that later modules can assume are available.
   epkgs = epkgs: [
     epkgs.use-package
