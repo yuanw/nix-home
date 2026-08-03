d1d54760c3481e21adc77848a52fd85f946faf64
yuan <me@yuanwang.ca>
2026-08-03 11:39:29 -0600

    fix

--- modules/editor/emacs/nima/features/dired.nix
+++ modules/editor/emacs/nima/features/dired.nix
@@ -11,9 +11,10 @@
       :config
       (put 'dired-find-alternate-file 'disabled nil)
       ;; macOS /bin/ls does not support --group-directories-first;
-      ;; use GNU coreutils gls which does.
+      ;; use GNU coreutils from Nix instead.  Nix's coreutils binary is `ls`,
+      ;; unlike Homebrew's `gls`.
       (when (eq system-type 'darwin)
-        (setq insert-directory-program "${pkgs.coreutils}/bin/gls")
+        (setq insert-directory-program "${pkgs.coreutils}/bin/ls")
         (setq dired-use-ls-dired t))
       ;; Be smart about choosing file targets.
       (setq dired-dwim-target t)
