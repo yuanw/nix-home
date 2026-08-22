fe70d9b52d8c8b3660a7e6fed93eabde125a2593
yuan <me@yuanwang.ca>
2026-08-22 10:00:23 -0600

    try

--- modules/editor/emacs/features/auto-dark.nix
+++ modules/editor/emacs/features/auto-dark.nix
@@ -1,12 +1,14 @@
 {
   epkgs = epkgs: [ epkgs.auto-dark ];
   elisp = ''
-
-
     (use-package auto-dark
-      :custom
-      (auto-dark-themes (mapcar #'list modus-themes-to-toggle))
+      :after ef-themes
       :init
-      (auto-dark-mode))
+      ;; `modus-themes-to-toggle' is not available unless modus-themes is
+      ;; loaded.  We use Ef themes here, so configure Auto Dark explicitly:
+      ;; first list is dark themes, second list is light themes.
+      (setq auto-dark-themes '((ef-owl) (ef-spring)))
+      :config
+      (auto-dark-mode 1))
   '';
 }
