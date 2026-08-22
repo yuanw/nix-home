e44497fd8792543b808f6e3c3aee0ee0d0aec85d
yuan <me@yuanwang.ca>
2026-08-22 08:52:51 -0600

    clean up

--- tests/flake-module.nix
+++ tests/flake-module.nix
@@ -4,7 +4,5 @@
     { pkgs, ... }:
     {
       checks.mergetools = pkgs.callPackage ./mergetools.nix { };
-      checks.nima-feature-helper = pkgs.callPackage ./nima-feature-helper.nix { };
-      checks.nima-use-package-helper = pkgs.callPackage ./nima-use-package-helper.nix { };
     };
 }
