2a50ad060ae63b7398a7061250a06ee461202b1e
yuanwang <yuan.wang@workiva.com>
2026-08-24 15:15:15 -0600

    update the hel ref

--- packages/emacs/hel.nix
+++ packages/emacs/hel.nix
@@ -11,7 +11,8 @@
 }:
 
 let
-  version = "0.12.0";
+  version = "0.12.0-unstable-2026-03-16";
+  rev = "7706d7a5adbbef9d604d30f7e86a9322a7936447";
 in
 
 melpaBuild {
@@ -19,15 +20,11 @@ melpaBuild {
   inherit version;
 
   src = fetchFromGitHub {
-    owner = "anuvyklack";
+    owner = "helheim-emacs";
     repo = "hel";
-    rev = "v${version}";
-    sha256 = "sha256-1YXo9QXIxrvUa09Fh7zSMyIsxrFpFdizP4fWyBdDtbs=";
+    inherit rev;
+    sha256 = "sha256-VWT90d1NCh68fTyJ5R5s5ZDCNDeVSWhGY1WNxXLfhTc=";
   };
-
-  patches = [
-    ./patches/hel-integration-z-prefix.patch
-  ];
 
   packageRequires = [
     dash
@@ -38,14 +35,14 @@ melpaBuild {
 
   recipe = writeText "recipe" ''
     (hel
-     :repo "anuvyklack/hel"
+     :repo "helheim-emacs/hel"
      :fetcher github
      :files ("*.el"))
   '';
 
   meta = with lib; {
     description = "Helix emulation layer for Emacs";
-    homepage = "https://github.com/anuvyklack/hel";
+    homepage = "https://github.com/helheim-emacs/hel";
     license = licenses.gpl3Only;
     platforms = platforms.all;
   };
